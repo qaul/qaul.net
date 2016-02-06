6a258529569af11d74b2dcf95002c8c2d5e47dc9
Jaromil <jaromil@dyne.org>
2016-02-06 15:42:45 +0100

    small fixes to the cli client
    gather the configured PREFIX from the build stage
    use stderr for all logs so scripts can pipe and parse

--- src/client/cli/main.c
+++ src/client/cli/main.c
@@ -21,34 +21,35 @@
 #include "qaullib.h"
 #include <QaulConfig.h>
 
-
 // ------------------------------------------------------------
 int main(int argc, char *argv[])
 {
 	char cCurrentPath[FILENAME_MAX];
+        char prefix[FILENAME_MAX];
 
 	if (!GetCurrentDir(cCurrentPath, sizeof(cCurrentPath)))
 	{
-		printf ("ERROR: couldn't get directory\n");
+		fprintf(stderr,"ERROR: couldn't get directory\n");
 		return EXIT_FAILURE;
 	}
 	cCurrentPath[sizeof(cCurrentPath) - 1] = '\0';
-	printf ("The current working directory is %s\n", cCurrentPath);
+	fprintf(stderr,"The current working directory is %s\n", cCurrentPath);
 
-	// TODO: set resource path in accordance with to platform
-	Qaullib_Init(cCurrentPath, "/usr/local/lib/qaul");
+        snprintf(prefix,FILENAME_MAX,"%s/lib/qaul",PREFIX);
+
+	Qaullib_Init(cCurrentPath, prefix);
 	// enable debug menu
 	qaul_conf_debug = 1;
 
 	if(!Qaullib_WebserverStart())
-		printf("Webserver startup failed\n");
+		fprintf(stderr,"Webserver startup failed\n");
 	Qaullib_ConfigStart();
 
-	printf("----------------------------------------------------\n");
-	printf(" config started\n");
-	printf("----------------------------------------------------\n");
+	fprintf(stderr,"----------------------------------------------------\n");
+	fprintf(stderr," config started\n");
+	fprintf(stderr,"----------------------------------------------------\n");
 	// The invoking of Qaullib_GetIP() is mandatory to load the IP.
-	printf("IP: %s\n", Qaullib_GetIP());
+	fprintf(stderr,"IP: %s\n", Qaullib_GetIP());
 
 	// wait until user name is set
 	int username_flag = 0;
@@ -57,31 +58,31 @@ int main(int argc, char *argv[])
 		if(username_flag == 0)
 		{
 			username_flag = 1;
-			printf("waiting until user name is set ...\n");
-			printf("open web browser with http://localhost:8081/qaul.html to set it ...\n");
+			fprintf(stderr,"waiting until user name is set ...\n");
+			fprintf(stderr,"open web browser with http://localhost:8081/qaul.html to set it ...\n");
 		}
 		sleep(1);
 	}
-	printf("user name successfully set!\n");
+	fprintf(stderr,"user name successfully set!\n");
 
 	if(!Qaullib_IpcConnect())
-		printf("Ipc connection failed\n");
+		fprintf(stderr,"Ipc connection failed\n");
 	Qaullib_SetConfVoIP();
 	if(!Qaullib_UDP_StartServer())
-		printf("UDP server failed\n");
+		fprintf(stderr,"UDP server failed\n");
 	if(!Qaullib_CaptiveStart())
-		printf("Captive portal failed\n");
+		fprintf(stderr,"Captive portal failed\n");
 	Qaullib_ConfigurationFinished();
 
 	// test config
-	printf("IP: %s\n", Qaullib_GetIP());
-	printf("Qaul started\n");
+	fprintf(stderr,"IP: %s\n", Qaullib_GetIP());
+	fprintf(stderr,"Qaul started\n");
 
 	// loop variables
 	int socketCounter = 0;
 	int ipcCounter = 0;
 
-	printf("kill app to exit!\n");
+	fprintf(stderr,"kill app to exit!\n");
 
 	// main loop
 	while (1) {
@@ -90,11 +91,11 @@ int main(int argc, char *argv[])
 		// get event
 		int event = Qaullib_TimedCheckAppEvent();
 		if(event == QAUL_EVENT_QUIT)
-			printf("quit app\n");
+			fprintf(stderr,"quit app\n");
 		else if(event == QAUL_EVENT_CHOOSEFILE)
-			printf("open file chooser\n");
+			fprintf(stderr,"open file chooser\n");
 		else if(event == QAUL_EVENT_OPENFILE)
-			printf("open file\n");
+			fprintf(stderr,"open file\n");
 
 		// check sockets
 		if(socketCounter >= 10)
