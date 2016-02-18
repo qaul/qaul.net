9806b48291fe9e1344ab32e778603eaecf45d5e7
saces <saces@qaul.net>
2016-02-18 01:22:42 +0100

    maybe fix broken cli build

--- cmake/BuildLinux.cmake
+++ cmake/BuildLinux.cmake
@@ -2,6 +2,9 @@
 find_package (PkgConfig)
 pkg_check_modules (DBUS1 REQUIRED dbus-1)
 
+pkg_check_modules (OPENCORE_AMRNB opencore-amrnb)
+pkg_check_modules (OPENCORE_AMRWB opencore-amrwb)
+
 find_package (Autotools REQUIRED)
 
 add_subdirectory (src/qaulhelper)
