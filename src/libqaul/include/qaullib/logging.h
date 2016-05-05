c9cfc08da9f73021889ed42ab2a57bb346c71517
saces <saces@qaul.net>
2016-05-05 02:13:01 +0200

    change logger prefix ql -> Ql

--- src/libqaul/include/qaullib/logging.h
+++ src/libqaul/include/qaullib/logging.h
@@ -37,41 +37,41 @@ const char *const LOG_LEVEL_NAMES[] = {
     "DEBUG"
 };
 
-#define ql_levelname(M) LOG_LEVEL_NAMES[M]
+#define Ql_levelname(M) LOG_LEVEL_NAMES[M]
 
 #ifdef NDEBUG
-#define ql_debug(M, ...)
+#define Ql_debug(M, ...)
 #else
-#define ql_debug(M, ...) ql_logline(LOG_DEBUG, 0, M, ##__VA_ARGS__)
+#define Ql_debug(M, ...) Ql_logline(LOG_DEBUG, 0, M, ##__VA_ARGS__)
 #endif
 
-#define ql_clean_errno() (errno == 0 ? "None" : strerror(errno))
+#define Ql_clean_errno() (errno == 0 ? "None" : strerror(errno))
 
-#define ql_logline(L, E, M, ...) { \
+#define Ql_logline(L, E, M, ...) { \
 	char date[20]; \
 	struct timeval tv; \
 	gettimeofday(&tv, NULL); \
 	strftime(date, sizeof(date) / sizeof(*date), "%Y-%m-%dT%H:%M:%S", gmtime(&tv.tv_sec)); \
 	if ( E ) { \
-		fprintf(stderr, "%s.%03dZ [%s] (%s:%d: errno: %s) " M "\n", &date[0], (tv.tv_usec/1000), ql_levelname( L ), __FILE__, __LINE__, ql_clean_errno(), ##__VA_ARGS__); \
+		fprintf(stderr, "%s.%03dZ [%s] (%s:%d: errno: %s) " M "\n", &date[0], (tv.tv_usec/1000), Ql_levelname( L ), __FILE__, __LINE__, Ql_clean_errno(), ##__VA_ARGS__); \
 	} else { \
-		fprintf(stderr, "%s.%03dZ [%s] (%s:%d) " M "\n", &date[0], (tv.tv_usec/1000), ql_levelname( L ), __FILE__, __LINE__, ##__VA_ARGS__); \
+		fprintf(stderr, "%s.%03dZ [%s] (%s:%d) " M "\n", &date[0], (tv.tv_usec/1000), Ql_levelname( L ), __FILE__, __LINE__, ##__VA_ARGS__); \
 	} \
 } \
 
-#define ql_log_err(M, ...) if ( LOG_ERROR <= loglevel ) ql_logline(LOG_ERROR, 1, M , ##__VA_ARGS__)
+#define Ql_log_err(M, ...) if ( LOG_ERROR <= loglevel ) Ql_logline(LOG_ERROR, 1, M , ##__VA_ARGS__)
 
-#define ql_log_warn(M, ...) if ( LOG_WARN <= loglevel ) ql_logline(LOG_WARN, 1, M , ##__VA_ARGS__)
+#define Ql_log_warn(M, ...) if ( LOG_WARN <= loglevel ) Ql_logline(LOG_WARN, 1, M , ##__VA_ARGS__)
 
-#define ql_log_info(M, ...) if ( LOG_INFO <= loglevel ) ql_logline(LOG_INFO, 0, M , ##__VA_ARGS__)
+#define Ql_log_info(M, ...) if ( LOG_INFO <= loglevel ) Ql_logline(LOG_INFO, 0, M , ##__VA_ARGS__)
 
-#define ql_check(A, M, ...) if(!(A)) { ql_log_err(M, ##__VA_ARGS__); errno=0; goto ql_error; }
+#define Ql_check(A, M, ...) if(!(A)) { Ql_log_err(M, ##__VA_ARGS__); errno=0; goto Ql_error; }
 
-#define ql_sentinel(M, ...)  { ql_log_err(M, ##__VA_ARGS__); errno=0; goto ql_error; }
+#define Ql_sentinel(M, ...)  { Ql_log_err(M, ##__VA_ARGS__); errno=0; goto Ql_error; }
 
-#define ql_check_mem(A) ql_check((A), "Out of memory.")
+#define Ql_check_mem(A) Ql_check((A), "Out of memory.")
 
-#define ql_check_debug(A, M, ...) if(!(A)) { ql_debug(M, ##__VA_ARGS__); errno=0; goto error; }
+#define Ql_check_debug(A, M, ...) if(!(A)) { ql_debug(M, ##__VA_ARGS__); errno=0; goto Ql_error; }
 
 END_EXTERN_C
 
