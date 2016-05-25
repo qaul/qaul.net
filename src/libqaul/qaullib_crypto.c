a040a60035727fd470add5038c90f9c8ce78695b
MathJud <jud@wachter-jud.net>
2016-05-25 16:18:48 +0200

    Logging: comment experimental logging out due to windows build failure

--- src/libqaul/qaullib_crypto.c
+++ src/libqaul/qaullib_crypto.c
@@ -6,7 +6,7 @@
 #include "qaullib_private.h"
 #include "qaullib_crypto.h"
 #include "mbedtls/sha1.h"
-#include "qaullib/logging.h"
+//#include "qaullib/logging.h"
 
 
 // ------------------------------------------------------------
@@ -18,12 +18,12 @@ int Ql_sha1_file(char *filepath, unsigned char *hash)
 	mbedtls_sha1_context ctx;
     unsigned char buf[1024];
 
-    Ql_log_debug("Ql_sha1_file %s", filepath);
+//    Ql_log_debug("Ql_sha1_file %s", filepath);
 
     ret = 1;
     if((f = fopen(filepath, "rb")) == NULL)
     {
-    	Ql_log_err("Ql_sha1_file failed to open file: %s\n", filepath);
+//    	Ql_log_err("Ql_sha1_file failed to open file: %s\n", filepath);
     	return 0;
     }
 
@@ -37,7 +37,7 @@ int Ql_sha1_file(char *filepath, unsigned char *hash)
 
     if(ferror(f) != 0)
     {
-    	Ql_log_err("Ql_sha1_file file error on file: %s\n", filepath);
+//    	Ql_log_err("Ql_sha1_file file error on file: %s\n", filepath);
     	ret = 0;
         goto cleanup;
     }
@@ -56,7 +56,7 @@ int Ql_HashToString(unsigned char *hash, char *string)
 {
 	int i;
 
-	Ql_log_debug("Ql_HashToString");
+//	Ql_log_debug("Ql_HashToString");
 
 	// FIXME: big-endian / little-endian
 	for(i=0;i<MAX_HASH_LEN;i++)
