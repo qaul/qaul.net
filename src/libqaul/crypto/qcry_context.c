546cdd7e26137d0de7a0fb8cc6354eb5741784c8
Katharina Fey <kookie@spacekookie.de>
2016-06-03 00:14:33 +0200

    Re-importing mbedtls trunk for development purposes. See issue #2

--- src/libqaul/crypto/qcry_context.c
+++ src/libqaul/crypto/qcry_context.c
@@ -1,10 +1,3 @@
 #include "qcry_context.h"
 #include <stdio.h>
-
-#include "mbedtls/ecdh.h"
-
-int qcry_context_init(char *username, char *foobar)
-{
-    printf("This is a dummy function in the crypto code...\n");
-    return 0;
-}
+#include <string.h>
