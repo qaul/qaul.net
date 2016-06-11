db9a521a5904ff3aecc7302b80f3c52f3d487d9d
Katharina Sabel <sabel.katharina@gmail.com>
2016-06-11 23:21:55 +0200

    Updating a load of files to geneate keys!

--- src/libqaul/qcry_wrapper.c
+++ src/libqaul/qcry_wrapper.c
@@ -4,6 +4,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <malloc.h>
+#include <mbedtls/cipher.h>
+#include <mbedtls/platform.h>
 
 // Include public key crypto headers
 #include "mbedtls/pk.h"
@@ -12,33 +14,64 @@
 
 int qcry_devel_init()
 {
-    printf("Wrapper INIT\n");
+    int ret = 0, i, k;
+    unsigned char buf[1024];
 
-    printf("This example will do public key cryptography with RSA...\n");
-
-    char *to_encrypt = "SUPER SECRET";
-    size_t to_encrypt_len = strlen(to_encrypt);
-
-    /******************************************
-     * INITIALISE THE CORE CRYPTO FUNCTIONS
-     *
-     *****************************************/
-
-    mbedtls_ctr_drbg_context ctr_drbg;
     mbedtls_entropy_context entropy;
+    mbedtls_ctr_drbg_context ctr_drbg;
 
-    size_t key_size = 32;
-    unsigned char key[key_size];
-    int ret;
-
-    unsigned char *personal = "libqaul is aaaaawesome!";
-
-    mbedtls_entropy_init(&entropy);
     mbedtls_ctr_drbg_init(&ctr_drbg);
+    mbedtls_entropy_init(&entropy);
+    printf("Context initialisation...done\n");
 
-    mbedtls_ctr_drbg_random(&ctr_drbg, key, key_size);
-    printf("Return value is %d. Good or bad?\n", ret);
-    return 0;
+    ret = mbedtls_ctr_drbg_seed( &ctr_drbg, mbedtls_entropy_func, &entropy, (const unsigned char *) "RANDOM_GEN", 10 );
+    printf("Seed generation...done\n");
+
+    mbedtls_ctr_drbg_set_prediction_resistance(&ctr_drbg, MBEDTLS_CTR_DRBG_PR_ON);
+    printf("Prediction resistance...done\n");
+
+    ret = mbedtls_ctr_drbg_random(&ctr_drbg, buf, sizeof(buf));
+    printf("Random buffer flush: %s", buf);
+
+    /** Set return value to 0 so we are sure we succeeded */
+    ret = 0;
+
+    /** Cleanup before shutting down */
+    exit:
+    mbedtls_entropy_free(&entropy);
+    mbedtls_ctr_drbg_free(&ctr_drbg);
+
+    return( ret );
+}
+
+
+//    printf("Wrapper INIT\n");
+//
+//    printf("This example will do public key cryptography with RSA...\n");
+//
+//    char *to_encrypt = "SUPER SECRET";
+//    size_t to_encrypt_len = strlen(to_encrypt);
+//
+//    /******************************************
+//     * INITIALISE THE CORE CRYPTO FUNCTIONS
+//     *
+//     *****************************************/
+//
+//    mbedtls_ctr_drbg_context ctr_drbg;
+//    mbedtls_entropy_context entropy;
+//
+//    size_t key_size = 32;
+//    unsigned char key[key_size];
+//    int ret;
+//
+//    unsigned char *personal = "libqaul is aaaaawesome!";
+//
+//    mbedtls_entropy_init(&entropy);
+//    mbedtls_ctr_drbg_init(&ctr_drbg);
+//
+//    mbedtls_ctr_drbg_random(&ctr_drbg, key, key_size);
+//    printf("Return value is %d. Good or bad?\n", ret);
+//    return 0;
 
 //    int ret = 0;
 //    mbedtls_pk_context pk;
@@ -88,5 +121,3 @@ int qcry_devel_init()
 //    exit:
 //    printf("Exiting because of an error!\n");
 //    return 255;
-
-    }
