a235cfeb0079c4c3b8cefb89e301b446e30c7900
Katharina Sabel <sabel.katharina@gmail.com>
2016-06-19 18:42:04 +0200

    Macros are soooo awesome :)

--- src/libqaul/crypto/qcry_context.c
+++ src/libqaul/crypto/qcry_context.c
@@ -2,8 +2,8 @@
 
 #include <memory.h>
 #include <malloc.h>
+#include <zconf.h>
 
-#define MAGICK_NO 3
 
 int qcry_context_init(qcry_usr_ctx *ctx, unsigned char *usr_name, qcry_ciph_t ciph_t)
 {
@@ -11,55 +11,92 @@ int qcry_context_init(qcry_usr_ctx *ctx, unsigned char *usr_name, qcry_ciph_t ci
     ctx->usr_name = usr_name;
     ctx->ciph_t = ciph_t;
 
-    ctx->pk_ctx = (mbedtls_pk_context*) malloc(sizeof(mbedtls_pk_context));
-    if(!ctx->pk_ctx) return QCRY_STATUS_MALLOC_FAIL;
-    mbedtls_pk_init(ctx->pk_ctx);
+    /* Prepare space for future crypto targets */
+    ctx->trgts = (qcry_trgt_t*) calloc(sizeof(qcry_trgt_t), TRGT_C);
+    ctx->usd_trgt = 0;
+    ctx->max_trgt = TRGT_C;
 
+    /* Signal we're done */
     ctx->mgno = MAGICK_NO;
-}
-
-int qcry_context_attach_keys(qcry_usr_ctx *ctx, unsigned char *usr_key_pri, unsigned char *usr_key_pub)
-{
-    CHECK_SANE
-
-    size_t pri_len = strlen(usr_key_pri);
-    size_t pub_len = strlen(usr_key_pub);
-
-    if(pri_len != QCRY_KEY_LEN[ctx->ciph_t] || pub_len != QCRY_KEY_LEN[ctx->ciph_t] ) {
-        return QCRY_STATUS_INVALID_KEYS;
-    }
-
-    ctx->usr_key_pri = (unsigned char *) calloc(sizeof(unsigned char), QCRY_KEY_LEN[ctx->ciph_t]);
-    if(!ctx->usr_key_pri) return QCRY_STATUS_MALLOC_FAIL;
-
-    ctx->usr_key_pub = (unsigned char *) calloc(sizeof(unsigned char), QCRY_KEY_LEN[ctx->ciph_t]);
-    if(!ctx->usr_key_pri) {
-        free(ctx->usr_key_pri);
-        return QCRY_STATUS_MALLOC_FAIL;
-    }
-
     return QCRY_STATUS_OK;
 }
 
-int qcry_encrypt_ictx(qcry_usr_ctx *ctx, const char *msg, size_t ilen, unsigned char *(*ciph))
+int qcry_context_prik_attach(qcry_usr_ctx *ctx, const unsigned char *usr_key_pri)
 {
-    /* Check context is valid */
     CHECK_SANE
 
-    /* Then check our keys are valid */
-    CHECK_KEYS
+    /* Make sure our data is valid */
+    size_t pri_len = strlen(usr_key_pri);
+    if(pri_len != QCRY_KEY_LEN[ctx->ciph_t]) return QCRY_STATUS_INVALID_KEYS;
 
-    mbedtls_pk_get_len(ctx);
+    /* Allocate memory for our key */
+    ctx->usr_key_pri = (unsigned char *) calloc(sizeof(unsigned char), QCRY_KEY_LEN[ctx->ciph_t]);
+    if(!ctx->usr_key_pri) return QCRY_STATUS_MALLOC_FAIL;
 
-    mbedtls_pk_encrypt(ctx->pk_ctx, msg, ilen, *ciph, 15);
+    /* Copy key from const buffer and exit */
+    memcpy(ctx->usr_key_pri, usr_key_pri, pri_len);
+    ctx->use_ctr = 0;
+    return QCRY_STATUS_OK;
 }
 
-int qcry_decrypt_ictx(qcry_usr_ctx *ctx, const char *ciph, size_t ilen, unsigned char *(*msg))
+int qcry_context_prik_detach(qcry_usr_ctx *ctx)
 {
-    /* Check context is valid */
     CHECK_SANE
 
-    /* Then check our keys are valid */
-    CHECK_KEYS
+    int ctr_t;
+    while(ctx->use_ctr != 0) {
+        ctr_t += TIME_SLEEP;
+        sleep(TIME_SLEEP);
 
+        if(ctr_t >= MAX_TIMEOUT) {
+            return QCRY_STATUS_KEY_BUSY;
+        }
+    }
+
+    /* Then simply free the key */
+    free(ctx->usr_key_pri);
+    return QCRY_STATUS_OK;
+}
+
+int qcry_context_add_trgt(qcry_usr_ctx *ctx, const qcry_trgt_t *trgt, qcry_ciph_t ciph_t, unsigned int *trgt_no)
+{
+    CHECK_SANE
+
+    /* Check the key length for our target */
+    size_t pri_len;
+    if(ciph_t == AES256) pri_len = strlen(trgt->data.sk->sh_key_pri);
+    else                 pri_len = strlen(trgt->data.pk->usr_key_pub);
+    if(QCRY_KEY_LEN[pri_len] != ciph_t) return QCRY_STATUS_INVALID_TARGET;
+
+    /* Increases the target buffer if needs be */
+    CHECK_BUFFER(qcry_trgt_t* , ctx->trgts, ctx->max_trgt, ctx->usd_trgt)
+
+    /* Copy in target data from const source */
+    ctx->usd_trgt++;
+    memcpy(ctx->trgts[ctx->usd_trgt], trgt, sizeof(qcry_trgt_t));
+    *trgt_no = ctx->usd_trgt;
+
+    if(ciph_t == AES256) {
+        ctx->trgts[ctx->usd_trgt]->mbed_ctx.sk_ctx = (mbedtls_aes_context*) malloc(sizeof(mbedtls_aes_context));
+        mbedtls_aes_init(ctx->trgts[ctx->usd_trgt]->mbed_ctx.sk_ctx);
+    } else {
+        ctx->trgts[ctx->usd_trgt]->mbed_ctx.pk_ctx = (mbedtls_pk_context*) malloc(sizeof(mbedtls_pk_context));
+        mbedtls_pk_init(ctx->trgts[ctx->usd_trgt]->mbed_ctx.pk_ctx);
+    }
+
+    /* Signal back all clear */
+    return QCRY_STATUS_OK;
+}
+
+int qcry_context_remove_trgt(qcry_usr_ctx *ctx, unsigned int *trgt_no)
+{
+    CHECK_SANE
+
+    if(!ctx->trgts[*trgt_no]) return QCRY_STATUS_INVALID_TARGET;
+
+    /* Clear target memory and resize buffer if needed */
+    CLEAR_TARGET(ctx->ciph_t, ctx->trgts[*trgt_no])
+    CHECK_BUFFER(qcry_trgt_t* , ctx->trgts, ctx->max_trgt, ctx->usd_trgt)
+
+    return QCRY_STATUS_OK;
 }
--- src/libqaul/crypto/qcry_context.h
+++ src/libqaul/crypto/qcry_context.h
@@ -1,4 +1,4 @@
-*
+/*
  * qaul.net is free software
  * licensed under GPL (version 3)
  */
@@ -6,6 +6,7 @@
 #ifndef _QCRY_CONTEXT_
 #define _QCRY_CONTEXT_
 
+#include <mbedtls/aes.h>
 #include "qcry_helper.h"
 #include "mbedtls/pk.h"
 
@@ -19,7 +20,6 @@ typedef enum {
 
 /* Data required to do public key crypto*/
 typedef struct {
-    mbedtls_pk_context  *pk_ctx;
     unsigned char       *usr_key_pub;
     unsigned char       *fp;
     short               mgno;
@@ -32,25 +32,34 @@ typedef struct {
 
 /** Combined structure to hold unionised data */
 typedef struct {
-    qcry_ciph_t type;
-
     union data
     {
         qcry_sk_taret   *sk;
         qcry_pk_target  *pk;
     } data;
 
+    union mbed_ctx {
+        mbedtls_pk_context  *pk_ctx;
+        mbedtls_aes_context *sk_ctx;
+    } mbed_ctx;
+
 } qcry_trgt_t;
 
 
 typedef struct {
+
+    /* Store private key and key context */
     unsigned char       *usr_key_pri;
+    unsigned int        use_ctr;
+    qcry_ciph_t         ciph_t;
+
+    /* Metadata about a uesr */
     unsigned char       *usr_name;
     unsigned char       *usr_fp;
     short               mgno;
 
     /* Target data for this context */
-    qcry_trgt_t         *trgts;
+    qcry_trgt_t         **trgts;
     unsigned int        usd_trgt;
     unsigned int        max_trgt;
 } qcry_usr_ctx;
@@ -64,15 +73,31 @@ typedef struct {
             return QCRY_STATUS_INVALID_KEYS; \
         }
 
+#define CLEAR_TARGET(ciph_t, trgt) \
+    { if(ciph_t == AES256) { mbedtls_aes_free(trgt->mbed_ctx.sk_ctx); \
+        free(trgt->mbed_ctx.sk_ctx); free(trgt->data.sk->sh_key_pri); \
+    free(trgt->data.sk); } else { mbedtls_pk_free(trgt->mbed_ctx.pk_ctx); \
+        free(trgt->mbed_ctx.pk_ctx); free(trgt->data.pk->usr_key_pub); \
+        free(trgt->data.pk->fp); free(trgt->data.pk); } free(trgt); }
+
 /**
  * Initialises context for a username and a cipher type
  */
 int qcry_context_init(qcry_usr_ctx *ctx, unsigned char *usr_name, qcry_ciph_t ciph_t);
 
 /**
- * Attaches a public/ private keypair to a context
+ * Attaches a private key to a context. The key is validated and length matched
  */
-int qcry_context_attach_keys(qcry_usr_ctx *ctx, unsigned char *usr_key_pri, unsigned char *usr_key_pub);
+int qcry_context_prik_attach(qcry_usr_ctx *ctx, const unsigned char *usr_key_pri);
+
+/**
+ * Detaches a private key from a context for whatever reason.
+ */
+int qcry_context_prik_detach(qcry_usr_ctx *ctx);
+
+int qcry_context_add_trgt(qcry_usr_ctx *ctx, const qcry_trgt_t *trgt, qcry_ciph_t ciph_t, unsigned int *trgt_no);
+
+int qcry_context_remove_trgt(qcry_usr_ctx *ctx, unsigned int *trgt_no);
 
 int qcry_encrypt_ictx(qcry_usr_ctx *ctx, const char *msg, size_t ilen, unsigned char *(*ciph));
 
