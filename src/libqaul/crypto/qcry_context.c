0783f50df44617db0be607ab3e5e2c6d020b5bab
Katharina Sabel <sabel.katharina@gmail.com>
2016-06-19 17:38:10 +0200

    Massive changes to how the crypto module holds contexts and handles multi target encryption.

--- src/libqaul/crypto/qcry_context.c
+++ src/libqaul/crypto/qcry_context.c
@@ -1,3 +1,65 @@
 #include "qcry_context.h"
-#include <stdio.h>
-#include <string.h>
+
+#include <memory.h>
+#include <malloc.h>
+
+#define MAGICK_NO 3
+
+int qcry_context_init(qcry_usr_ctx *ctx, unsigned char *usr_name, qcry_ciph_t ciph_t)
+{
+    memset(ctx, 0, sizeof(qcry_usr_ctx));
+    ctx->usr_name = usr_name;
+    ctx->ciph_t = ciph_t;
+
+    ctx->pk_ctx = (mbedtls_pk_context*) malloc(sizeof(mbedtls_pk_context));
+    if(!ctx->pk_ctx) return QCRY_STATUS_MALLOC_FAIL;
+    mbedtls_pk_init(ctx->pk_ctx);
+
+    ctx->mgno = MAGICK_NO;
+}
+
+int qcry_context_attach_keys(qcry_usr_ctx *ctx, unsigned char *usr_key_pri, unsigned char *usr_key_pub)
+{
+    CHECK_SANE
+
+    size_t pri_len = strlen(usr_key_pri);
+    size_t pub_len = strlen(usr_key_pub);
+
+    if(pri_len != QCRY_KEY_LEN[ctx->ciph_t] || pub_len != QCRY_KEY_LEN[ctx->ciph_t] ) {
+        return QCRY_STATUS_INVALID_KEYS;
+    }
+
+    ctx->usr_key_pri = (unsigned char *) calloc(sizeof(unsigned char), QCRY_KEY_LEN[ctx->ciph_t]);
+    if(!ctx->usr_key_pri) return QCRY_STATUS_MALLOC_FAIL;
+
+    ctx->usr_key_pub = (unsigned char *) calloc(sizeof(unsigned char), QCRY_KEY_LEN[ctx->ciph_t]);
+    if(!ctx->usr_key_pri) {
+        free(ctx->usr_key_pri);
+        return QCRY_STATUS_MALLOC_FAIL;
+    }
+
+    return QCRY_STATUS_OK;
+}
+
+int qcry_encrypt_ictx(qcry_usr_ctx *ctx, const char *msg, size_t ilen, unsigned char *(*ciph))
+{
+    /* Check context is valid */
+    CHECK_SANE
+
+    /* Then check our keys are valid */
+    CHECK_KEYS
+
+    mbedtls_pk_get_len(ctx);
+
+    mbedtls_pk_encrypt(ctx->pk_ctx, msg, ilen, *ciph, 15);
+}
+
+int qcry_decrypt_ictx(qcry_usr_ctx *ctx, const char *ciph, size_t ilen, unsigned char *(*msg))
+{
+    /* Check context is valid */
+    CHECK_SANE
+
+    /* Then check our keys are valid */
+    CHECK_KEYS
+
+}
--- src/libqaul/crypto/qcry_context.h
+++ src/libqaul/crypto/qcry_context.h
@@ -1,4 +1,4 @@
-/*
+*
  * qaul.net is free software
  * licensed under GPL (version 3)
  */
@@ -6,9 +6,77 @@
 #ifndef _QCRY_CONTEXT_
 #define _QCRY_CONTEXT_
 
-int qcry_context_init(char *username, char *foobar);
+#include "qcry_helper.h"
+#include "mbedtls/pk.h"
 
-char * qcry_context_getkey(char *username, char *token);
+/** Describes the context in which a crypto context is held */
+typedef enum {
+    RSA_ECDSA = 0,
+    ECC_ECDSA = 1,
+    AES256 = 2
+
+} qcry_ciph_t;
+
+/* Data required to do public key crypto*/
+typedef struct {
+    mbedtls_pk_context  *pk_ctx;
+    unsigned char       *usr_key_pub;
+    unsigned char       *fp;
+    short               mgno;
+} qcry_pk_target;
+
+/* Data required to do sym key crypto*/
+typedef struct {
+    unsigned char      *sh_key_pri;
+} qcry_sk_taret;
+
+/** Combined structure to hold unionised data */
+typedef struct {
+    qcry_ciph_t type;
+
+    union data
+    {
+        qcry_sk_taret   *sk;
+        qcry_pk_target  *pk;
+    } data;
+
+} qcry_trgt_t;
+
+
+typedef struct {
+    unsigned char       *usr_key_pri;
+    unsigned char       *usr_name;
+    unsigned char       *usr_fp;
+    short               mgno;
+
+    /* Target data for this context */
+    qcry_trgt_t         *trgts;
+    unsigned int        usd_trgt;
+    unsigned int        max_trgt;
+} qcry_usr_ctx;
+
+#define CHECK_SANE \
+    if(ctx->mgno != 3) return QCRY_STATUS_CTX_INVALID;
+
+#define CHECK_KEYS \
+    if(!(ctx->ciph_t == AES256 && ctx->usr_key_pri) || \
+        !((ctx->ciph_t == RSA_ECDSA || ctx->ciph_t == ECC_ECDSA) && (ctx->usr_key_pub && ctx->usr_key_pri))) { \
+            return QCRY_STATUS_INVALID_KEYS; \
+        }
+
+/**
+ * Initialises context for a username and a cipher type
+ */
+int qcry_context_init(qcry_usr_ctx *ctx, unsigned char *usr_name, qcry_ciph_t ciph_t);
+
+/**
+ * Attaches a public/ private keypair to a context
+ */
+int qcry_context_attach_keys(qcry_usr_ctx *ctx, unsigned char *usr_key_pri, unsigned char *usr_key_pub);
+
+int qcry_encrypt_ictx(qcry_usr_ctx *ctx, const char *msg, size_t ilen, unsigned char *(*ciph));
+
+int qcry_decrypt_ictx(qcry_usr_ctx *ctx, const char *ciph, size_t ilen, unsigned char *(*msg));
 
 #endif // _QCRY_CONTEXT_
 
