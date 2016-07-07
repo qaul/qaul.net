daf9778acfa3e92386d45a6030cba8051a908bd6
Tristan B. Kildaire <deavmi@users.noreply.github.com>
2016-07-07 15:24:10 +0200

    Update main.c

--- src/client/cli/main.c
+++ src/client/cli/main.c
@@ -33,7 +33,7 @@ int main(int argc, char *argv[])
 		return EXIT_FAILURE;
 	}
 	cCurrentPath[sizeof(cCurrentPath) - 1] = '\0';
-	fprintf(stderr,"The current working directory is %s\n", cCurrentPath);
+	fprintf(stderr,"The current working directory is '%s'\n", cCurrentPath);
 
         snprintf(prefix, FILENAME_MAX, "%s/lib/qaul", QAUL_ROOT_PATH);
 
@@ -42,14 +42,14 @@ int main(int argc, char *argv[])
 	qaul_conf_debug = 1;
 
 	if(!Qaullib_WebserverStart())
-		fprintf(stderr,"Webserver startup failed\n");
+		fprintf(stderr,"Webserver startup failed!\n");
 	Qaullib_ConfigStart();
 
 	fprintf(stderr,"----------------------------------------------------\n");
 	fprintf(stderr," config started\n");
-	fprintf(stderr,"----------------------------------------------------\n");
+	fprintf(stderr,"----------------------------------------------------\n\n");
 	// The invoking of Qaullib_GetIP() is mandatory to load the IP.
-	fprintf(stderr,"IP: %s\n", Qaullib_GetIP());
+	fprintf(stderr,"IP: %s\n\n", Qaullib_GetIP());
 
 	// wait until user name is set
 	int username_flag = 0;
@@ -91,11 +91,11 @@ int main(int argc, char *argv[])
 		// get event
 		int event = Qaullib_TimedCheckAppEvent();
 		if(event == QAUL_EVENT_QUIT)
-			fprintf(stderr,"quit app\n");
+			fprintf(stderr,"Quit app\n");
 		else if(event == QAUL_EVENT_CHOOSEFILE)
-			fprintf(stderr,"open file chooser\n");
+			fprintf(stderr,"Open file chooser\n");
 		else if(event == QAUL_EVENT_OPENFILE)
-			fprintf(stderr,"open file\n");
+			fprintf(stderr,"Open file\n");
 
 		// check sockets
 		if(socketCounter >= 10)
