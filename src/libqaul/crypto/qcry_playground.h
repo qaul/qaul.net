a0dad954a2d6760d8dde04416b7fe4dca0be3814
Katharina Sabel <sabel.katharina@gmail.com>
2016-07-27 15:51:29 +0200

    Adding new crypto functions to:  - Create keys  - Write keys  - Sign messages
    These functions will be moved into the arbiter API after the've been sufficiently tested!

---
+++ src/libqaul/crypto/qcry_playground.h
@@ -0,0 +1,29 @@
+//
+// Created by spacekookie on 18/07/16.
+//
+
+#ifndef QAUL_QCRY_PLAYGROUND_H
+#define QAUL_QCRY_PLAYGROUND_H
+
+
+#include "mbedtls/entropy.h"
+#include "mbedtls/ctr_drbg.h"
+#include "mbedtls/bignum.h"
+#include "mbedtls/x509.h"
+#include "mbedtls/rsa.h"
+
+#include <stdio.h>
+#include <string.h>
+#include <mbedtls/error.h>
+#include <mbedtls/platform.h>
+
+
+int qcry_key_generate(mbedtls_pk_context **key);
+
+int qcry_generate_key();
+
+int sign_msg(mbedtls_pk_context *key, const char *msgfile);
+
+int qcry_sign_with_key();
+
+#endif //QAUL_QCRY_PLAYGROUND_H
