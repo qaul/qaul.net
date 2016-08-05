ed1af2bf09f138b7b89b3d04ac954f4834702c60
Katharina Fey <kookie@spacekookie.de>
2016-08-05 16:38:13 +0200

    - Adding base64 encoding to qcry_hashing.  - Fixing several issues with the implementation that would segfault in certain scenarios.

--- src/libqaul/include/qaullib/qcry_hashing.h
+++ src/libqaul/include/qaullib/qcry_hashing.h
@@ -5,14 +5,14 @@
 
 struct qcry_hash_ctx {
     /** Things the user should set */
-    unsigned short          hash;
-    unsigned short          encoding;
+    unsigned int            hash;
+    unsigned int            encoding;
     void                    *md_ctx;
-    unsigned char           *salt;
+    char                    *salt;
 
     /** Some things required to run the show */
     char                    *curr_bfr;
-    int                     bfr_s, bfr_occ;
+    size_t                  bfr_s, bfr_occ;
     short                   mgno;
 };
 
@@ -20,12 +20,13 @@ struct qcry_hash_ctx {
 #define QCRY_HASH_SHA256    (1 << 1)
 #define QCRY_HASH_SHA512    (1 << 2)
 
+#define QCRY_SHA512_LENGTH  64
+#define QCRY_SHA256_LENGTH  32
+
 /** Encodings we support. Base64 requires additional import */
 #define QCRY_HASH_BINARY    (1 << 1)
 #define QCRY_HASH_HEX       (1 << 2)
 #define QCRY_HASH_BASE64    (1 << 3)
-
-// ...
 
 /**
  * Create and delete a hashing context, initialised for a hash function and a salt
@@ -35,6 +36,7 @@ int qcry_hashing_free(struct qcry_hash_ctx *ctx);
 
 /** Append a message to the buffer **/
 int qcry_hashing_append(struct qcry_hash_ctx *ctx, const char *msg);
+int qcry_hashing_clear(struct qcry_hash_ctx *ctx);
 
 /** Read the current state of the buffer. Can return NULL. Should only be used for debugging! **/
 const char *qcry_hashing_buffer(struct qcry_hash_ctx *ctx);
@@ -43,6 +45,6 @@ const char *qcry_hashing_buffer(struct qcry_hash_ctx *ctx);
 size_t qcry_hashing_getlen(unsigned int hash);
 
 /** Build the buffer to a hash and encode it into a return buffer */
-int qcry_hashing_build(struct qcry_hash_ctx *ctx, unsigned int encoding, unsigned char *(*buffer));
+int qcry_hashing_build(struct qcry_hash_ctx *ctx, unsigned int encoding, char *(*buffer));
 
 #endif //QAUL_QCRY_HASHING_H
