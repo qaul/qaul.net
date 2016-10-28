f5467c4fd42868c73fe1094eda7ea3c30e384cbf
saces <saces@qaul.net>
2016-10-28 21:03:14 +0200

    AStudio: adjust paths in cmake and doc

--- cmake/BuildAndroid.cmake
+++ cmake/BuildAndroid.cmake
@@ -18,29 +18,29 @@ if(NOT ANDROID_EABI)
 endif()
 
 add_custom_target(copy_android
-    ${CMAKE_COMMAND} -DSRC=${CMAKE_SOURCE_DIR}/android -DDEST=${CMAKE_BINARY_DIR} -P ${CMAKE_SOURCE_DIR}/cmake/FileCopy.cmake
+    ${CMAKE_COMMAND} -DSRC=${CMAKE_SOURCE_DIR}/android/app/src/main -DDEST=${CMAKE_BINARY_DIR}/android/app/src -P ${CMAKE_SOURCE_DIR}/cmake/FileCopy.cmake
     ${CMAKE_COMMAND} -DSRC=${CMAKE_SOURCE_DIR}/GUI/www -DDEST=${CMAKE_BINARY_DIR} -P ${CMAKE_SOURCE_DIR}/cmake/FileCopy.cmake
     ${CMAKE_COMMAND} -DSRC=${CMAKE_SOURCE_DIR}/GUI/files -DDEST=${CMAKE_BINARY_DIR} -P ${CMAKE_SOURCE_DIR}/cmake/FileCopy.cmake
 )
 
 add_custom_target(AndroidJNI
-                  NDK_PROJECT_PATH=${CMAKE_BINARY_DIR}/android ${NDK_ROOT}/ndk-build -C ${CMAKE_BINARY_DIR}/android
+                  NDK_PROJECT_PATH=${CMAKE_BINARY_DIR}/android/app/src/main ${NDK_ROOT}/ndk-build -C ${CMAKE_BINARY_DIR}/android/app/src/main
                   PJSIP_PATH=${CMAKE_BINARY_DIR}/third_party/pjsip/src/pjsip
                   EXTRALIB_PATH=${EXTRALIB_PATH}
                   LIBQAUL_SRC=${CMAKE_SOURCE_DIR}/src/libqaul
                   QAULCONFIG_INCLUDE=${CMAKE_BINARY_DIR}/include
 
-                  COMMAND ${CMAKE_COMMAND} -DSRC=${CMAKE_BINARY_DIR}/android/libs/armeabi/wificonfig -DDEST=${CMAKE_BINARY_DIR}/android/res/raw -P ${CMAKE_SOURCE_DIR}/cmake/FileCopy.cmake
-                  COMMAND ${CMAKE_COMMAND} -DSRC=${CMAKE_BINARY_DIR}/android/libs/armeabi/tether -DDEST=${CMAKE_BINARY_DIR}/android/res/raw -P ${CMAKE_SOURCE_DIR}/cmake/FileCopy.cmake
-                  COMMAND ${CMAKE_COMMAND} -DSRC=${EXTRALIB_PATH}/ifconfig -DDEST=${CMAKE_BINARY_DIR}/android/res/raw -P ${CMAKE_SOURCE_DIR}/cmake/FileCopy.cmake
-                  COMMAND ${CMAKE_COMMAND} -DSRC=${EXTRALIB_PATH}/iptables -DDEST=${CMAKE_BINARY_DIR}/android/res/raw -P ${CMAKE_SOURCE_DIR}/cmake/FileCopy.cmake
+                  COMMAND ${CMAKE_COMMAND} -DSRC=${CMAKE_BINARY_DIR}/android/app/src/main/libs/armeabi/wificonfig -DDEST=${CMAKE_BINARY_DIR}/android/res/raw -P ${CMAKE_SOURCE_DIR}/cmake/FileCopy.cmake
+                  COMMAND ${CMAKE_COMMAND} -DSRC=${CMAKE_BINARY_DIR}/android/app/src/main/libs/armeabi/tether -DDEST=${CMAKE_BINARY_DIR}/android/res/raw -P ${CMAKE_SOURCE_DIR}/cmake/FileCopy.cmake
+                  COMMAND ${CMAKE_COMMAND} -DSRC=${EXTRALIB_PATH}/ifconfig -DDEST=${CMAKE_BINARY_DIR}/android/app/src/main/res/raw -P ${CMAKE_SOURCE_DIR}/cmake/FileCopy.cmake
+                  COMMAND ${CMAKE_COMMAND} -DSRC=${EXTRALIB_PATH}/iptables -DDEST=${CMAKE_BINARY_DIR}/android/app/src/main/res/raw -P ${CMAKE_SOURCE_DIR}/cmake/FileCopy.cmake
                   DEPENDS olsr pjsip wt socat copy_android
                   WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/android)
 
-add_custom_target(AndroidUPDATE android update project -p ${CMAKE_BINARY_DIR}/android
+add_custom_target(AndroidUPDATE android update project -t android-17 -p ${CMAKE_BINARY_DIR}/android/app/src/main
                   DEPENDS AndroidJNI
-                  WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/android)
+                  WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/android/app/src/main)
 
-add_custom_target(AndroidAPK ALL ant debug
+add_custom_target(AndroidAPK ALL ant -Dsource.dir=java debug
                   DEPENDS AndroidUPDATE
-                  WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/android)
+                  WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/android/app/src/main)
