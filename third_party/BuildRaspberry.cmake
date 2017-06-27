aeecc4cd6b09ec021ac825f7883c885656db8c4a
Mathias Jud <jud@wachter-jud.net>
2017-06-27 09:45:04 +0200

    Raspberry: workaround for pjsip webrtc build failure
    pjsip webrtc build fails on Raspberry Pi and is therefore not built on
    this platform.
    related to issue #166

--- third_party/BuildRaspberry.cmake
+++ third_party/BuildRaspberry.cmake
@@ -8,7 +8,7 @@ ExternalProject_Add(pjsip
     BUILD_IN_SOURCE 1
 	PATCH_COMMAND patch -p1 -t -N -i ${CMAKE_CURRENT_SOURCE_DIR}/pjsipLittleEndian.patch
     COMMAND patch -p1 -t -N -i ${CMAKE_CURRENT_SOURCE_DIR}/pjsip.patch
-    CONFIGURE_COMMAND ./aconfigure --prefix=${CMAKE_INSTALL_PREFIX} --disable-ffmpeg --disable-ssl --disable-video
+    CONFIGURE_COMMAND ./aconfigure --prefix=${CMAKE_INSTALL_PREFIX} --disable-ffmpeg --disable-ssl --disable-video --disable-libwebrtc ${PJSIP_CONFIGURE_OPTIONS}
     BUILD_COMMAND make dep COMMAND make
     INSTALL_COMMAND ""
 )
