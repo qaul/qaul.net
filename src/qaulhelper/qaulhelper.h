bcc22f3c3af994f75bcee60a6dfbf1761bf51241
Mathias Jud <jud@wachter-jud.net>
2017-07-26 17:39:27 +0200

    qaulhelper: make validation independently usable, by creating own .h file
    qaulhelper has validation functions to validate user input strings.
    Those functions could be used in the qaullib too.
    To do that we put the function declarations into an own header file.

--- src/qaulhelper/qaulhelper.h
+++ src/qaulhelper/qaulhelper.h
@@ -32,6 +32,7 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <QaulConfig.h>
+#include "validate.h"
 
 
 /**
@@ -158,53 +159,3 @@ int set_dhcp (int argc, const char * argv[]);
 
 #endif // QAUL_PORT_OSX
 
-
-/**
- * validate IP argument
- */
-int validate_ip (const char* str);
-
-/**
- * validate interface argument
- */
-int validate_interface (const char* str);
-
-/**
- * validate service argument
- */
-int validate_service (const char* str);
-
-/**
- * validate essid argument
- */
-int validate_essid (const char* str);
-
-/**
- * validate service argument
- */
-int validate_bssid (const char* str);
-
-/**
- * validate number argument
- */
-int validate_number (const char* str);
-
-/**
- * validate path argument
- */
-int validate_path (const char* str);
-
-/**
- * check if the char is a number
- */
-int validate_char_number (char mychar);
-
-/**
- * check if the char is an ascii letter
- */
-int validate_char_letter (char mychar);
-
-/**
- * check char for problematic entities
- */
-int validate_char_problematic (char mychar);
