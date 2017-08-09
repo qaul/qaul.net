b841353f53b27bbdb88f225c78d3f917ce24ff5a
Katharina Fey <kookie@spacekookie.de>
2017-08-09 21:34:50 +0200

    Adding basic outline of userstore API

---
+++ src/libqaul/user/qluser_store.c
@@ -0,0 +1,18 @@
+
+
+
+
+/** Holds data about a node */
+typedef struct qluser_node_t {
+    char              *ip;
+    struct qluser_t   **identities;
+};
+
+/** Holds data about a user identity */
+typedef struct qluser_t {
+    const char *fp;
+    const char *pubkey;
+    char *name;
+    qluser_node_t *node;
+};
+
