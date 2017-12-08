bae7df35649f0a08e0855aee8fb80056c796297f
Katharina Fey <kookie@spacekookie.de>
2017-12-08 22:46:09 +0100

    Giving up on the concept of having all datastructures in a single file

---
+++ src/libqaul/include/qaul/qluser.h
@@ -0,0 +1,18 @@
+//
+// Created by spacekookie on 08.12.17.
+//
+
+#ifndef QAUL_QLUSER_H
+#define QAUL_QLUSER_H
+
+/**
+ * The central qaul user structure
+ *
+ *
+ */
+typedef struct ql_user {
+    char *username;
+    char *fingerprint;
+};
+
+#endif //QAUL_QLUSER_H
