bc96ad7ad08a7b3e032a4c7efe3fcf3c69bd005a
Katharina Fey <kookie@spacekookie.de>
2018-01-31 22:44:41 +0100

    Refactoring more files. Moving messaging and files to protocol. More license headers

--- src/libqaul/userstore/keystore.c
+++ src/libqaul/userstore/keystore.c
@@ -1,1 +1,6 @@
+/*
+ * qaul.net is free software
+ * licensed under GPL (version 3)
+ */
+
 #include "keystore.h"
--- src/libqaul/userstore/userstore.c
+++ src/libqaul/userstore/userstore.c
@@ -1,3 +1,8 @@
+/*
+ * qaul.net is free software
+ * licensed under GPL (version 3)
+ */
+
 #include "userstore.h"
 
 // #include <dirent.h>
