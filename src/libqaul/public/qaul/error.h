699714647d50b6dd914bc4217a09277ae27d2fb8
Katharina Fey <kookie@spacekookie.de>
2018-01-31 16:50:52 +0100

    Refactoring the API a little

--- src/libqaul/public/qaul/error.h
+++ src/libqaul/public/qaul/error.h
@@ -4,12 +4,27 @@
 
 typedef enum ql_error_t {
 
-    /* Generic errors */
-    SUCCESS = 0,
-    ERROR = 1,
-    FATAL = 2,
+    SUCCESS = 0,                    // Everything went okay
+    ERROR = 1,                      // An unknown error occured
+    FATAL = 2,                      // A fatal error occured
+    MEMORY_ALLOCATION_FAILED,       // Not enough memory
 
+    NOT_INITIALISED,                // The module wasn't initialised yet
+    ALREADY_INITIALISED,            // The module has already been initialised
 
-};
+    INVALID_PARAMETERS,             // The provided parameters were invalid
+
+    USER_ALREADY_EXISTS,
+    USER_DOES_NOT_EXIST,
+    INVALID_PASSPHRASE,
+
+    ALREADY_AUTHENTICATED,
+    INVALID_AUTHENTICATION,
+    NOT_AUTHENTICATED,
+
+    FILE_ALREADY_EXISTS,
+    FILE_DOES_NOT_EXIST,
+
+} ql_error_t;
 
 #endif //QAUL_ERROR_H
