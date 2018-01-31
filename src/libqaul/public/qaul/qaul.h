97a8391f402fc5aeb7ff4aa57099b9215552db08
Katharina Fey <kookie@spacekookie.de>
2018-01-31 16:50:52 +0100

    Refactoring the API a little

--- src/libqaul/public/qaul/qaul.h
+++ src/libqaul/public/qaul/qaul.h
@@ -1,5 +1,7 @@
 
 #include <glob.h>
+#include <qaul/error.h>
+#include <qaul/query.h>
 
 typedef struct qaul {
     int bla;
@@ -10,45 +12,46 @@ typedef struct ql_auth_token {
 
 };
 
-unsigned int ql_initialise(struct qaul **state);
 
-unsigned int ql_shutdown(struct qaul *state);
+ql_error_t ql_initialise(struct qaul **state);
 
-unsigned int ql_create_user(struct qaul *state, const char *username, const char *passphrase);
-unsigned int ql_delete_user(struct qaul *state, const char *username);
+ql_error_t ql_shutdown(struct qaul *state);
 
-unsigned int ql_login(struct qaul *state, const char *username, const char *passphrase, struct ql_auth_token **token);
-unsigned int ql_logout(struct qaul *state, struct ql_auth_token *token);
+ql_error_t ql_create_user(struct qaul *state, const char *username, const char *passphrase);
+ql_error_t ql_delete_user(struct qaul *state, const char *username);
+
+ql_error_t ql_login(struct qaul *state, const char *username, const char *passphrase, struct ql_auth_token **token);
+ql_error_t ql_logout(struct qaul *state, struct ql_auth_token *token);
 
 /** Get libary configuration */
-unsigned int ql_get_configuration(struct qaul *state, const char *key, void **value);
+ql_error_t ql_get_configuration(struct qaul *state, const char *key, void **value);
 
 /** Set library configuration */
-unsigned int ql_set_configuration(struct qaul *state, const char *key, void *value);
+ql_error_t ql_set_configuration(struct qaul *state, const char *key, void *value);
 
-unsigned int ql_get_users(struct qaul *state, struct ql_auth_token *token, struct ql_query *query, size_t *length, void **data);
-unsigned int ql_get_user_data(struct qaul *state, struct ql_auth_token *token, const char *key, void **value);
-unsigned int ql_set_user_data(struct qaul *state, struct ql_auth_token *token, const char *key, void *value);
+ql_error_t ql_get_users(struct qaul *state, struct ql_auth_token *token, struct ql_query *query, size_t *length, void **data);
+ql_error_t ql_get_user_data(struct qaul *state, struct ql_auth_token *token, const char *key, void **value);
+ql_error_t ql_set_user_data(struct qaul *state, struct ql_auth_token *token, const char *key, void *value);
 
-unsigned int ql_get_messages(struct qaul *state, struct ql_auth_token *token, struct ql_query *query, size_t *length, void **data);
-unsigned int ql_send_message(struct qaul *state, struct ql_auth_token *token, const char *recipient, const char *message);
+ql_error_t ql_get_messages(struct qaul *state, struct ql_auth_token *token, struct ql_query *query, size_t *length, void **data);
+ql_error_t ql_send_message(struct qaul *state, struct ql_auth_token *token, const char *recipient, const char *message);
 
-unsigned int ql_get_all_files(struct qaul *state, struct ql_auth_token *token, struct ql_query *query, size_t *length, void **data);
-unsigned int ql_get_file(struct qaul *state, struct ql_auth_token *token, const char *name);
-unsigned int ql_get_file_meta(struct qaul *state, struct ql_auth_token *token, const char *name, void **data);
+ql_error_t ql_get_all_files(struct qaul *state, struct ql_auth_token *token, struct ql_query *query, size_t *length, void **data);
+ql_error_t ql_get_file(struct qaul *state, struct ql_auth_token *token, const char *name);
+ql_error_t ql_get_file_meta(struct qaul *state, struct ql_auth_token *token, const char *name, void **data);
 
-unsigned int ql_add_file(struct qaul *state, struct ql_auth_token *token, const char *id);
-unsigned int ql_delete_file(struct qaul *state, struct ql_auth_token *token, const char *id);
-unsigned int ql_download_file(struct qaul *state, struct ql_auth_token *token, const char *id);
+ql_error_t ql_add_file(struct qaul *state, struct ql_auth_token *token, const char *id);
+ql_error_t ql_delete_file(struct qaul *state, struct ql_auth_token *token, const char *id);
+ql_error_t ql_download_file(struct qaul *state, struct ql_auth_token *token, const char *id);
 
-unsigned int ql_init_call(struct qaul *state, const char *username);
-unsigned int ql_end_call(struct qaul *state, const char *username);
-unsigned int ql_accept_call(struct qaul *state);
-unsigned int ql_reject_call(struct qaul *state);
+ql_error_t ql_init_call(struct qaul *state, const char *username);
+ql_error_t ql_end_call(struct qaul *state, const char *username);
+ql_error_t ql_accept_call(struct qaul *state);
+ql_error_t ql_reject_call(struct qaul *state);
 
-unsigned int ql_get_network(struct qaul *state);
-unsigned int ql_configure_network(struct qaul *state);
-unsigned int ql_get_binaries(struct qaul *state);
+ql_error_t ql_get_network(struct qaul *state);
+ql_error_t ql_configure_network(struct qaul *state);
+ql_error_t ql_get_binaries(struct qaul *state);
 
 
 /*
