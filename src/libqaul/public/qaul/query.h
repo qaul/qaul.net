2e48ee4d134fe7ee3b3eab90b0cdd21569061724
Katharina Fey <kookie@spacekookie.de>
2018-01-31 16:43:53 +0100

    Adding API files to build chain

--- src/libqaul/public/qaul/query.h
+++ src/libqaul/public/qaul/query.h
@@ -1,4 +1,6 @@
 
+#include <glob.h>
+
 typedef enum ql_limit {
   
   STARTS_WITH, ENDS_WITH, EQUALS,
@@ -8,11 +10,11 @@ typedef enum ql_limit {
 
 typedef struct ql_query {
   char **name;
-  ql_limit *name_limits;
+  enum ql_limit *name_limits;
   short names;
 
   unsigned int *time;
-  ql_limit *time_limits;
+  enum ql_limit *time_limits;
   short times;
 
   size_t max_count;
