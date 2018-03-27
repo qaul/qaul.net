27ad0c3c47c03bfd0aa8ee3f39d13d56c419f9c9
Lukas Kohler <lukas.kohler@ontheblueplanet.com>
2018-03-27 23:50:05 +0200

    fix online CP

--- app/models/user.js
+++ app/models/user.js
@@ -1,7 +1,15 @@
 import DS from 'ember-data';
+import { computed, get } from '@ember/object';
+import moment from 'moment';
 
 export default DS.Model.extend({
   username: DS.attr('string'),
   trust: DS.attr('boolean'),
   lastSeen: DS.attr('date'),
+
+  online: computed('lastSeen', {
+    get() {
+        return moment.duration(moment().diff(get(this, 'lastSeen'))).asMinutes() < 2;
+    }
+  }),
 });
--- mirage/factories/user.js
+++ mirage/factories/user.js
@@ -1,15 +1,7 @@
 import { Factory, faker } from 'ember-cli-mirage';
-import { computed, get } from '@ember/object';
-import moment from 'moment';
 
 export default Factory.extend({
     username: () => faker.random.words(),
     trust: () => faker.random.boolean(),
     lastSeen: () => faker.date.recent(),
-
-    online: computed('lastSeen', {
-        get() {
-            return moment().diff(get(this, 'lastSeen')).asMinutes() < 2;
-        }
-    }),
 });
