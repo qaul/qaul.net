c7eedbeffcc7818418f63506f64e45900c4f082d
Mathias Jud <jud@wachter-jud.net>
2018-03-27 22:42:31 +0200

    model & mirage update

--- app/models/user.js
+++ app/models/user.js
@@ -3,4 +3,5 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   username: DS.attr('string'),
   trust: DS.attr('boolean'),
+  lastSeen: Ds.attr('date'),
 });
--- mirage/factories/user.js
+++ mirage/factories/user.js
@@ -1,4 +1,8 @@
 import { Factory } from 'ember-cli-mirage';
 
 export default Factory.extend({
+    username: () => faker.random.words(),
+    trust: () => faker.system.commonFileExt(),
+    online: () => faker.random.number(),
+    lastSeen: () => faker.date.recent(),
 });
