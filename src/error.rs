e7f866aa4dcaae3b0d682c36a2a9a42934b7bcc8
Katharina Fey <kookie@spacekookie.de>
2018-06-05 13:30:23 +0200

    We probably want to use failure for error handling

--- src/error.rs
+++ src/error.rs
@@ -1,6 +1,10 @@
 //! Error handling for routers
 
+/// Errors related to the routing core
 pub enum RouteError {
+    /// Indicates that a feature isn't implemented for a certain type
     FeatureNotSupported,
-    
+    ///
+    #[doc(hidden)]
+    __NonExhaustive,
 }
