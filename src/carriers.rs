f6079429d425ffcb8439d2bbd11d9a33d9f7da82
Katharina Fey <kookie@spacekookie.de>
2018-06-07 19:33:56 +0200

    Also supporting ipv6 addresses and changing header layout

--- src/carriers.rs
+++ src/carriers.rs
@@ -9,22 +9,26 @@ pub type Fingerprint = [u8; 8];
 /// A signature buffer (32 bytes – 256bit)
 pub type Signature = [u8; 32];
 
-/// An IPv4 address (TODO: Change this to IPv6 ?)
-pub type IPAddress = [u8; 4];
+/// An IP address, both for IPv4 and v6
+#[derive(Serialize, Deserialize)]
+pub enum IpAddress {
+    V4([u8; 4]),
+    V6([u8; 16])
+}
 
 /// A header contains package metadata and routing information
 #[derive(Serialize, Deserialize)]
 pub struct Header {
     /// A sender timestamp for ordering
-    timestamp: u64,
+    timestamp: u32,
     /// Signature of the message body (if applicable)
-    signature: Signature,
+    signature: Option<Signature>,
     /// Cryptographic sender fingerprint ID
     sender_fp: Fingerprint,
     /// Receiver fingerprint ID
     receivr_fp: Fingerprint,
     /// Routing target IP
-    target: IPAddress,
+    target: IpAddress,
 }
 
 /// Represents a base message sent via the qaul.net protocol
