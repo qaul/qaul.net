0c4785453dccc3631c0a34d1ac1f46c46601c163
Mathias Jud <jud@wachter-jud.net>
2018-08-22 13:40:08 +0200

    fixed errors that prevented ember from display the GUI

--- app/components/nav-bar/component.js
+++ app/components/nav-bar/component.js
@@ -1,5 +1,6 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-  localClassNames: ['nav-bar']
+    //localClassNames: ['nav-bar']
+    localClassNames: 'nav-bar'
 });
--- app/models/user.js
+++ app/models/user.js
@@ -3,21 +3,22 @@ import { computed, get } from '@ember/object';
 import moment from 'moment';
 import Owner from './user';
 
-export default Owner.extend({
-  fpToken: DS.attr('string'),
-  username: DS.attr('string'),
-  bio: DS.attr('string'),
-  trust: DS.attr('number'),
-  starred: DS.attr('boolean'),
-  // age or birthdate?
-  // avatar and token link?
-  gender: DS.attr('string'),
+//export default Owner.extend({
+export default DS.Model.extend({
+    fpToken: DS.attr('string'),
+    username: DS.attr('string'),
+    bio: DS.attr('string'),
+    trust: DS.attr('number'),
+    starred: DS.attr('boolean'),
+    // age or birthdate?
+    // avatar and token link?
+    gender: DS.attr('string'),
 
-  lastSeen: DS.attr('date'),
+    lastSeen: DS.attr('date'),
 
-  online: computed('lastSeen', {
-    get() {
-        return moment.duration(moment().diff(get(this, 'lastSeen'))).asMinutes() < 2;
-    }
-  }),
+    online: computed('lastSeen', {
+        get() {
+            return moment.duration(moment().diff(get(this, 'lastSeen'))).asMinutes() < 2;
+        }
+    }),
 });
