0f5a1a1bc7a4c887ff70e691d35190bbfdb04530
Lukas Kohler <lukas.kohler@ontheblueplanet.com>
2019-03-20 01:50:19 +0100

    cleanup crazy folder

--- qaul-webgui/qaul-web/app/components/nav-bar/component.js
+++
@@ -1,6 +0,0 @@
-import Component from '@ember/component';
-
-export default Component.extend({
-    //localClassNames: ['nav-bar']
-    localClassNames: 'nav-bar'
-});
--- qaul-webgui/qaul-web/app/models/interface.js
+++
@@ -1,7 +0,0 @@
-import DS from 'ember-data';
-import attr from 'ember-data/attr';
-
-export default DS.Model.extend({
-  name: attr('string'),
-  shared: attr('boolean'),
-});
