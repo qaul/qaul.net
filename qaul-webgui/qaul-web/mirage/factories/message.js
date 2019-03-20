06b922882fa5c513ce8786b73046070ab49894df
Lukas Kohler <lukas.kohler@ontheblueplanet.com>
2019-03-20 01:50:19 +0100

    cleanup crazy folder

--- qaul-webgui/qaul-web/mirage/factories/message.js
+++
@@ -1,7 +0,0 @@
-import { Factory, faker } from 'ember-cli-mirage';
-
-export default Factory.extend({
-  senderName: () => faker.name.firstName(),
-  content: () => faker.lorem.sentence(),
-  timestamp: () => faker.date.recent(),
-});
