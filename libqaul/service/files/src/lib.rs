79d85e6bac6447340605d4378cf89c32c92aa736
Katharina Fey <kookie@spacekookie.de>
2019-06-29 11:42:04 +0100

    qaul.net service drafts
    This PR adds two services: messaging and files,
    which allow people on a qaul network to exchange
    text messages (like e-mail) as well as file-share links.
    
    The API of the `messaging` service is flexible enough
    to also allow inlining if a payload isn't too big.
    Alternatively, the fallback "share link" will be used,
    which mirrors the current behaviour of qaul.net (`<2.0.0`)

--- libqaul/service/files/src/lib.rs
+++ libqaul/service/files/src/lib.rs
@@ -1,7 +1,32 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+//! `qaul.net` filesharing service
+
+use qaul::{Qaul, QaulResult, UserAuth};
+use identity::Identity;
+
+/// A typed file that can be sent across the network
+pub struct File {
+    mime: String,
+    data: Vec<u8>,
+}
+
+/// Filesharing service state
+pub struct Filesharing<'q> {
+    qaul: &'q Qaul,
+}
+
+impl<'q> Filesharing<'q> {
+    /// Send a single file to a group of people
+    pub fn send_file(
+        &self,
+        user: UserAuth,
+        recipients: Vec<Identity>,
+        file: File,
+    ) -> QaulResult<()> {
+        unimplemented!()
+    }
+
+    /// Get all files that were received since the last poll
+    pub fn poll_files(&self, user: UserAuth) -> QaulResult<Vec<File>> {
+        unimplemented!()
     }
 }
