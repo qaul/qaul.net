5f0f9905356f365ff9ff0f55f3bc472908ca8647
Katharina Fey <kookie@spacekookie.de>
2019-07-03 19:28:26 +0100

    De-foldering auth and crypto modules

---
+++ libqaul/src/auth.rs
@@ -0,0 +1,14 @@
+//! Internal user authentication modules
+//!
+//! Hooks up to the secret data store and validates user passphrases,
+//! tokens and secrets.
+
+
+/// Handles all authentication state
+pub(crate) struct Authenticator {
+
+}
+
+impl Authenticator {
+
+}
