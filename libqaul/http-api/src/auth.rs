843487d88f65960af51312b8ebda2281dab96ff6
Jess 3Jane <me@jess.coffee>
2019-07-03 15:16:05 -0400

    Added http-api with authenticator
      - Added http-api
      - Added authenticator middeware to http-api
      - Removed old qaul-http api
      - Changed identity to be Copy/Clone
      - Added user stub
      - Cleaned up a few imports

---
+++ libqaul/http-api/src/auth.rs
@@ -0,0 +1,44 @@
+use crate::QaulCore;
+use iron::{
+    BeforeMiddleware,
+    prelude::*,
+    headers::{Authorization, Bearer},
+    typemap,
+};
+use libqaul::{
+    Identity
+};
+use std::{
+    collections::HashMap,
+    sync::{
+        Arc,
+        Mutex,
+    },
+};
+
+#[derive(Clone)]
+pub struct Authenticator{
+    tokens: Arc<Mutex<HashMap<String, Identity>>>,
+}
+
+impl Authenticator {
+    pub fn new() -> Authenticator {
+        Authenticator {
+            tokens: Arc::new(Mutex::new(HashMap::new())),
+        }
+    }
+}
+
+impl typemap::Key for Authenticator { type Value = Option<Identity>; }
+
+impl BeforeMiddleware for Authenticator {
+    fn before(&self, req: &mut Request) -> IronResult<()> {
+        let identity = req.headers.get::<Authorization<Bearer>>()
+            .and_then(|bearer| self.tokens.lock().unwrap()
+                      .get(&bearer.token)
+                      .map(|identity| *identity));
+        req.extensions.insert::<Authenticator>(identity);
+
+        Ok(())
+    }
+}
