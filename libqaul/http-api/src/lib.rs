442e68362ac710506c785dc9bda2830b932da02f
Jess 3Jane <me@jess.coffee>
2019-07-03 15:16:05 -0400

    Added http-api with authenticator
      - Added http-api
      - Added authenticator middeware to http-api
      - Removed old qaul-http api
      - Changed identity to be Copy/Clone
      - Added user stub
      - Cleaned up a few imports

---
+++ libqaul/http-api/src/lib.rs
@@ -0,0 +1,78 @@
+use libqaul::{
+    Qaul,
+    QaulResult,
+    UserAuth, 
+    Identity,
+};
+use iron::{
+    error::HttpResult,
+    Listening,
+    typemap,
+    prelude::*,
+    status::Status,
+    middleware::BeforeMiddleware,
+};
+use std::{
+    net::ToSocketAddrs,
+    sync::Arc,
+};
+
+mod auth;
+use auth::Authenticator;
+
+// stand in for a real handler 
+// coming soon to a pull request near you
+fn not_really_a_handler(_: &mut Request) -> IronResult<Response> {
+    Ok(Response::with(Status::ImATeapot))
+}
+
+pub struct ApiServer {
+    authenticator: Authenticator,
+    listening: Listening,
+}
+
+impl ApiServer {
+    pub fn new<A: ToSocketAddrs>(qaul: Arc<Qaul>, addr: A) -> HttpResult<ApiServer> {
+        let authenticator = Authenticator::new();
+
+        let mut chain = Chain::new(not_really_a_handler);
+        chain.link_before(QaulCore::new(qaul));
+        chain.link_before(authenticator.clone());
+
+        let listening = Iron::new(chain).http(addr)?;
+
+        Ok(ApiServer{ 
+            authenticator: authenticator.clone(), 
+            listening 
+        })
+    }
+
+    /// According to https://github.com/hyperium/hyper/issues/338 this _probably_
+    /// does nothing, but i'm providing it in the hope that in the future
+    /// some one will figure out how to shutdown a webserver without crashing it
+    pub fn close(&mut self) -> HttpResult<()> {
+        self.listening.close()
+    }
+}
+
+struct QaulCore{
+    qaul: Arc<Qaul>,
+}
+
+impl QaulCore {
+    pub fn new(qaul: Arc<Qaul>) -> QaulCore {
+        QaulCore{
+            qaul,
+        }
+    }
+}
+
+impl typemap::Key for QaulCore { type Value = Arc<Qaul>; }
+
+impl BeforeMiddleware for QaulCore {
+    fn before(&self, req: &mut Request) -> IronResult<()> {
+        req.extensions.insert::<QaulCore>(self.qaul.clone());
+        Ok(())
+    }
+}
+
--- ratman/identity/src/lib.rs
+++ ratman/identity/src/lib.rs
@@ -34,7 +34,7 @@
 pub const ID_LEN: usize = 12;
 
 /// A RATMAN identity
-#[derive(PartialEq, Eq, Debug)]
+#[derive(PartialEq, Eq, Debug, Copy, Clone)]
 pub struct Identity([u8; ID_LEN]);
 
 impl Identity {
