526a8d3fd4d0b6fb2fa47d6e143278db0e109985
Katharina Fey <kookie@spacekookie.de>
2019-07-03 17:45:23 +0100

    Adding qaul.net service API drafts (#251)
    * qaul.net service drafts
    
    This PR adds two services: messaging and files,
    which allow people on a qaul network to exchange
    text messages (like e-mail) as well as file-share links.
    
    The API of the `messaging` service is flexible enough
    to also allow inlining if a payload isn't too big.
    Alternatively, the fallback "share link" will be used,
    which mirrors the current behaviour of qaul.net (`<2.0.0`)
    
    * Adding `mime` crate for handling MIME types in files
    * Switching from `Vec<u8>` to `File` abstraction for attachments
    * Adding a small description for the "core" service

--- libqaul/service/core/src/lib.rs
+++ libqaul/service/core/src/lib.rs
@@ -1,1 +1,9 @@
 //! Provides the core service layer with user management
+//!
+//! The idea behind this crate is to wrap all the "core" functions
+//! of the `libqaul` service API in another layer to make them easier
+//! to consume for the HTTP API.
+//!
+//! Depending on how important this is, this crate might either be
+//! a shim crate, or be removed again in the future because it's redundant.
+
--- libqaul/service/messaging/src/lib.rs
+++ libqaul/service/messaging/src/lib.rs
@@ -1,7 +1,63 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+//! `qaul.net` messaging service
+//!
+//! Provides a simple interface to deliver plain-text
+//! messages, with inline attachments, or optionally
+//! async-delivery links (requires `filesharing` service).
+//! It's basically decentralised e-mail.
+//! It's basically e-mail.
+
+use qaul::{Qaul, QaulResult, UserAuth};
+use identity::Identity;
+use files::File;
+
+/// A list of file-attachments
+pub type Attachments = Vec<File>;
+
+/// A plain-text message with optional attachments
+pub struct Message {
+    text: String,
+    attachments: Option<Attachments>,
+}
+
+/// Messaging service state
+pub struct Messaging<'q> {
+    async_files: bool,
+    qaul: &'q Qaul,
+}
+
+impl<'q> Messaging<'q> {
+    /// Initialise the messaging service
+    ///
+    /// In order to initialise, a valid and running
+    /// `Qaul` reference needs to be provided.
+    /// This is then used to register this service,
+    /// but also check for the existence of a `filesharing` service.
+    /// Depending on this check, the `async_files` capability
+    /// can be set.
+    pub fn init(qaul: &'q Qaul) -> Self {
+        Self {
+            async_files: false,
+            qaul,
+        }
+    }
+
+    /// Check if the `async_files` capability is set on this service
+    pub fn async_files(&self) -> bool {
+        self.async_files
+    }
+
+    /// Send a plain-text message with optional arbitrary attachments
+    pub fn send_message(
+        &self,
+        user: UserAuth,
+        recipients: Vec<Identity>,
+        msg: Message,
+    ) -> QaulResult<()> {
+        unimplemented!()
+    }
+
+    /// Get all messages that were received since the last poll
+    pub fn poll_messages(&self, user: UserAuth) -> QaulResult<Vec<Message>> {
+        unimplemented!()
     }
 }
