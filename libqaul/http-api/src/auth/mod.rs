55cb8980187f5e5812ceb41b712c741d0c82b50c
Jess 3Jane <me@jess.coffee>
2019-07-23 19:39:19 -0400

    added log out endpoint

--- libqaul/http-api/src/auth/mod.rs
+++ libqaul/http-api/src/auth/mod.rs
@@ -3,7 +3,10 @@ pub use authenticator::CurrentUser;
 pub (crate) use authenticator::Authenticator;
 
 mod login;
-pub use login::login;
+pub (crate) use login::login;
+
+mod logout;
+pub (crate) use logout::logout;
 
 mod error;
 pub (crate) use error::AuthError;
