2d4b14209054fd2b3e0d29c5b26f5460b9034426
Leonora Tindall <nora@nora.codes>
2019-08-01 11:19:18 -0700

    Revert "Add contacts_find and contacts_get_all"
    These implementations weren't corrent.
    
    This reverts commit 0424afb3e356f7ecf047ebcf0d77ee4135b986b8.

--- libqaul/src/api/mod.rs
+++ libqaul/src/api/mod.rs
@@ -126,29 +126,13 @@ impl Qaul {
     }
 
     /// Find a subset of contacts with some query
-    pub fn contacts_find<S: Into<String>>(
-        &self,
-        _user: UserAuth,
-        query: S,
-    ) -> QaulResult<Vec<User>> {
-        let users = self.users.lock().unwrap();
-        let query = query.into();
-        Ok(users
-            .iter()
-            .filter(|(_, user)| user.data.like_query(&query))
-            .map(|(_, user)| user.clone())
-            .collect())
+    pub fn contacts_find(&self, user: UserAuth, query: String) -> QaulResult<Vec<User>> {
+        unimplemented!()
     }
 
     /// Enumerate all contacts known by a user
-    pub fn contacts_get_all(&self, _user: UserAuth) -> QaulResult<Vec<User>> {
-        Ok(self
-            .users
-            .lock()
-            .unwrap()
-            .iter()
-            .map(|(_, user)| user.clone())
-            .collect())
+    pub fn contacts_get_all(&self, user: UserAuth) -> QaulResult<Vec<User>> {
+        unimplemented!()
     }
 
     /// Send a message to another user
