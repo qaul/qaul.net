5a9192314b3db09b14e6eca640dc34930bb0f943
Jess 3Jane <me@jess.coffee>
2019-08-09 23:19:59 -0400

    added tests for user management

--- libqaul/src/api/mod.rs
+++ libqaul/src/api/mod.rs
@@ -220,3 +220,60 @@ impl Qaul {
         Ok(())
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    
+    #[test]
+    fn user_lifecycle() {
+        let qaul = Qaul::start();
+
+        // can we make a user?
+        let auth = qaul.user_create("password").unwrap();
+
+        // does something get added to the correct parts of state?
+        {
+            assert_eq!(qaul.auth.lock().unwrap().len(), 1);
+            assert_eq!(qaul.keys.lock().unwrap().len(), 1);
+            assert_eq!(qaul.users.lock().unwrap().len(), 1);
+        }
+
+        // are we trusted?
+        let (id, key) = qaul.user_authenticate(auth.clone()).unwrap();
+
+        // can we login as that user?
+        let auth2 = qaul.user_login(id.clone(), "password").unwrap();
+
+        // is the state updated appropriately?
+        {
+            assert_eq!(qaul.auth.lock().unwrap().len(), 1);
+            assert_eq!(qaul.keys.lock().unwrap().len(), 2);
+            assert_eq!(qaul.users.lock().unwrap().len(), 1);
+        }
+
+        // are we trusted?
+        let (id2, key2) = qaul.user_authenticate(auth2.clone()).unwrap();
+
+        // do we get back the same id?
+        assert_eq!(id.clone(), id2);
+        // do we get a different key?
+        assert_ne!(key.clone(), key2);
+
+        // can we log out?
+        qaul.user_logout(auth2.clone());
+
+        // is the state updated appropriately
+        {
+            assert_eq!(qaul.auth.lock().unwrap().len(), 1);
+            assert_eq!(qaul.keys.lock().unwrap().len(), 1);
+            assert_eq!(qaul.users.lock().unwrap().len(), 1);
+        }
+
+        // are we not trusted now?
+        assert!(qaul.user_authenticate(auth2).is_err());
+
+        // does logging in with the wrong password actually error?
+        assert!(qaul.user_login(id.clone(), "not password").is_err());
+    }
+}
