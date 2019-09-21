9e300b25f133af32cee728ece79edb061af1f3e5
Jess 3Jane <me@jess.coffee>
2019-09-20 22:47:41 -0400

    added logout tests

--- libqaul/http-api/src/auth/logout.rs
+++ libqaul/http-api/src/auth/logout.rs
@@ -59,3 +59,106 @@ pub fn logout(req: &mut Request) -> IronResult<Response> {
 
     Ok(Response::with((Status::Ok, serde_json::to_string(&doc).unwrap(), JSONAPI_MIME.clone())))
 }
+
+#[cfg(test)]
+mod test {
+    use anneal::RequestBuilder;
+    use crate::{cookie::CookieManager, JsonApi};
+    use cookie::{CookieJar, Cookie};
+    use iron::{
+        method::Method,
+        middleware::BeforeMiddleware,
+        headers::{Authorization, Bearer},
+    };
+    use std::io::Read;
+    use super::*;
+    use libqaul::{
+        Qaul,
+        UserUpdate,
+        Identity,
+    };
+    use json_api::ResourceObject;
+
+    fn setup() -> (Vec<Box<dyn BeforeMiddleware>>, Authenticator, CookieManager, Identity, String) {
+        let qaul = Qaul::start();
+        let user_auth = qaul.user_create("a".into()).unwrap();
+        let (ident, key) = qaul.user_authenticate(user_auth).unwrap();
+
+        let core = QaulCore::new(&qaul);
+        let (before_manager, after_manager) = CookieManager::new();
+        let auth = Authenticator::new();
+        { auth.tokens.lock().unwrap().insert(key.clone(), ident.clone()); } 
+
+        (
+            vec![Box::new(core), Box::new(before_manager), Box::new(JsonApi), Box::new(auth.clone())], 
+            auth,
+            after_manager,
+            ident,
+            key
+        )
+    }
+
+    #[test]
+    fn logout_cookie() {
+        let (chain, auth, cookie_manager, ident, key) = setup();
+
+        let mut jar = CookieJar::new();
+        jar.add(Cookie::new("bearer", key.clone()));
+        RequestBuilder::new(Method::Get, "https://127.0.0.1/")
+            .unwrap()
+            .set_cookies(&jar)
+            .request(|mut req| {
+                for m in chain { m.before(&mut req).unwrap() }
+                let response = logout(&mut req).unwrap();
+
+                let mut body = Vec::new();
+                response.body.unwrap().write_body(&mut body).unwrap();
+                let body = String::from_utf8(body).unwrap();
+                let doc : Document = serde_json::from_str(&body).unwrap();
+                let go = match doc.data {
+                    OptionalVec::One(Some(go)) => go,
+                    o => panic!("Exepected single generic object, got {:?}", o),
+                };
+                let ro : ResourceObject<Success> = go.try_into().unwrap();
+
+                assert_eq!(auth.tokens.lock().unwrap().get(&key), None);
+                assert_eq!(req.extensions.get::<Cookies>().unwrap().get("bearer"), None);
+            });
+    }
+
+    #[test]
+    fn logout_token() {
+        let (chain, auth, cookie_manager, ident, key) = setup();
+
+        RequestBuilder::new(Method::Get, "https://127.0.0.1/")
+            .unwrap()
+            .set_header(Authorization(Bearer { token: key.clone() }))
+            .request(|mut req| {
+                for m in chain { m.before(&mut req).unwrap() }
+                let response = logout(&mut req).unwrap();
+
+                let mut body = Vec::new();
+                response.body.unwrap().write_body(&mut body).unwrap();
+                let body = String::from_utf8(body).unwrap();
+                let doc : Document = serde_json::from_str(&body).unwrap();
+                let go = match doc.data {
+                    OptionalVec::One(Some(go)) => go,
+                    o => panic!("Exepected single generic object, got {:?}", o),
+                };
+                let ro : ResourceObject<Success> = go.try_into().unwrap();
+
+                assert_eq!(auth.tokens.lock().unwrap().get(&key), None);
+            });
+    }
+
+    #[test]
+    fn no_login() {
+        let (chain, auth, cookie_manager, ident, key) = setup();
+        RequestBuilder::new(Method::Get, "https://127.0.0.1/")
+            .unwrap()
+            .request(|mut req| {
+                for m in chain { m.before(&mut req).unwrap() }
+                assert!(logout(&mut req).is_err())
+            });
+    }
+}
