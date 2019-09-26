55c42bf370bdc10343b59581c09d32a37f12e690
Lux <lux@lux.name>
2019-09-26 19:51:22 +0200

    move to .ember-cli.js

--- webgui/.ember-cli.js
+++ webgui/.ember-cli.js
@@ -1,4 +1,11 @@
-{
+"use strit";
+
+// eslint-disable-next-line node/no-extraneous-require
+const { setEdition } = require('@ember/edition-utils');
+
+setEdition('octane');
+
+module.exports = {
   /**
     Ember CLI sends analytics information by default. The data is completely
     anonymous, but there are times when you might want to disable this behavior.
@@ -6,4 +13,4 @@
     Setting `disableAnalytics` to true will prevent any data from being sent.
   */
   "disableAnalytics": true
-}
+};
