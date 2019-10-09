4d5118384501ffac53d335555e5610a6cbd90f64
Katharina Fey <kookie@spacekookie.de>
2019-10-09 13:57:43 +0000

    Updating

--- libqaul/src/auth.rs
+++ libqaul/src/auth.rs
@@ -6,4 +6,27 @@
 /// Handles all authentication state
 pub(crate) struct Authenticator {}
 
+/// A wrapper around a salted password hash
+pub(crate) struct PwHash {
+    hash: Vec<u8>,
+    salt: Vec<u8>,
+}
+
+impl PwHash {
+    pub(crate) fn matches_with<S>(pw: S) -> bool
+    where
+        S: Into<String>,
+    {
+        false
+    }
+
+    pub(crate) fn new<S>(pw: S) -> Self
+    where
+        S: Into<String>,
+    {
+        
+        
+    }
+}
+
 impl Authenticator {}
