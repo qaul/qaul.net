46ef058d83da412b5d0cdab6e54094cde3285194
Leonora Tindall <nora@nora.codes>
2019-10-26 21:19:56 -0500

    Split netmod-mem::media into multiple files.
    This is just a quick reformatting commit, but it will make things much
    easier when adding additional in-memory media.

---
+++ netmod-mem/src/media/tagged_frame.rs
@@ -0,0 +1,14 @@
+use ratman_netmod::{Endpoint, Frame};
+/// A `Frame` tagged with an identifier and a time to live, for propagation through
+/// a simulated medium.
+pub(crate) struct TaggedFrame {
+    pub tag: u32,
+    pub ttl: u32,
+    pub frame: Frame,
+}
+
+impl TaggedFrame {
+    pub(crate) fn new(tag: u32, ttl: u32, frame: Frame) -> Self {
+        Self { tag, ttl, frame }
+    }
+}
---
+++ netmod-mem/tests/broadcast.rs
@@ -0,0 +1,41 @@
+use netmod_mem::media::BroadcastMedium;
+use ratman_netmod::{Frame, Endpoint};
+#[test]
+fn broadcast_medium_ping_pong() {
+    let mut medium = BroadcastMedium::new(1);
+    let mut a = medium.make_netmod();
+    let mut b = medium.make_netmod();
+    a.send(Frame::dummy())
+        .expect("Failed to send message from a. Error");
+    medium = medium.tick();
+    b.poll()
+        .expect("Failed to get message at b. Error")
+        .expect("No message available.");
+    b.send(Frame::dummy())
+        .expect("Failed to send message from b. Error");
+    medium.tick();
+    a.poll()
+        .expect("Failed to get message at a. Error")
+        .expect("No message available.");
+}
+
+#[test]
+fn broadcast_medium_ping_broadcast() {
+    let mut medium = BroadcastMedium::new(1);
+    let mut a = medium.make_netmod();
+    let mut b = medium.make_netmod();
+    let mut c = medium.make_netmod();
+    let mut d = medium.make_netmod();
+    a.send(Frame::dummy())
+        .expect("Failed to send message from a. Error");
+    medium.tick();
+    b.poll()
+        .expect("Failed to get message at b. Error")
+        .expect("No message available.");
+    c.poll()
+        .expect("Failed to get message at c. Error")
+        .expect("No message available.");
+    d.poll()
+        .expect("Failed to get message at d. Error")
+        .expect("No message available.");
+}
