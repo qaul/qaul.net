5860b8434b78ad60f10eb203936fa4e162c0e990
Leonora Tindall <nora@nora.codes>
2019-10-27 21:46:53 -0500

    Remove unnecessary RwLock in MemMod.
    Since we were already just using .get_mut() anyway, we can drop the
    RwLock entirely and just use a regular reference.

--- netmod-mem/src/lib.rs
+++ netmod-mem/src/lib.rs
@@ -3,49 +3,47 @@
 //! This aims to make testing any structure that binds against
 //! `netmod` easier and reproducable.
 
-use ratman_identity::Identity;
 use ratman_netmod::{Endpoint, Error as NetError, Frame, Result as NetResult};
-use std::{
-    sync::mpsc::{self, Receiver, Sender, TryRecvError},
-    sync::RwLock,
-};
+use std::sync::mpsc::TryRecvError;
 
 /// An input/output pair of `mpsc::channel`s.
+///
+/// This is the actual mechanism by which data is moved around between `MemMod`s in
+/// different places.
 pub(crate) mod io;
 /// Simulated transmission media.
 pub mod media;
 
-/// Represent a single netmod endpoint that can connect to exactly one
-/// other
-///
-/// Both `latency` and `bn` are public so that they can be dynamically
-/// adjusted in a simulation.
+/// Represents a single netmod endpoint that can connect to exactly one other, either
+/// as a 1-to-1 link between libqaul instances or as a link into a transmission
+/// medium of some kind.
 pub struct MemMod {
     /// Internal memory access to send/receive
-    io: RwLock<Option<io::Io>>,
-    /// Apply artificial latency (not implemented, needs async)
-    pub latency: u8,
-    /// The troughput limit in bytes per second (not implemented, needs async)
-    pub bn: u32,
+    io: Option<io::Io>,
 }
 
 impl MemMod {
     /// Create a new, unpaired `MemMod`.
     pub fn new() -> Self {
         Self {
-            io: RwLock::new(None),
-            latency: 1,
-            bn: std::u32::MAX,
+            io: None
         }
+    }
+
+    /// Create two already-paired `MemMod`s, ready for use.
+    pub fn make_pair() -> (Self, Self) {
+        let (mut a, mut b) = (MemMod::new(), MemMod::new());
+        a.link(&mut b);
+        (a, b)
     }
 
     /// Return `true` if the MemMod is linked to another one or
     /// `false` otherwise.
     pub fn linked(&self) -> bool {
-        self.io.read().expect("RWLock poisoned").is_some()
+        self.io.is_some()
     }
 
-    /// Establish a 1-1 link between two MemMods
+    /// Establish a 1-to-1 link between two `MemMod`s.
     ///
     /// # Panics
     ///
@@ -55,8 +53,8 @@ impl MemMod {
             panic!("Attempted to link an already linked MemMod.");
         }
         let (my_io, their_io) = io::Io::make_pair();
-        *self.io.get_mut().expect("RWLock poisoned") = Some(my_io);
-        *pair.io.get_mut().expect("RWLock poisoned") = Some(their_io);
+        self.io = Some(my_io);
+        pair.io = Some(their_io);
     }
 
     /// Establish a link to an `Io` module
@@ -67,21 +65,21 @@ impl MemMod {
         if self.linked() {
             panic!("Attempted to link an already linked MemMod.");
         }
-        *self.io.get_mut().expect("RWLock poisoned") = Some(io);
+        self.io = Some(io);
     }
 
     /// Remove the connection between MemMods.
-    pub fn split(&self) {
+    pub fn split(&mut self) {
         // The previous value in here will now be dropped,
         // so future messages will fail.
-        *self.io.write().expect("RwLock poisoned") = None;
+        self.io = None;
     }
 }
 
 impl Endpoint for MemMod {
     /// Provides maximum frame-size information to `RATMAN`
     fn size_hint(&self) -> usize {
-        self.bn as usize
+        ::std::u32::MAX as usize
     }
 
     /// Send a message to a specific endpoint (client)
@@ -91,7 +89,7 @@ impl Endpoint for MemMod {
     /// Returns `OperationNotSupported` if attempting to send through
     /// a connection that is not yet connected.
     fn send(&mut self, frame: Frame) -> NetResult<()> {
-        match &*self.io.read().expect("RWLock poisoned") {
+        match self.io {
             None => Err(NetError::NotSupported),
             Some(ref io) => match io.out.send(frame) {
                 Ok(_) => Ok(()),
@@ -101,7 +99,7 @@ impl Endpoint for MemMod {
     }
 
     fn poll(&mut self) -> NetResult<Option<Frame>> {
-        match *self.io.get_mut().expect("RWLock poisoned") {
+        match self.io {
             None => Err(NetError::NotSupported),
             Some(ref mut io) => match io.inc.try_recv() {
                 Ok(v) => {
@@ -115,7 +113,7 @@ impl Endpoint for MemMod {
     }
 
     fn listen(&mut self, mut handler: Box<dyn FnMut(Frame) -> NetResult<()>>) -> NetResult<()> {
-        match &mut *self.io.get_mut().expect("RWLock poisoned") {
+        match self.io {
             None => Err(NetError::NotSupported),
             Some(ref mut io) => match io.inc.recv() {
                 Ok(v) => handler(v),
