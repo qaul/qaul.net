847cc818c92b85f4fde8393692cd93ae1db8e2b8
Katharina Fey <kookie@spacekookie.de>
2019-11-14 15:29:48 +0100

    Bumping async-std to 1.0 and implementing announce task

--- libqaul/src/discover.rs
+++ libqaul/src/discover.rs
@@ -1,37 +1,45 @@
-//!
-
-use ratman::{Message, Router};
+use async_std::task;
+use ratman::{Protocol, Message, Router, Identity};
 use std::{
     sync::{mpsc::Receiver, Arc},
     thread::{self, JoinHandle},
+    time::Duration,
 };
 
 #[derive(Clone)]
 pub(crate) struct Discovery {
-    router: Option<Arc<Router>>,
-    worker: Option<Arc<JoinHandle<()>>>,
+    router: Arc<Router>,
+    worker: Arc<JoinHandle<()>>,
 }
 
 impl Discovery {
-    /// Create empty Discovery daemon
-    ///
-    /// Don't forget to initialise later!
-    pub(crate) fn missing() -> Self {
-        Self {
-            router: None,
-            worker: None,
-        }
-    }
 
+    /// Enable the announcement cycle for an active User session
+    ///
+    /// Spawns an async task on the local executor. Currently there's
+    /// no way of ending an active session and there's no validation
+    /// done by this task to identity when an Announce should be
+    /// stopped.  This needs to be refactored but can be done at a
+    /// later point.
+    pub(crate) fn start_announce(&self, id: Identity) {
+        let router = Arc::clone(&self.router);
+        task::spawn(async move {
+            loop {
+                task::sleep(Duration::from_secs(2)).await;
+                router.send(Protocol::announce(id.clone())).unwrap();
+            }
+        });
+    }
+    
     /// Initialise and start Discovery feature
-    pub(crate) fn new(r: Arc<Router>, router: Receiver<Message>) -> Self {
+    pub(crate) fn new(router: Arc<Router>, recvr: Receiver<Message>) -> Self {
         Self {
-            router: Some(r),
-            worker: Some(Arc::new(thread::spawn(move || {
-                while let Ok(msg) = router.recv() {
+            router,
+            worker: Arc::new(thread::spawn(move || {
+                while let Ok(msg) = recvr.recv() {
                     dbg!(msg);
                 }
-            }))),
+            })),
         }
     }
 }
