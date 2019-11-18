e05b16740aaef82e93331370d34873bccd6e36c8
Mathias Jud <jud@wachter-jud.net>
2019-11-18 12:53:00 +0100

    removed bottom navigation bar

--- webgui/app/components/nav-bar/component.js
+++
@@ -1,8 +0,0 @@
-import Component from '@ember/component';
-
-export default Component.extend({});
-
-// import Component from '@glimmer/component';
-
-// export default class MyComponentComponent extends Component {
-// }
