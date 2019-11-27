ef4df1e97315ff3081a9ee0774fcbe425ac72387
Lux <lux@lux.name>
2019-11-27 20:45:45 +0100

    zeugs

--- webgui/app/application/controller.js
+++ webgui/app/application/controller.js
@@ -4,5 +4,16 @@ import { inject as service } from '@ember/service';
 export default Controller.extend({
   style: service(),
   intl: service(),
+  actions: {
+    show(what) {
+      this.set('shownav', false);
+      this.set('showaside', false);
+      this.set('show'+what, true);
+    },
+    hide() {
+      this.set('shownav', false);
+      this.set('showaside', false);
+    },
+  }
 });
 
