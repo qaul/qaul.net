c9087d97305ef8c4904b4b62a25cdf411d76fb01
Katharina Fey <kookie@spacekookie.de>
2019-12-20 03:35:33 +0100

    netmod-udp: changing nat model and simplifying dependencies

--- netmod-udp/src/lib.rs
+++ netmod-udp/src/lib.rs
@@ -1,9 +1,18 @@
 //! netmod-udp is a UDP overlay for RATMAN
 
 use identity::Identity;
-use std::collections::{BTreeMap, BTreeSet};
+use std::{collections::BTreeMap, net::IpAddr};
 
 /// Represents an IP network tunneled via UDP
 pub struct Endpoint {
-    nat: BTreeMap<String, BTreeSet<Identity>>,
+    nat: BTreeMap<Identity, IpAddr>,
+}
+
+impl Endpoint {
+    /// Create a new UDP endpoint handler
+    pub fn new() -> Self {
+        Self {
+            nat: Default::default(),
+        }
+    }
 }
