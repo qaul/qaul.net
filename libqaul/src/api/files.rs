f22d446fe37348e04f62b6f2991f8fb4c1a27e27
Katharina Fey <kookie@spacekookie.de>
2019-12-20 02:51:46 +0100

    libqaul: adding file api stubs to new api structure

--- libqaul/src/api/files.rs
+++ libqaul/src/api/files.rs
@@ -1,51 +1,163 @@
-//! Service API file access
-//!
-//! Not to be confused with the `files` filesharing service. This is
-//! basic fs abstraction access to allow services to store files in a
-//! users namespace.
+#![allow(unused)]
 
-use super::models::{File, FileFilter, FileMeta, UserAuth};
-use crate::{Qaul, QaulResult};
+use crate::error::Result;
+use crate::users::UserAuth;
+use serde::{Deserialize, Serialize};
 
-impl Qaul {
-    /// Query available files to a user
-    ///
-    /// Optionally: use the `filter` parameter to exclude certain
-    /// types of files, based on the lifecycle in the system.
-    pub fn files_query<I>(user: UserAuth, filter: Option<FileFilter>) -> QaulResult<I>
+/// Length of an `MsgId`, for converting to and from arrays
+pub const ID_LEN: usize = 16;
+
+/// A unique, randomly generated file ID
+#[derive(Serialize, Deserialize, Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct FileId(pub(crate) [u8; ID_LEN]);
+
+impl FileId {
+    /// Generate a new **random** message ID
+    pub(crate) fn new() -> Self {
+        crate::utils::random(ID_LEN)
+            .into_iter()
+            .zip(0..ID_LEN)
+            .fold(Self([0; ID_LEN]), |mut acc, (x, i)| {
+                acc.0[i] = x;
+                acc
+            })
+    }
+}
+
+/// Implement RAW `From` binary array
+impl From<[u8; ID_LEN]> for FileId {
+    fn from(i: [u8; ID_LEN]) -> Self {
+        Self(i)
+    }
+}
+
+/// Implement RAW `From` binary (reference) array
+impl From<&[u8; ID_LEN]> for FileId {
+    fn from(i: &[u8; ID_LEN]) -> Self {
+        Self(i.clone())
+    }
+}
+
+/// Implement binary array `From` RAW
+impl From<FileId> for [u8; ID_LEN] {
+    fn from(i: FileId) -> Self {
+        i.0
+    }
+}
+
+/// Implement binary array `From` RAW reference
+impl From<&FileId> for [u8; ID_LEN] {
+    fn from(i: &FileId) -> Self {
+        i.0.clone()
+    }
+}
+
+/// Implement RAW identity to binary array reference
+impl AsRef<[u8]> for FileId {
+    fn as_ref(&self) -> &[u8] {
+        &self.0
+    }
+}
+
+
+/// Local file abstraction
+pub struct File {
+    pub name: String,
+    pub data: Option<Vec<u8>>,
+}
+
+/// Describe a file's lifecycle
+///
+/// Not to be confused with `FileFilter`, which is part of public API
+/// functions to allow users to easily filter for only certain types
+/// of file data.
+///
+/// Filter functions then take a `Filter` and return a `Meta`.
+pub enum FileMeta {
+    /// Files owned by the current user
+    Local(File),
+    /// Network files, fully locally mirrored
+    Available(File),
+    /// Network files, still downloading
+    InProgress {
+        size: usize,
+        local: usize,
+        stalled: bool,
+    },
+    /// A network advertised file that hasn't started downloading
+    Advertised { size: usize },
+}
+
+/// Describe a file's lifecycle
+///
+/// Filter functions for each time exist and enable
+/// different sub-services based on which phase they
+/// aim for.
+pub enum FileFilter {
+    Local,
+    Available,
+    InProgress,
+}
+
+/// API scope type to access file functions
+///
+/// Used entirely to namespace API endpoints on `Qaul` instance,
+/// without having long type identifiers.
+///
+/// ```norun
+/// # use libqaul::{Qaul, Files};
+/// # let user = unimplemented!();
+/// let q = Qaul::default();
+/// q.files().list(user)?;
+/// ```
+///
+/// It's also possible to `drop` the current scope, back into the
+/// primary `Qaul` scope, although this is not often useful.
+///
+/// ```norun
+/// # use libqaul::{Qaul, Messages};
+/// # let q = Qaul::default();
+/// q.files().drop(); // Returns `&Qaul` again
+/// ```
+pub struct Files<'chain> {
+    pub(crate) q: &'chain crate::Qaul,
+}
+
+impl<'qaul> Files<'qaul> {
+    /// Query the local file store for a specific constraint
+    pub fn query<I>(&self, user: UserAuth, filter: FileFilter) -> Result<I>
     where
         I: Iterator<Item = FileMeta>,
     {
+        self.q.auth.trusted(user)?;
+        unimplemented!()
+    }
+
+    /// List all available files
+    pub fn list<I>(&self, user: UserAuth) -> Result<I>
+    where
+        I: Iterator<Item = FileMeta>,
+    {
+        self.q.auth.trusted(user)?;
         unimplemented!()
     }
 
     /// Stream one particular file from storage
-    ///
-    /// Returns an error if the file couldn't be loaded because it's
-    /// namespace doesn't exist or because it's not ready to load yet
-    /// (FileMeta mismatch).
-    ///
-    /// **ASYNC THIS** currently this function avoids being blocking
-    /// by returning a Lambda, but this is a _bad_ solution. A prime
-    /// candidate for being an `async fn`!
-    pub fn files_get<L>(user: UserAuth, file: &str) -> QaulResult<L>
-    where
-        L: FnOnce() -> QaulResult<File>,
+    pub async fn get(&self, user: UserAuth, file: FileId) -> Result<File>
     {
+        self.q.auth.trusted(user)?;
         unimplemented!()
     }
 
     /// Adds a new file to the local user's storage
-    pub fn files_add(user: UserAuth, name: &str, file: File) -> QaulResult<()> {
-        Ok(())
+    pub fn add(&self, user: UserAuth, name: &str, file: File) -> Result<FileId> {
+        self.q.auth.trusted(user)?;
+        unimplemented!()
     }
 
-    /// Delete a file from local storage
-    ///
-    /// If the deleted file was still `InProgress`, all subsequent
-    /// received parts will be ignored, so that no invalid files are
-    /// created in the store.
-    pub fn files_delete(user: UserAuth, name: &str) -> QaulResult<()> {
-        Ok(())
+    /// Delete a file from the local user store 
+    pub fn delete(&self, user: UserAuth, name: FileId) -> Result<()> {
+        self.q.auth.trusted(user)?;
+        unimplemented!()
     }
 }
--- libqaul/src/api/mod.rs
+++ libqaul/src/api/mod.rs
@@ -1,27 +1,16 @@
-//! # Service API scope providers
-//!
-//! The idea behind this interface is further documented in the
-//! `contribute` book. It goes into detail about using it to write
-//! decentralised networking services, using qaul.net as a backend.
-//!
-//! `qaul.net` itself provides a few primary services for "messaging",
-//! "file sharing" and "VoIP", as well as a sort of hidden, management
-//! "core" service.  All of them are implemented via this API,
-//! allowing external developers to write their own services using
-//! qaul.net libraries and networks.
-
-// #[cfg(test)]
-// mod tests;
-// mod files;
+//! libqaul api scopes
 
 pub use contacts::Contacts;
 pub(crate) mod contacts;
 
+pub use files::Files;
+pub(crate) mod files;
+
 pub use messages::Messages;
 pub(crate) mod messages;
 
-pub(crate) mod services;
 pub use services::Services;
+pub(crate) mod services;
 
 pub use users::Users;
 pub(crate) mod users;
