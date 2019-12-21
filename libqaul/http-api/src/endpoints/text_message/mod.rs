a2b443e0c4288ba0974c71d00439c024577d5000
Jess 3Jane <me@jess.coffee>
2019-12-20 22:14:51 -0500

    libqaul/http: add message polling endpoint

--- libqaul/http-api/src/endpoints/text_message/mod.rs
+++ libqaul/http-api/src/endpoints/text_message/mod.rs
@@ -4,6 +4,10 @@ use router::Router;
 mod create;
 use create::text_message_create;
 
+mod poll;
+use poll::text_message_poll;
+
 pub fn route(router: &mut Router) {
     router.route(Method::Post, "/api/text_messages", text_message_create, "text_message_create");
+    router.route(Method::Get, "/api/text_messages", text_message_poll, "text_message_poll");
 }
