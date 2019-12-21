538b4382602fb8c57330be8f32f5488953425add
Jess 3Jane <me@jess.coffee>
2019-12-20 22:53:32 -0500

    libqaul/http: add message get endpoint

--- libqaul/http-api/src/endpoints/text_message/mod.rs
+++ libqaul/http-api/src/endpoints/text_message/mod.rs
@@ -1,6 +1,9 @@
 use iron::method::Method;
 use router::Router;
 
+mod get;
+use get::text_message_get;
+
 mod create;
 use create::text_message_create;
 
@@ -8,6 +11,7 @@ mod poll;
 use poll::text_message_poll;
 
 pub fn route(router: &mut Router) {
+    router.route(Method::Get, "/api/text_messages/:id", text_message_get, "text_message_get");
     router.route(Method::Post, "/api/text_messages", text_message_create, "text_message_create");
     router.route(Method::Get, "/api/text_messages", text_message_poll, "text_message_poll");
 }
