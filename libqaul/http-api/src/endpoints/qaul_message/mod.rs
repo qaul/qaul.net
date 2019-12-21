90267de86ebd4174a6db35f90c934fb8c265ff1b
Jess 3Jane <me@jess.coffee>
2019-12-21 15:49:54 -0500

    libqaul/http: add service messaging query

--- libqaul/http-api/src/endpoints/qaul_message/mod.rs
+++ libqaul/http-api/src/endpoints/qaul_message/mod.rs
@@ -7,6 +7,9 @@ use get::qaul_message_get;
 mod create;
 use create::qaul_message_create;
 
+mod query;
+use query::qaul_message_query;
+
 mod poll;
 use poll::qaul_message_poll;
 
@@ -20,7 +23,11 @@ pub fn route(router: &mut Router) {
     );
 
     fn poll_handler(req: &mut Request) -> IronResult<Response> {
-        qaul_message_poll(req)
+        if req.url.query().is_some() {
+            qaul_message_query(req)
+        } else {
+            qaul_message_poll(req)
+        }
     }
     router.route(Method::Get, "/api/qaul_messages/:service", poll_handler, "qaul_message_poll");
 }
