7e67544e66543ff1eecf10f256455c98b022bbe8
Jess 3Jane <me@jess.coffee>
2019-12-21 15:24:18 -0500

    libqaul/http: added service message create

--- libqaul/http-api/src/endpoints/qaul_message/mod.rs
+++ libqaul/http-api/src/endpoints/qaul_message/mod.rs
@@ -4,6 +4,15 @@ use router::Router;
 mod get;
 use get::qaul_message_get;
 
+mod create;
+use create::qaul_message_create;
+
 pub fn route(router: &mut Router) {
     router.route(Method::Get, "/api/qaul_messages/:service/:id", qaul_message_get, "qaul_message_get");
+    router.route(
+        Method::Post, 
+        "/api/qaul_messages/:service", 
+        qaul_message_create, 
+        "qaul_message_create"
+    );
 }
