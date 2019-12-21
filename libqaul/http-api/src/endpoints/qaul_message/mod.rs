174a73bd86cc414577c2522c65a2412a601c8bf9
Jess 3Jane <me@jess.coffee>
2019-12-21 15:41:39 -0500

    libqaul/http: added service message poll endpoint

--- libqaul/http-api/src/endpoints/qaul_message/mod.rs
+++ libqaul/http-api/src/endpoints/qaul_message/mod.rs
@@ -1,4 +1,4 @@
-use iron::method::Method;
+use iron::{method::Method, prelude::*};
 use router::Router;
 
 mod get;
@@ -6,6 +6,9 @@ use get::qaul_message_get;
 
 mod create;
 use create::qaul_message_create;
+
+mod poll;
+use poll::qaul_message_poll;
 
 pub fn route(router: &mut Router) {
     router.route(Method::Get, "/api/qaul_messages/:service/:id", qaul_message_get, "qaul_message_get");
@@ -15,4 +18,9 @@ pub fn route(router: &mut Router) {
         qaul_message_create, 
         "qaul_message_create"
     );
+
+    fn poll_handler(req: &mut Request) -> IronResult<Response> {
+        qaul_message_poll(req)
+    }
+    router.route(Method::Get, "/api/qaul_messages/:service", poll_handler, "qaul_message_poll");
 }
