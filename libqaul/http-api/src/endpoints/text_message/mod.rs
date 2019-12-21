72222fd0e780bd12391104f23f6d7d28f50d23d4
Jess 3Jane <me@jess.coffee>
2019-12-21 00:15:16 -0500

    libqaul/http: add text message query endpoint

--- libqaul/http-api/src/endpoints/text_message/mod.rs
+++ libqaul/http-api/src/endpoints/text_message/mod.rs
@@ -1,4 +1,4 @@
-use iron::method::Method;
+use iron::{method::Method, prelude::*};
 use router::Router;
 
 mod get;
@@ -7,11 +7,23 @@ use get::text_message_get;
 mod create;
 use create::text_message_create;
 
+mod query;
+use query::text_message_query;
+
 mod poll;
 use poll::text_message_poll;
 
 pub fn route(router: &mut Router) {
     router.route(Method::Get, "/api/text_messages/:id", text_message_get, "text_message_get");
     router.route(Method::Post, "/api/text_messages", text_message_create, "text_message_create");
-    router.route(Method::Get, "/api/text_messages", text_message_poll, "text_message_poll");
+
+    // routes /api/text_messages? differently than /api/text_messages
+    fn poll_handler(req: &mut Request) -> IronResult<Response> {
+        if req.url.query().is_some() {
+            text_message_query(req)
+        } else {
+            text_message_poll(req)
+        }
+    }
+    router.route(Method::Get, "/api/text_messages", poll_handler, "text_message_poll");
 }
