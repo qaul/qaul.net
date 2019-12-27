4c2372720ac93c5152caf9c909ab25483e295bdf
Katharina Fey <kookie@spacekookie.de>
2019-12-27 15:57:24 +0100

    clients/linux: Creating an even more minimal example

--- clients/linux-cli/src/main.rs
+++ clients/linux-cli/src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use libqaul::{messages::Recipient, Qaul};
+use ratman::Router;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let r = Router::new();
+    // TDOD: Add network drivers
+
+    let q = Qaul::new(r);
+    let user = q.users().create("password")?;
+
+    // Register a service
+    q.services().register("de.spacekookie.myapp")?;
+    q.messages().send(
+        user,
+        Recipient::Flood,
+        "de.spacekokie.myapp",
+        vec![1, 2, 3, 4],
+    )?;
+
+    q.messages().listen(user, "de.spacekookie.myapp", |msg| {
+        println!("Received message: {:?}", msg);
+        Ok(()) // Return error if parsing fails
+    });
+
+    Ok(())
 }
