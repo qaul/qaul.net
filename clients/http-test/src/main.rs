6a657aef9fabf7c0d5a0618cbe06068baa9bb16d
Jess 3Jane <me@jess.coffee>
2020-01-13 16:12:38 -0500

    clients/http: fixed http test client
    This involved:
    - re-adding it to the workspace
    - changing how it starts libqaul and libqaul-http
    - making it mount a messaging service too

--- clients/http-test/src/main.rs
+++ clients/http-test/src/main.rs
@@ -1,12 +1,19 @@
 use libqaul::*;
-use qaul_http::*;
+use libqaul_http::*;
+use std::sync::Arc;
+use qaul_messaging::Messaging;
 
 fn main() {
-    let qaul = Qaul::start();
-    qaul.user_create("acab").expect("Failed to create test user!");
-    
-    let _server = ApiServer::new(&qaul, "0.0.0.0:9090")
+    let qaul = Arc::new(Qaul::dummy());
+
+    let messaging = Messaging::new(qaul.clone()); 
+
+    let _server = ServerBuilder::new(qaul.clone())
+        .messaging(&messaging)
+        .start("0.0.0.0:9090")
         .expect("Failed to start qaul.net API server (0.0.0.0:9090)!");
+
+    println!("Started qaul.net API server at http://0.0.0.0:9090");
 
     #[allow(deprecated)]
     loop { std::thread::sleep_ms(500) };
