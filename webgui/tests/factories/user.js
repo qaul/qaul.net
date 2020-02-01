156309fbc0c17732ff65c59b2ba07e361764a388
Lux <lux@lux.name>
2020-02-01 23:00:57 +0100

    use real name in template

--- webgui/tests/factories/user.js
+++ webgui/tests/factories/user.js
@@ -3,11 +3,10 @@ import faker from 'faker';
 
 FactoryGuy.define('user', {
   default: {
-    id: () => btoa(faker.random.number(Math.pow(2, 128))),
-    username: () => faker.random.words(),
-    trust: () => faker.random.number({min: 0, max: 2}),
-    lastSeen: () => faker.date.recent(),
-    identicon: () => faker.image.avatar(),
-    avatar: () => faker.image.avatar(),
+    realName: () => faker.random.words(),
+    // trust: () => faker.random.number({min: 0, max: 2}),
+    // lastSeen: () => faker.date.recent(),
+    // identicon: () => faker.image.avatar(),
+    // avatar: () => faker.image.avatar(),
   }
 });
