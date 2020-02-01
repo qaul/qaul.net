c6effb93d811baf713e79278ba9736c186ba7f35
Lux <lux@lux.name>
2020-02-01 20:34:54 +0100

    fix fontawesome

---
+++ webgui/config/icons.js
@@ -0,0 +1,6 @@
+module.exports = function() {
+  return {
+    'free-solid-svg-icons': 'all',
+    'free-brands-svg-icons': ['bluetooth'],
+  };
+};
--- webgui/ember-cli-build.js
+++ webgui/ember-cli-build.js
@@ -4,9 +4,6 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
-    'ember-font-awesome': {
-      removeUnusedIcons: EmberApp.env() === 'production',
-    },
   });
 
   return app.toTree();
