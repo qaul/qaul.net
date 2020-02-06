4897a86841d7a5cbcc911454b9c951bfdcfbde20
Katharina Fey <kookie@spacekookie.de>
2020-02-06 19:15:45 +0100

    netmod-wd: adjusting module to new netmod interface and code cleanup

--- netmod-wd/src/lib.rs
+++ netmod-wd/src/lib.rs
@@ -7,10 +7,9 @@ use async_std::{
     task::{self, Poll},
 };
 use async_trait::async_trait;
-use std::{collections::VecDeque, ffi::c_void, slice};
-
 use conjoiner;
-use netmod::{Endpoint, Frame, Result};
+use netmod::{Endpoint, Error, Frame, Result, Target};
+use std::{collections::VecDeque, ffi::c_void};
 
 /// The wifi direct state holder that implements the endpoint trait
 #[repr(C)]
@@ -33,16 +32,12 @@ pub extern "C" fn new() -> *mut wifid_t {
 pub extern "C" fn give(this: *mut wifid_t, f: *const c_void, len: usize) {
     let this = unsafe { Box::from_raw(this) };
     let buf = unsafe { *(f as *const &[u8]) };
-    let vec: Vec<u8> = buf.into_iter().take(len).collect();
-    
-    buf.len();
-    
-    // let buf: &[u8] = unsafe { slice::from_raw_parts(f, len) };
-    // let frame = conjoiner::deserialise(buf);
+    let vec: Vec<u8> = buf.into_iter().take(len).cloned().collect();
+    let frame = conjoiner::deserialise(&vec).unwrap();
 
-    // task::spawn(async move {
-    //     this.inc.lock().await.push_back(Ok(f));
-    // });
+    task::spawn(async move {
+        this.inc.lock().await.push_back(Ok(frame));
+    });
 }
 
 extern "C" {
@@ -50,7 +45,7 @@ extern "C" {
     ///
     /// Hands off a const buffer with a length and target specifier.
     /// Is not responsible for encoding data.
-    fn send(f: *const c_void, length: u32, target: i16) -> u16;
+    fn send_raw(f: *const c_void, length: usize, target: i16) -> u16;
 }
 
 #[async_trait]
@@ -59,17 +54,24 @@ impl Endpoint for wifid_t {
         0
     }
 
-    async fn send(&mut self, frame: Frame, target: i16) -> Result<()> {
-        unimplemented!()
+    async fn send(&mut self, frame: Frame, _: Target) -> Result<()> {
+        let buf = Box::new(conjoiner::serialise(&frame).unwrap());
+        let len = buf.len();
+        let c = unsafe { send_raw(Box::into_raw(buf) as *const c_void, len, 0) };
+        match c {
+            0 => Ok(()),
+            // TODO: disambiguate errors here (create error mapping?)
+            _ => Err(Error::ConnectionLost),
+        }
     }
 
-    async fn next(&mut self) -> Result<(Frame, i16)> {
+    async fn next(&mut self) -> Result<(Frame, Target)> {
         let inc = Arc::clone(&self.inc);
         future::poll_fn(|ctx| {
             let lock = &mut inc.lock();
             match unsafe { Pin::new_unchecked(lock).poll(ctx) } {
                 Poll::Ready(mut inc) => match inc.pop_front() {
-                    Some(Ok(f)) => Poll::Ready(Ok((f, 0))),
+                    Some(Ok(f)) => Poll::Ready(Ok((f, Target::default()))),
                     Some(Err(e)) => Poll::Ready(Err(e)),
                     None => Poll::Pending,
                 },
