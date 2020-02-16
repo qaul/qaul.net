7a93693ebee0493effaeefc8548c792659010eeb
Jess 3Jane <me@jess.coffee>
2020-02-16 02:10:35 -0500

    libqaul: move `*Diff` structs to `helpers`

--- libqaul/src/api/helpers/mod.rs
+++ libqaul/src/api/helpers/mod.rs
@@ -1,10 +1,10 @@
 //! A set of API helpers
 
 mod subs;
-pub use subs::*;
+pub use subs::{SubId, Subscription};
 
 mod diff;
-pub use diff::*;
+pub use diff::{ItemDiff, ItemDiffExt, MapDiff, MapDiffExt, SetDiff, SetDiffExt};
 
 mod tag;
-pub use tag::*;
+pub use tag::Tag;
