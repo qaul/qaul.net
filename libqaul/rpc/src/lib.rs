3fc041bd987a01d4d7e02e22e48eee9acef5c4d4
Jess 3Jane <me@jess.coffee>
2020-02-16 02:14:30 -0500

    libqaul/rpc: move traits to api where they belong

--- libqaul/rpc/src/lib.rs
+++ libqaul/rpc/src/lib.rs
@@ -22,33 +22,3 @@ pub use api::{contacts, files, messages, users};
 
 //mod proto;
 
-use async_trait::async_trait;
-use libqaul::Qaul;
-
-/// An extension allowing `QaulRPC` object to be applied directly
-/// to an instance of `Qaul`
-#[async_trait]
-pub trait QaulExt {
-    async fn apply<R, T>(&self, r: T) -> R
-    where
-        R: Send + Sync,
-        T: Send + Sync + QaulRPC<Response = R>;
-}
-
-#[async_trait]
-impl QaulExt for Qaul {
-    async fn apply<R, T>(&self, r: T) -> R
-    where
-        R: Send + Sync,
-        T: Send + Sync + QaulRPC<Response = R>,
-    {
-        r.apply(self).await
-    }
-}
-
-/// A trait for objects that can modify an instance of `Qaul`
-#[async_trait]
-pub trait QaulRPC {
-    type Response;
-    async fn apply(self, qaul: &Qaul) -> Self::Response;
-}
