ff78134fbbd9ef64862ce9595ce2c4cd1e8657e9
Mathias Jud <jud@wachter-jud.net>
2020-02-21 13:47:46 +0100

    added linting declaration for i18n/translation string checking

--- webgui/.template-lintrc.js
+++ webgui/.template-lintrc.js
@@ -3,6 +3,7 @@
 module.exports = {
   extends: 'octane',
   rules: {
+    'no-bare-strings': false,
     // 'no-action': true,
     // 'require-button-type': true,
     // 'no-curly-component-invocation': true,
