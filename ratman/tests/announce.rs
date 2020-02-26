ccf8197e0f0a48e2fafea3da9ca6f61c92e6cef7
Katharina Fey <kookie@spacekookie.de>
2020-02-26 15:46:08 +0000

    ratman: changing announce test to use multi-hops

--- ratman/tests/announce.rs
+++ ratman/tests/announce.rs
@@ -1,3 +1,17 @@
+//! An announce test on a three-node network
+//!
+//! This test is similar to the one featured in the root doc page,
+//! except that the network is a bit more complicated.  This test
+//! implements a three-node network, meaning that messages _have_ to
+//! be re-transmitted by a middle router.
+//!
+//! From experience, these kinds of low-level, distributed tests are
+//! good at catching errors in the code, but also demonstrate how the
+//! concepts in Ratman scale.
+//!
+//! Because a `MemMod` is always 1-to-1, we need to create two, and
+//! then add two interfaces to the middle router.
+
 use async_std::task;
 use netmod_mem::MemMod;
 use ratman::{Identity, Result, Router};
@@ -5,31 +19,37 @@ use ratman::{Identity, Result, Router};
 async fn testing() -> Result<()> {
     // Build a simple channel in memory
     let mm1 = MemMod::new();
-    let mm2 = MemMod::new();
-    mm1.link(&mm2);
+    let mm2_1 = MemMod::new();
+    let mm2_3 = MemMod::new();
+    let mm3 = MemMod::new();
+    mm1.link(&mm2_1);
+    mm2_3.link(&mm3);
 
     // Initialise two routers, one for each device
     let r1 = Router::new();
     let r2 = Router::new();
+    let r3 = Router::new();
 
     // Add channel endpoints to routers
     r1.add_endpoint(mm1).await;
-    r2.add_endpoint(mm2).await;
+    r2.add_endpoint(mm2_1).await;
+    r2.add_endpoint(mm2_3).await;
+    r3.add_endpoint(mm3).await;
 
     // Create some users and add them to the routers
     let u1 = Identity::random();
     r1.add_user(u1).await?;
 
-    let u2 = Identity::random();
-    r2.add_user(u2).await?;
+    let u3 = Identity::random();
+    r3.add_user(u3).await?;
 
     // And mark them "online"
     r1.online(u1).await?;
-    r2.online(u2).await?;
+    r3.online(u3).await?;
 
     // The routers will now start announcing their new users on the
     // micro-network.  You can now poll for new user discoveries.
-    assert_eq!(r1.discover().await, u2);
+    assert_eq!(r1.discover().await, u3);
 
     Ok(())
 }
