8c211a7358602663f1adbe45448484c6942874ea
Leonora Tindall <nora@nora.codes>
2020-02-26 18:28:44 -0600

    librobot: allow all libs to build (still fails to link)

--- librobot/build.sh
+++ librobot/build.sh
@@ -6,8 +6,20 @@ if [ ! -d $JNI_LIBS ]; then
     exit 1
 fi
 
+NDK_ROOT=~/.NDK
+echo $NDK_ROOT
+echo "$NDK_ROOT/whatev"
+
+export AR="$NDK_ROOT/arm64/bin/aarch64-linux-android-ar"
+export CC="$NDK_ROOT/arm64/bin/aarch64-linux-android-clang"
 cargo build --target aarch64-linux-android --release
+
+export AR="$NDK_ROOT/arm/bin/arm-linux-androideabi-ar"
+export CC="$NDK_ROOT/arm/bin/arm-linux-androideabi-clang"
 cargo build --target armv7-linux-androideabi --release
+
+export AR="$NDK_ROOT/x86/bin/i686-linux-android-ar"
+export CC="$NDK_ROOT/x86/bin/i686-linux-android-clang"
 cargo build --target i686-linux-android --release
 
 rm -rf $JNI_LIBS
@@ -16,6 +28,6 @@ mkdir $JNI_LIBS/arm64-v8a
 mkdir $JNI_LIBS/armeabi-v7a
 mkdir $JNI_LIBS/x86
 
-cp target/aarch64-linux-android/release/librust.so $JNI_LIBS/arm64-v8a/librust.so
-cp target/armv7-linux-androideabi/release/librust.so $JNI_LIBS/armeabi-v7a/librust.so
-cp target/i686-linux-android/release/librust.so $JNI_LIBS/x86/librust.so
+cp target/aarch64-linux-android/release/librobot.so $JNI_LIBS/arm64-v8a/librobot.so
+cp target/armv7-linux-androideabi/release/librobot.so $JNI_LIBS/armeabi-v7a/librobot.so
+cp target/i686-linux-android/release/librobot.so $JNI_LIBS/x86/librobot.so
