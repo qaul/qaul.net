8dcc45495e16e5df3417b5e01d0bb080db1961a3
Mathias Jud <jud@wachter-jud.net>
2020-03-24 18:32:59 +0100

    webgui: disabled linting error 'no-console' for testing

--- webgui/.eslintrc.js
+++ webgui/.eslintrc.js
@@ -22,7 +22,8 @@ module.exports = {
     browser: true
   },
   rules: {
-    'ember/no-jquery': 'error'
+    'ember/no-jquery': 'error',
+    'no-console': 'off',
   },
   overrides: [
     // node files
