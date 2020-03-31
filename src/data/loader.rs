13d070d733a5b1675fe36e74e580296d702932f9
Katharina Fey <kookie@spacekookie.de>
2020-03-31 02:44:57 +0200

    Implementing basic file loading

---
+++ src/data/loader.rs
@@ -0,0 +1,78 @@
+//! A record data loader abstraction
+
+use crate::{
+    crypto::{asym::KeyPair, Encrypted},
+    data::{Body, Header, Record, SecHeader, Tag, Type},
+    error::Result,
+    Id,
+};
+use std::collections::BTreeSet;
+use async_std::{fs::File, io::ReadExt, path::Path};
+use bincode;
+
+impl Record {
+    /// Lazy load a record from disk, depending on the payload
+    pub(crate) async fn load_record<'p, P: Into<&'p Path>>(
+        path: P,
+        id: Id,
+        key: &KeyPair,
+    ) -> Result<Self> {
+        let mut f = File::open(path.into()).await?;
+
+        // Read the header lenth
+        let mut buf: [u8; 8] = [0; 8];
+        f.read_exact(&mut buf).await?;
+        let hlen = u64::from_le_bytes(buf);
+
+        // Read the pub header
+        let mut hser = vec![0; hlen as usize];
+        f.read_exact(&mut hser).await?;
+
+        // Build the header
+        let Header { id, tags, mut sec } = bincode::deserialize(&hser)?;
+
+        // Attempt to decrypt the sec header
+        sec.open(&key)?;
+        let SecHeader {
+            ref t,
+            ref size,
+            ref chunks,
+        } = sec.deref()?;
+
+        match t {
+            /// We load kv records immediately
+            Type::Kv => {
+                let mut bser = vec![0; *size as usize];
+                f.read_exact(&mut bser).await?;
+                let mut body: Encrypted<Body, KeyPair> = bincode::deserialize(&bser)?;
+                body.open(&key)?;
+
+                Ok(Self {
+                    header: Header { id, tags, sec },
+                    body,
+                })
+            }
+            /// We don't _really_ load blob records
+            Type::Blob => unimplemented!(),
+        }
+    }
+}
+
+pub(crate) async fn load_tags<'p, P: Into<&'p Path>>(path: P) -> Result<BTreeSet<Tag>> {
+    let mut f = File::open(path.into()).await?;
+
+    // Read the header lenth
+    let mut buf: [u8; 8] = [0; 8];
+    f.read_exact(&mut buf).await?;
+    let hlen = u64::from_le_bytes(buf);
+
+    // Read the pub header
+    let mut hser = vec![0; hlen as usize];
+    f.read_exact(&mut hser).await?;
+
+    // Build the header
+    let Header { id, tags, sec } = bincode::deserialize(&hser)?;
+
+    // Return the tags
+    Ok(tags)
+}
