63b29a3d6e1bd802ebd7b360bdb03212345aeea4
Katharina Fey <kookie@spacekookie.de>
2020-04-02 16:38:36 +0200

    libqaul: implementing dual encode mode for Tag payloads

--- libqaul/src/api/helpers/tag.rs
+++ libqaul/src/api/helpers/tag.rs
@@ -1,4 +1,9 @@
-use serde::{ser::SerializeStruct, Deserialize, Deserializer, Serialize, Serializer};
+use serde::{
+    de::{Error, MapAccess, SeqAccess, Visitor},
+    ser::SerializeStruct,
+    Deserialize, Deserializer, Serialize, Serializer,
+};
+use std::fmt;
 
 /// A generic metadata tag
 ///
@@ -10,7 +15,7 @@ use serde::{ser::SerializeStruct, Deserialize, Deserializer, Serialize, Serializ
 ///
 /// This can be used to implement things like conversation ID's,
 /// In-Reply-To, and more.
-#[derive(Clone, Debug, Hash, PartialEq, Eq, Ord, PartialOrd, Deserialize)]
+#[derive(Clone, Debug, Hash, PartialEq, Eq, Ord, PartialOrd)]
 pub struct Tag {
     /// A string key for a tag
     pub key: String,
@@ -32,30 +37,145 @@ impl Tag {
     }
 }
 
+#[derive(Clone, Debug, Hash, PartialEq, Eq, Ord, PartialOrd)]
+struct HumanVec(Vec<u8>);
+
+impl Serialize for HumanVec {
+    fn serialize<S>(&self, ser: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        if ser.is_human_readable() {
+            ser.serialize_str(
+                &hex::encode_upper(&self.0)
+                    .as_bytes()
+                    .chunks(4)
+                    .map(std::str::from_utf8)
+                    .collect::<Result<String, _>>()
+                    .unwrap(),
+            )
+        } else {
+            ser.serialize_bytes(&self.0)
+        }
+    }
+}
+
+impl<'de> Deserialize<'de> for HumanVec {
+    fn deserialize<D>(der: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        struct HumanVecVis;
+
+        impl HumanVecVis {
+            fn from_str<E: Error>(s: &str) -> Result<HumanVec, E> {
+                Self::from_bytes(&hex::decode(s).map_err(|e| E::custom(e))?)
+            }
+
+            fn from_bytes<E: Error, V: AsRef<[u8]>>(v: V) -> Result<HumanVec, E> {
+                let v = v.as_ref();
+                Ok(HumanVec(v.iter().cloned().collect()))
+            }
+        }
+
+        impl<'de> Visitor<'de> for HumanVecVis {
+            type Value = HumanVec;
+
+            fn expecting(&self, f: &mut fmt::Formatter) -> fmt::Result {
+                write!(f, "A byte array or a hex string encoded byte array",)
+            }
+
+            fn visit_borrowed_str<E: Error>(self, v: &'de str) -> Result<Self::Value, E> {
+                Self::from_str(v)
+            }
+
+            fn visit_string<E: Error>(self, v: String) -> Result<Self::Value, E> {
+                Self::from_str(&v)
+            }
+
+            fn visit_borrowed_bytes<E: Error>(self, v: &'de [u8]) -> Result<Self::Value, E> {
+                Self::from_bytes(v)
+            }
+
+            fn visit_byte_buf<E: Error>(self, v: Vec<u8>) -> Result<Self::Value, E> {
+                Self::from_bytes(v)
+            }
+
+            fn visit_seq<A>(self, mut seq: A) -> Result<Self::Value, A::Error>
+            where
+                A: SeqAccess<'de>,
+            {
+                let mut v = Vec::new();
+                while let Some(b) = seq.next_element::<u8>()? {
+                    v.push(b);
+                }
+
+                Self::from_bytes(v)
+            }
+        }
+
+        if der.is_human_readable() {
+            der.deserialize_str(HumanVecVis)
+        } else {
+            der.deserialize_bytes(HumanVecVis)
+        }
+    }
+}
+
 impl Serialize for Tag {
     fn serialize<S>(&self, ser: S) -> Result<S::Ok, S::Error>
     where
         S: Serializer,
     {
-        let human = ser.is_human_readable();
         let mut state = ser.serialize_struct("Tag", 2)?;
         state.serialize_field("key", &self.key)?;
+        state.serialize_field("val", &self.val)?;
+        state.end()
+    }
+}
 
-        if human {
-            state.serialize_field(
-                "val",
-                &hex::encode_upper(&self.val)
-                    .as_bytes()
-                    .chunks(4)
-                    .map(std::str::from_utf8)
-                    .collect::<Result<Vec<_>, _>>()
-                    .unwrap()
-                    .join(" "),
-            )?;
-        } else {
-            state.serialize_field("key", &self.val)?;
+impl<'de> Deserialize<'de> for Tag {
+    fn deserialize<D>(der: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        /// Responsible for deserialising hex-encoded payloads
+        ///
+        /// This visitor is called when the deserialiser is working
+        /// for a human readable format, such as json.
+        struct TagVisitor;
+
+        impl<'de> Visitor<'de> for TagVisitor {
+            type Value = Tag;
+
+            fn visit_seq<A>(self, mut seq: A) -> Result<Self::Value, A::Error>
+            where
+                A: SeqAccess<'de>,
+            {
+                let key: String = seq
+                    .next_element()?
+                    .ok_or_else(|| Error::invalid_length(0, &self))?;
+
+                let hvec: HumanVec = seq
+                    .next_element()?
+                    .ok_or_else(|| Error::invalid_length(0, &self))?;
+                let val: Vec<u8> = hvec.0;
+
+                Ok(Tag { key, val })
+            }
+
+            fn visit_map<A>(self, map: A) -> Result<Self::Value, A::Error>
+            where
+                A: MapAccess<'de>,
+            {
+                unimplemented!()
+            }
+
+            fn expecting(&self, fmt: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error> {
+                fmt.write_str("struct Tag { key, val }")
+            }
         }
 
-        state.end()
+        der.deserialize_struct("Tag", &["key", "val"], TagVisitor)
     }
 }
