543efa395c58d33ca29e2a767e044152677a15f1
Mathias Jud <jud@wachter-jud.net>
2020-04-29 12:29:56 +0200

    Ember REST: changed rest namespace and authentication structure

--- webgui/app/adapters/application.js
+++ webgui/app/adapters/application.js
@@ -2,14 +2,14 @@ import RESTAdapter from '@ember-data/adapter/rest';
 import { inject as service } from '@ember/service';
 
 export default class ApplicationAdapter extends RESTAdapter {
-  namespace = 'api';
+  namespace = 'rest';
 
   @service() session;
 
   get headers() {
     if(this.session.isAuthenticated) {
       return {
-        Authorization: `Bearer ${this.session.data.authenticated.token}`,
+        Authorization: `{"id":"${this.session.data.authenticated.id}","token":"${this.session.data.authenticated.token}"}`,
       }
     }
 
