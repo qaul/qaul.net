1c982139c86d4091b0eee121eb023556e6f04a54
Mathias Jud <jud@wachter-jud.net>
2020-04-30 13:29:34 +0200

    REST tests: added users for chat-rooms create test

--- tests/REST-tests/chat-rooms_create.sh
+++ tests/REST-tests/chat-rooms_create.sh
@@ -3,7 +3,8 @@
 # creates a new chat room
 # 
 # usage:
-# ./chat-rooms_create.sh
+# ./chat-rooms_create.sh "USER_ID"
 
 http POST 127.0.0.1:9900/rest/chat/rooms \
+    users:="[\"$QAUL_ID\",\"$1\"]" \
     "Authorization:{\"id\":\"$QAUL_ID\",\"token\":\"$QAUL_TOKEN\"}"
