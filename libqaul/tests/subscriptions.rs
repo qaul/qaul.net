5f4442bf97e6e852ae680d8fab2a108ed6ecfaca
Katharina Fey <kookie@spacekookie.de>
2020-05-10 17:05:51 +0200

    libqaul: adding subscription test

---
+++ libqaul/tests/subscriptions.rs
@@ -0,0 +1,53 @@
+//! libqaul message tests
+
+mod harness;
+use harness::{millis, sec10, sec5, zzz};
+
+use libqaul::{
+    helpers::TagSet,
+    messages::{Mode, MsgQuery},
+    users::UserAuth,
+    Identity, Qaul,
+};
+use std::{sync::Arc, time::Instant};
+
+async fn send_simple(q: &Arc<Qaul>, auth: &UserAuth, target: Identity) -> Identity {
+    dbg!(q
+        .messages()
+        .send(
+            auth.clone(),
+            Mode::Std(target),
+            "net.qaul.testing",
+            TagSet::empty(),
+            vec![1 as u8, 3, 1, 2],
+        )
+        .await
+        .unwrap())
+}
+
+#[async_std::test]
+async fn subscribe_one() {
+    let net = harness::init().await;
+    let auth_a = net.a().users().create("abc").await.unwrap();
+    let auth_b = net.b().users().create("abc").await.unwrap();
+
+    // The announcements need to spread
+    zzz(millis(2000)).await;
+
+    // Send a message from a
+    let id = send_simple(net.a(), &auth_a, auth_b.0).await;
+
+    let msg = harness::timeout(sec5(), async {
+        let sub = net
+            .b()
+            .messages()
+            .subscribe(auth_b.clone(), "net.qaul.testing", TagSet::empty())
+            .await
+            .unwrap();
+        sub.next().await.unwrap()
+    })
+    .await
+    .unwrap();
+
+    assert_eq!(msg.id, id);
+}
