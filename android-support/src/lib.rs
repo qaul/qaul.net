f7a4972938ef11e500830224a5b6eab0fcbbab2a
Katharina Fey <kookie@spacekookie.de>
2020-05-30 20:15:53 +0200

    android-support: mostly fiddling

--- android-support/src/lib.rs
+++ android-support/src/lib.rs
@@ -8,8 +8,8 @@ extern crate log;
 extern crate android_logger;
 
 use async_std::task::{block_on, spawn};
-use jni::objects::{JList, JObject, JString};
-use jni::sys::{jboolean, jint};
+use jni::objects::{JList, JObject, JString, JValue};
+use jni::sys::{jboolean, jint, jobject, jlong};
 use jni::JNIEnv;
 use std::{
     ffi::{CStr, CString},
@@ -34,6 +34,22 @@ fn conv_jstring(env: &JNIEnv, s: JString) -> String {
         .to_str()
         .unwrap()
         .into()
+}
+
+unsafe fn extract_state(ptr: i64) -> Arc<Qaul> {
+    let b: Box<AndroidState> = Box::from_raw(ptr as *mut AndroidState);
+    Arc::clone(&b.libqaul)
+}
+
+#[allow(unused)]
+fn get_qaul_state(env: &JNIEnv, this: JObject) -> Arc<Qaul> {
+    let class = env.get_object_class(this).unwrap();
+    let ptr = match env.get_field(*class, "libqaulState", "J") {
+        Ok(JValue::Long(l)) => l,
+        e => panic!("Failed to get JNI data: {:?}", e),
+    };
+
+    unsafe { extract_state(ptr) }
 }
 
 // Set a panic handler that will logcan print stacktraces
@@ -65,15 +81,11 @@ pub unsafe extern "C" fn Java_net_qaul_app_ffi_NativeQaul_startServer(
     this: JObject,
     port: jint,
     path: JString,
-) {
-    android_logger::init_once(
-        Config::default()
-            .with_filter(FilterBuilder::new().parse("async-std=error").build())
-            .with_min_level(Level::Trace),
-    );
+) -> jlong {
+    android_logger::init_once(Config::default().with_min_level(Level::Info));
     init_panic_handling_once();
 
-    trace!("Hello from Rust, about to bootstrap the code, yo");
+    info!("Hello from Rust, about to bootstrap the code, yo");
 
     let port = port as u16;
     let path = conv_jstring(&env, path);
@@ -83,16 +95,25 @@ pub unsafe extern "C" fn Java_net_qaul_app_ffi_NativeQaul_startServer(
         //.endpoint(EpBuilder::wifi_direct())
         .build();
 
-    trace!("Network builder done: {:?}", net);
+    info!("Network builder done: {:?}", net);
 
     let router = net.into_router();
-    trace!("Router done");
+    info!("Router done");
 
     let libqaul = Qaul::new(router);
+    block_on(async {
+        use libqaul::users::UserUpdate;
+        let auth = libqaul.users().create("1234").await.unwrap();
+        libqaul
+            .users()
+            .update(auth, UserUpdate::DisplayName(Some("Alice".to_string())))
+            .await
+            .unwrap();
+    });
 
     let chat = block_on(async { Chat::new(Arc::clone(&libqaul)).await }).unwrap();
 
-    trace!("Chat service done");
+    info!("Chat service done");
 
     let http = HttpServer::set_paths(
         path,
@@ -110,8 +131,9 @@ pub unsafe extern "C" fn Java_net_qaul_app_ffi_NativeQaul_startServer(
 
     trace!("Chat service listening done");
 
-    // let boxed = Box::new(AndroidState { libqaul });
-    // Box::into_raw(boxed) as i64
+    // Store the libqaulState variable here
+    let boxed = Box::new(AndroidState { libqaul });
+    Box::into_raw(boxed) as i64
 }
 
 #[no_mangle]
@@ -123,48 +145,59 @@ pub unsafe extern "C" fn Java_net_qaul_app_ffi_NativeQaul_checkLogin(
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn Java_net_qaul_app_ffi_NativeQaul_userRegister<'this>(
-    env: JNIEnv<'this>,
+pub unsafe extern "C" fn Java_net_qaul_app_ffi_NativeQaul_usersList(
+    env: JNIEnv,
+    this: JObject,
+    qaul: jlong,
+) -> jobject {
+    let qaul = extract_state(qaul);
+    let list: JObject<'_> = *libqaul::ffi::java::users::list(&env, qaul);
+    list.into_inner()
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn Java_net_qaul_app_ffi_NativeQaul_userRegister<'env>(
+    env: JNIEnv<'env>,
     _: JObject,
     name: JString,
     pw: JString,
-) -> JObject<'this> {
+) -> JObject<'env> {
     JObject::null()
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn Java_net_qaul_app_ffi_NativeQaul_chatList<'this>(
-    env: JNIEnv<'this>,
+pub unsafe extern "C" fn Java_net_qaul_app_ffi_NativeQaul_chatList<'env>(
+    env: JNIEnv<'env>,
     _: JObject,
-) -> JObject<'this> {
+) -> JObject<'env> {
     JObject::null()
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn Java_net_qaul_app_ffi_NativeQaul_chatStart<'this>(
-    env: JNIEnv<'this>,
+pub unsafe extern "C" fn Java_net_qaul_app_ffi_NativeQaul_chatStart<'env>(
+    env: JNIEnv<'env>,
     _: JObject,
     name: JString,
     friends: JList,
-) -> JObject<'this> {
+) -> JObject<'env> {
     JObject::null()
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn Java_net_qaul_app_ffi_NativeQaul_chatSendMessage<'this>(
-    env: JNIEnv<'this>,
+pub unsafe extern "C" fn Java_net_qaul_app_ffi_NativeQaul_chatSendMessage<'env>(
+    env: JNIEnv<'env>,
     _: JObject,
     room_id: JString,
     content: JString,
-) -> JObject<'this> {
+) -> JObject<'env> {
     JObject::null()
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn Java_net_qaul_app_ffi_NativeQaul_chatLoadMessages<'this>(
-    env: JNIEnv<'this>,
+pub unsafe extern "C" fn Java_net_qaul_app_ffi_NativeQaul_chatLoadMessages<'env>(
+    env: JNIEnv<'env>,
     _: JObject,
     room_id: JString,
-) -> JObject<'this> {
+) -> JObject<'env> {
     JObject::null()
 }
