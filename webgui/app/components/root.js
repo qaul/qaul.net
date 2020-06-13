889a623d45c035e8b9cf1e470c083b81bf7dcc1f
Lux <lux@lux.name>
2020-06-13 20:08:48 +0200

    web: remove nav & aside when not logged in

--- webgui/app/components/root.js
+++ webgui/app/components/root.js
@@ -3,6 +3,7 @@ import { inject as service } from '@ember/service';
 
 export default class RootComponent extends Component {
   @service() viewport;
+  @service() session;
 
   get layoutComponentName() {
     return `layout/${this.viewport.layout}`
