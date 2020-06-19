508899b21429df719eb2a50aa28754108b61980e
Katharina Fey <kookie@spacekookie.de>
2020-06-19 18:50:30 +0200

    Updating code to make it easier to work on

--- app/src/main/java/net/qaul/app/MainActivity.kt
+++ app/src/main/java/net/qaul/app/MainActivity.kt
@@ -25,34 +25,3 @@ class MainActivity : AppCompatActivity() {
         navView.setupWithNavController(navController)
     }
 }
-
-
-
-//package net.qaul.app
-//
-//import android.os.Bundle
-//import androidx.appcompat.app.AppCompatActivity
-//import androidx.navigation.findNavController
-//import androidx.navigation.ui.AppBarConfiguration
-//import androidx.navigation.ui.setupActionBarWithNavController
-//import androidx.navigation.ui.setupWithNavController
-//import com.google.android.material.bottomnavigation.BottomNavigationView
-//
-//class MainActivity : AppCompatActivity() {
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
-//
-//        // Setup the bottom navigator
-//        val navView: BottomNavigationView = findViewById(R.id.nav_view)
-//        val navController = findNavController(R.id.nav_host_fragment)
-//
-//        val appBarConfiguration = AppBarConfiguration(setOf(
-//                R.id.navigation_files,
-//                R.id.navigation_users,
-//                R.id.navigation_settings))
-//        setupActionBarWithNavController(navController, appBarConfiguration)
-//        navView.setupWithNavController(navController)
-//    }
-//}
