1e21abfb706a016ee00e751db350fbd86671b50d
brenodt <54450520+brenodt@users.noreply.github.com>
2021-10-25 16:12:00 -0300

    add create account placeholder

--- qaul_ui/lib/helpers/navigation_helper.dart
+++ qaul_ui/lib/helpers/navigation_helper.dart
@@ -1,27 +1,39 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:qaul_ui/screens/create_account_screen.dart';
+import 'package:qaul_ui/screens/home_screen.dart';
 
 import '../screens/splash_screen.dart';
 
 class NavigationHelper {
   static const initial = '/';
+  static const createAccount = '/createAccount';
+  static const home = '/home';
 
   static Route<T> _buildRoute<T>(
           final RouteSettings settings, final WidgetBuilder page) =>
       CupertinoPageRoute(builder: page, settings: settings);
 
   static Route<dynamic> onGenerateRoute(final RouteSettings _settings) {
+    Widget routeWidget = const SizedBox.shrink();
     switch (_settings.name) {
       case initial:
-        return _buildRoute(
-            _settings,
-            (context) => WillPopScope(
-                  onWillPop: () async => false,
-                  child: const SplashScreen(),
-                ));
+        routeWidget = WillPopScope(
+            onWillPop: () async => false, child: const SplashScreen());
+        break;
+      case createAccount:
+        routeWidget = WillPopScope(
+            onWillPop: () async => false, child: const CreateAccountScreen());
+        break;
+      case home:
+        routeWidget = WillPopScope(
+            onWillPop: () async => false, child: const HomeScreen());
+        break;
       default:
         throw ArgumentError.value(_settings.name, 'Route name',
             'Handle this route in NavigationHelper.');
     }
+
+    return _buildRoute(_settings, (context) => routeWidget);
   }
 }
---
+++ qaul_ui/lib/screens/create_account_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class CreateAccountScreen extends StatelessWidget {
+  const CreateAccountScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: Update placeholder
+    return const Scaffold(body: Center(child: Text('Create account')));
+  }
+}
