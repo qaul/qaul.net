07757950ff857c10cb3db3fa9e001b9383fd0b45
brenodt <54450520+brenodt@users.noreply.github.com>
2021-11-24 14:32:11 -0300

    (etc): remove window_manager dependency on macos for the time being

--- qaul_ui/lib/main.dart
+++ qaul_ui/lib/main.dart
@@ -1,4 +1,4 @@
-import 'dart:io';
+// import 'dart:io';
 import 'dart:ui';
 
 import 'package:adaptive_theme/adaptive_theme.dart';
--- qaul_ui/macos/Runner/MainFlutterWindow.swift
+++ qaul_ui/macos/Runner/MainFlutterWindow.swift
@@ -1,6 +1,6 @@
 import Cocoa
 import FlutterMacOS
-import window_manager
+// import window_manager
 
 class MainFlutterWindow: NSWindow {
   override func awakeFromNib() {
@@ -14,8 +14,8 @@ class MainFlutterWindow: NSWindow {
     super.awakeFromNib()
   }
 
-  override public func order(_ place: NSWindow.OrderingMode, relativeTo otherWin: Int) {
-    super.order(place, relativeTo: otherWin)
-    hiddenWindowAtLaunch()
-  }
+//   override public func order(_ place: NSWindow.OrderingMode, relativeTo otherWin: Int) {
+//     super.order(place, relativeTo: otherWin)
+//     hiddenWindowAtLaunch()
+//   }
 }
