2b67ffd3308ddf145052471ade220b7de3b7f9e6
brenodt <54450520+brenodt@users.noreply.github.com>
2021-11-30 15:19:10 -0300

    (feat): add tab & shift+tab shortcut to change tabs

--- qaul_ui/lib/providers/tab_controller_provider.dart
+++ qaul_ui/lib/providers/tab_controller_provider.dart
@@ -5,7 +5,9 @@ final selectedTabProvider = Provider((ref) => SelectedTab(initialTab: 1));
 enum TabType { account, feed, users, chat, network }
 
 class SelectedTab extends StateNotifier<int> {
-  SelectedTab({int? initialTab}) : _initialTab = initialTab ?? 0, super(initialTab ?? 0);
+  SelectedTab({int? initialTab})
+      : _initialTab = initialTab ?? 0,
+        super(initialTab ?? 0);
 
   int get index => state;
 
@@ -21,4 +23,12 @@ class SelectedTab extends StateNotifier<int> {
   }
 
   void goToTab(TabType tab) => goToIndex(TabType.values.indexOf(tab));
+
+  void goToNext() {
+    index == TabType.values.length - 1 ? goToIndex(0) : goToIndex(index + 1);
+  }
+
+  void goToPrevious() {
+    index == 0 ? goToIndex(TabType.values.length - 1) : goToIndex(index - 1);
+  }
 }
--- qaul_ui/lib/screens/home/home_screen.dart
+++ qaul_ui/lib/screens/home/home_screen.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:qaul_ui/decorators/qaul_nav_bar_decorator.dart';
 import 'package:qaul_ui/providers/providers.dart';
 import 'package:qaul_ui/screens/home/tabs/tab.dart';
 import 'package:qaul_ui/screens/home/user_account_screen.dart';
+
+class SwitchTabIntent extends Intent {
+  const SwitchTabIntent._(this.switchForward);
+
+  final bool switchForward;
+
+  factory SwitchTabIntent.forward() => const SwitchTabIntent._(true);
+
+  factory SwitchTabIntent.backward() => const SwitchTabIntent._(false);
+}
 
 class HomeScreen extends HookConsumerWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -27,18 +38,37 @@ class HomeScreen extends HookConsumerWidget {
       [tabCtrl.index],
     );
 
+    final switchForward = useCallback(() => tabCtrl.goToNext(), [UniqueKey()]);
+    final switchBack = useCallback(() => tabCtrl.goToPrevious(), [UniqueKey()]);
+
     return Scaffold(
-      body: QaulNavBarDecorator(
-        child: PageView(
-          physics: const NeverScrollableScrollPhysics(),
-          controller: pageCtrl,
-          children: [
-            const UserAccountScreen(),
-            BaseTab.feed(),
-            BaseTab.users(),
-            BaseTab.chat(),
-            BaseTab.network(),
-          ],
+      body: Shortcuts(
+        shortcuts: {
+          LogicalKeySet(LogicalKeyboardKey.tab): SwitchTabIntent.forward(),
+          LogicalKeySet(LogicalKeyboardKey.shift, LogicalKeyboardKey.tab):
+              SwitchTabIntent.backward(),
+        },
+        child: Actions(
+          actions: {
+            SwitchTabIntent: CallbackAction<SwitchTabIntent>(
+              onInvoke: (intent) =>
+                  intent.switchForward ? switchForward() : switchBack(),
+            ),
+          },
+          child: QaulNavBarDecorator(
+            child: PageView(
+              controller: pageCtrl,
+              allowImplicitScrolling: true,
+              physics: const NeverScrollableScrollPhysics(),
+              children: [
+                const UserAccountScreen(),
+                BaseTab.feed(),
+                BaseTab.users(),
+                BaseTab.chat(),
+                BaseTab.network(),
+              ],
+            ),
+          ),
         ),
       ),
     );
