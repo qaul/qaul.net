44aae45fd65a6529525456d296970a27de832b92
Mathias Jud <jud@wachter-jud.net>
2021-12-10 09:14:18 +0100

    removed flutter folder

--- flutter/lib/screens/info.dart
+++
@@ -1,3 +0,0 @@
-// Copyright (c) 2021 Open Community Project Association https://ocpa.ch
-// This software is published under the AGPLv3 license.
-
--- flutter/libqaul_plugin/CHANGELOG.md
+++
@@ -1,5 +0,0 @@
-## 0.0.1
-
-* Initial flutter platform plugins of libqaul for
-  * iOS
-  * MacOS
