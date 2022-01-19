91633f99b06cd472c7ba592cacb274b8ed35553c
brenodt <54450520+brenodt@users.noreply.github.com>
2022-01-19 17:25:07 -0300

    feat: add support screen and enable sending logs

--- qaul_ui/lib/helpers/navigation_helper.dart
+++ qaul_ui/lib/helpers/navigation_helper.dart
@@ -3,6 +3,7 @@ import 'package:qaul_ui/screens/about_screen.dart';
 import 'package:qaul_ui/screens/create_account_screen.dart';
 import 'package:qaul_ui/screens/home/home_screen.dart';
 import 'package:qaul_ui/screens/settings_screen.dart';
+import 'package:qaul_ui/screens/support_screen.dart';
 
 import '../screens/splash_screen.dart';
 
@@ -12,25 +13,22 @@ class NavigationHelper {
   static const home = '/home';
   static const settings = '/settings';
   static const about = '/about';
+  static const support = '/support';
 
-  static Route<T> _buildRoute<T>(
-          final RouteSettings settings, final WidgetBuilder page) =>
+  static Route<T> _buildRoute<T>(final RouteSettings settings, final WidgetBuilder page) =>
       CupertinoPageRoute(builder: page, settings: settings);
 
   static Route<dynamic> onGenerateRoute(final RouteSettings _settings) {
     Widget routeWidget = const SizedBox.shrink();
     switch (_settings.name) {
       case initial:
-        routeWidget = WillPopScope(
-            onWillPop: () async => false, child: SplashScreen());
+        routeWidget = WillPopScope(onWillPop: () async => false, child: SplashScreen());
         break;
       case createAccount:
-        routeWidget = WillPopScope(
-            onWillPop: () async => false, child: CreateAccountScreen());
+        routeWidget = WillPopScope(onWillPop: () async => false, child: CreateAccountScreen());
         break;
       case home:
-        routeWidget = WillPopScope(
-            onWillPop: () async => false, child: const HomeScreen());
+        routeWidget = WillPopScope(onWillPop: () async => false, child: const HomeScreen());
         break;
       case settings:
         routeWidget = const SettingsScreen();
@@ -38,9 +36,12 @@ class NavigationHelper {
       case about:
         routeWidget = const AboutScreen();
         break;
+      case support:
+        routeWidget = const SupportScreen();
+        break;
       default:
-        throw ArgumentError.value(_settings.name, 'Route name',
-            'Handle this route in NavigationHelper.');
+        throw ArgumentError.value(
+            _settings.name, 'Route name', 'Handle this route in NavigationHelper.');
     }
 
     return _buildRoute(_settings, (context) => routeWidget);
