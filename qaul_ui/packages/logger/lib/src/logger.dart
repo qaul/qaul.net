f8def143ee9fab6bef87d16d54c45a8b9571a244
brenodt <54450520+brenodt@users.noreply.github.com>
2022-02-22 09:23:50 -0300

    refactor: Add voidLogger when running app in debug mode - enable seeing exceptions on console

--- qaul_ui/packages/logger/lib/src/logger.dart
+++ qaul_ui/packages/logger/lib/src/logger.dart
@@ -1,7 +1,9 @@
+import 'package:flutter/foundation.dart';
+
 import 'email_logger.dart';
 
 abstract class Logger {
-  static final instance = EmailLogger();
+  static final instance = kDebugMode ? _VoidLogger() : EmailLogger();
 
   Future<void> initialize();
 
@@ -20,4 +22,32 @@ abstract class Logger {
   Future<void> sendLogs();
 
   Future<void> deleteLogs();
+}
+
+class _VoidLogger implements Logger {
+  @override
+  bool loggingEnabled = false;
+
+  @override
+  Future<void> deleteLogs() async {}
+
+  @override
+  Future<bool> get hasLogsStored async => false;
+
+  @override
+  Future<void> initialize() async {}
+
+  @override
+  Future<void> logError(Object error, StackTrace stack,
+      {String? message}) async {}
+
+  @override
+  Future<void> logException(Exception e, StackTrace stack,
+      {String? message}) async {}
+
+  @override
+  Future<String> get logStorageSize async => '';
+
+  @override
+  Future<void> sendLogs() async {}
 }
