54aadf0984cee3697fdd8415513a0601c8382420
Mathias Jud <jud@wachter-jud.net>
2022-02-23 18:53:20 +0100

    Put qaul community on Port 9339 for Testing

--- rust/libqaul/src/storage/configuration.rs
+++ rust/libqaul/src/storage/configuration.rs
@@ -72,9 +72,9 @@ impl Default for Internet {
         Internet {
             active: true,
             #[cfg(any(target_os = "android", target_os = "ios"))]
-            peers: vec![String::from("/ip4/144.91.74.192/tcp/9229"); 1],
+            peers: vec![String::from("/ip4/144.91.74.192/tcp/9339"); 1],
             #[cfg(not(any(target_os = "android", target_os = "ios")))]
-            peers: vec![String::from("/ip4/144.91.74.192/tcp/9229"); 1],
+            peers: vec![String::from("/ip4/144.91.74.192/tcp/9339"); 1],
             //peers: Vec::new(),
             do_listen: false,
             #[cfg(any(target_os = "android", target_os = "ios"))]
