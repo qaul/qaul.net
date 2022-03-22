0a37ce269db72fef0e08fab579b73f5c72493068
brenodt <54450520+brenodt@users.noreply.github.com>
2022-03-22 10:22:17 -0300

    feat: add labels to filter buttons, update "all" filter button icon

--- qaul_ui/lib/screens/home/dynamic_network/dynamic_network_screen.dart
+++ qaul_ui/lib/screens/home/dynamic_network/dynamic_network_screen.dart
@@ -12,6 +12,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:forge2d/forge2d.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:intersperse/intersperse.dart';
--- qaul_ui/lib/screens/home/dynamic_network/widgets/network_type_filter.dart
+++ qaul_ui/lib/screens/home/dynamic_network/widgets/network_type_filter.dart
@@ -50,6 +50,7 @@ class _NetworkTypeFilterToolbar extends HookConsumerWidget {
     final buttons = List.generate(NetworkTypeFilter.values.length, (i) {
       final filter = NetworkTypeFilter.values[i];
       return filterButton(
+        context,
         filter: filter,
         backgroundColor: bgColorFor(filter),
         onTap: () => ref.read(_networkTypeFilter.notifier).state = filter,
@@ -67,22 +68,26 @@ class _NetworkTypeFilterToolbar extends HookConsumerWidget {
     );
   }
 
-  Widget filterButton({
+  Widget filterButton(
+    BuildContext context, {
     required NetworkTypeFilter filter,
     required Color backgroundColor,
     required VoidCallback? onTap,
   }) {
     return GestureDetector(
       onTap: onTap,
-      child: CircleAvatar(
-        foregroundColor: Colors.white,
-        backgroundColor: backgroundColor,
-        child: iconFor(filter),
+      child: Tooltip(
+        message: labelFor(filter),
+        child: CircleAvatar(
+          foregroundColor: Colors.white,
+          backgroundColor: backgroundColor,
+          child: iconFor(filter, context: context),
+        ),
       ),
     );
   }
 
-  Widget iconFor(NetworkTypeFilter filter) {
+  Widget iconFor(NetworkTypeFilter filter, {required BuildContext context}) {
     switch (filter) {
       case NetworkTypeFilter.bluetooth:
         return const Icon(Icons.bluetooth);
@@ -91,7 +96,23 @@ class _NetworkTypeFilterToolbar extends HookConsumerWidget {
       case NetworkTypeFilter.internet:
         return const Icon(CupertinoIcons.globe);
       case NetworkTypeFilter.all:
-        return const Icon(Icons.mediation);
+        return SvgPicture.asset(
+          'assets/icons/network.svg',
+          color: Theme.of(context).iconTheme.color,
+        );
+    }
+  }
+
+  String labelFor(NetworkTypeFilter filter) {
+    switch (filter) {
+      case NetworkTypeFilter.bluetooth:
+        return 'Bluetooth';
+      case NetworkTypeFilter.lan:
+        return 'Local';
+      case NetworkTypeFilter.internet:
+        return 'Wi-fi';
+      case NetworkTypeFilter.all:
+        return 'All';
     }
   }
 }
