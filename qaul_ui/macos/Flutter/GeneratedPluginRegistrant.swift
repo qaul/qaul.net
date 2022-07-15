24a8cc6642467a72dcaa218241793d5824a495f1
brenodt <54450520+brenodt@users.noreply.github.com>
2022-07-15 09:26:14 -0300

    refactor: simplify nav bar implementation

--- qaul_ui/macos/Flutter/GeneratedPluginRegistrant.swift
+++ qaul_ui/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,7 +8,7 @@ import Foundation
 import device_info_plus_macos
 import flutter_app_badger
 import flutter_local_notifications
-import native_context_menu
+import macos_ui
 import package_info_plus_macos
 import path_provider_macos
 import shared_preferences_macos
@@ -18,7 +18,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FlutterAppBadgerPlugin.register(with: registry.registrar(forPlugin: "FlutterAppBadgerPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
-  NativeContextMenuPlugin.register(with: registry.registrar(forPlugin: "NativeContextMenuPlugin"))
+  MacOSUiPlugin.register(with: registry.registrar(forPlugin: "MacOSUiPlugin"))
   FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
--- qaul_ui/windows/flutter/generated_plugin_registrant.cc
+++ qaul_ui/windows/flutter/generated_plugin_registrant.cc
@@ -6,12 +6,9 @@
 
 #include "generated_plugin_registrant.h"
 
-#include <native_context_menu/native_context_menu_plugin.h>
 #include <url_launcher_windows/url_launcher_windows.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
-  NativeContextMenuPluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("NativeContextMenuPlugin"));
   UrlLauncherWindowsRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("UrlLauncherWindows"));
 }
