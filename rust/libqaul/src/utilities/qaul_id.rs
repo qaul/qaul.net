705b88de11e41e814987e4c565dca262ef1aa813
Mathias Jud <jud@wachter-jud.net>
2022-08-15 08:26:19 +0200

    q8id new 8 Byte representation of the qaul user ID
    q8id is used for the routing table, to reduce bandwith

--- rust/libqaul/src/utilities/qaul_id.rs
+++ rust/libqaul/src/utilities/qaul_id.rs
@@ -2,19 +2,22 @@
 // This software is published under the AGPLv3 license.
 
 //! # qaul ID
-//! 
-//! Conversations and operations on the qaul ID
+//!
+//! Conversions of the qaul ID
+//!
+//! * qaul ID -> small version
+//!   * add prefix to small version for data base search
+//! * qaul ID -> q8id
+//!   * add prefix to q8id for data base search
 
 use libp2p::PeerId;
 
 /// Qaul ID structure
-pub struct QaulId {
-
-}
+pub struct QaulId {}
 
 impl QaulId {
     /// Convert a qaul ID to it's small 16 Byte version
-    /// 
+    ///
     /// The small version is used in the BLE module.
     /// It only uses byte 7 to 24 of the qaul id
     pub fn to_small(qaul_id: PeerId) -> Vec<u8> {
@@ -35,5 +38,119 @@ impl QaulId {
         key.extend(small_id);
 
         key
+    }
+
+    /// Convert a qaul user ID to it's 8 Byte ID (q8id)
+    ///
+    /// The 8 Byte user ID (q8id) is used for the routing protocol.
+    /// It only uses byte 7 to 14 of the qaul id.
+    /// Bytes 1-6 are always the same, as they declare the hash kind.
+    pub fn to_q8id(qaul_id: PeerId) -> Vec<u8> {
+        // convert to bytes
+        let bytes = qaul_id.to_bytes();
+
+        Self::bytes_to_q8id(bytes)
+    }
+
+    /// Converts the binary form of the qaul user ID to the 8 Byte ID (q8id)
+    pub fn bytes_to_q8id(bytes: Vec<u8>) -> Vec<u8> {
+        // only use bytes 7 to 14
+        let q8id = bytes[6..14].to_owned();
+
+        // return small id
+        q8id
+    }
+
+    /// Create a search key prefix for a small vector
+    pub fn q8id_to_search_prefix(q8id: Vec<u8>) -> Vec<u8> {
+        // add chopped prefix to small_id
+        let mut key: Vec<u8> = vec![0x0, 0x24, 0x8, 0x1, 0x12, 0x20];
+        key.extend(q8id);
+
+        key
+    }
+
+    /// Create the search range for the qaul ID from an q8id
+    ///
+    /// This function returns a tuple with two qaul ID's for
+    /// the range selection.
+    /// (lowest_id, highest_id)
+    ///
+    /// The range can be used to search for the qaul ID in a BTreeMap.
+    ///
+    /// A qaul ID has 38 bytes.
+    /// {prefix}{q8id}{24 bytes}
+    pub fn q8id_to_search_range(q8id: Vec<u8>) -> Result<(PeerId, PeerId), String> {
+        // add prefix to q8id
+        let prefix = Self::q8id_to_search_prefix(q8id);
+
+        // create lower range
+        let low_suffix: Vec<u8> = vec![
+            0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+            0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+        ];
+        let mut lowest = prefix.clone();
+        lowest.extend(low_suffix);
+
+        // create higher range
+        let high_suffix: Vec<u8> = vec![
+            0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+            0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+        ];
+        let mut highest = prefix;
+        highest.extend(high_suffix);
+
+        // convert bytes to PeerId
+        let lowest_result = PeerId::from_bytes(&lowest);
+        let highest_result = PeerId::from_bytes(&lowest);
+
+        if let (Ok(lowest_id), Ok(highest_id)) = (lowest_result, highest_result) {
+            return Ok((lowest_id, highest_id));
+        }
+
+        Err("q8id range conversion failed".to_string())
+    }
+
+    /// create conversation ID for a direct chat
+    ///
+    /// this is a predictable 16 Bytes UUID
+    /// which consists of the q8id of both users.
+    ///
+    /// {smaller q8id}{bigger q8id}
+    pub fn create_conversation_id(qaul_id_1: PeerId, qaul_id_2: PeerId) -> Vec<u8> {
+        let q8id_1 = Self::to_q8id(qaul_id_1);
+        let q8id_2 = Self::to_q8id(qaul_id_2);
+
+        Self::create_conversation_id_from_q8ids(q8id_1, q8id_2)
+    }
+
+    /// create the conversation ID for a direct chat from the q8id's of the users
+    pub fn create_conversation_id_from_q8ids(q8id_1: Vec<u8>, q8id_2: Vec<u8>) -> Vec<u8> {
+        log::info!("q8id_1 {:?}", q8id_1.clone());
+        log::info!("q8id_2 {:?}", q8id_2.clone());
+
+        let mut ids: Vec<Vec<u8>> = Vec::new();
+        ids.push(q8id_1);
+        ids.push(q8id_2);
+
+        ids.sort();
+
+        let mut conversation_id: Vec<u8> = Vec::new();
+        conversation_id.extend(ids[0].clone());
+        conversation_id.extend(ids[1].clone());
+
+        log::info!("conversation_id: {:?}", conversation_id.clone());
+
+        conversation_id
+    }
+
+    /// checks if the group id is a direct conversation
+    ///
+    /// one needs to provide the own user account ID
+    ///
+    /// returns None if the group is not a direct conversation ID
+    /// returns the q8id of the other party, if it is a direct conversation
+    pub fn conversation_to_q8id(_account_id: PeerId) -> Option<Vec<u8>> {
+        None
     }
 }
