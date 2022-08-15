13a213d9f5a1d13c5f0cea44ca8cfc7cd9d53e25
brenodt <54450520+brenodt@users.noreply.github.com>
2022-08-15 11:31:49 -0300

    fix: display description over the shared image message

---
+++ qaul_ui/lib/screens/home/tabs/chat/widgets/image_message_widget.dart
@@ -0,0 +1,152 @@
+part of 'chat.dart';
+
+class ImageMessageWidget extends StatefulWidget {
+  const ImageMessageWidget({
+    Key? key,
+    required this.message,
+    required this.messageWidth,
+    this.isDefaultUser = false,
+  }) : super(key: key);
+
+  final types.ImageMessage message;
+
+  final int messageWidth;
+
+  final bool isDefaultUser;
+
+  @override
+  _ImageMessageWidgetState createState() => _ImageMessageWidgetState();
+}
+
+class _ImageMessageWidgetState extends State<ImageMessageWidget> {
+  ImageProvider? _image;
+  ImageStream? _stream;
+  Size _size = const Size(0, 0);
+
+  @override
+  void initState() {
+    super.initState();
+    _image = Conditional().getProvider(widget.message.uri);
+    _size = Size(widget.message.width ?? 0, widget.message.height ?? 0);
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    if (_size.isEmpty) {
+      _getImage();
+    }
+  }
+
+  void _getImage() {
+    final oldImageStream = _stream;
+    _stream = _image?.resolve(createLocalImageConfiguration(context));
+    if (_stream?.key == oldImageStream?.key) {
+      return;
+    }
+    final listener = ImageStreamListener(_updateImage);
+    oldImageStream?.removeListener(listener);
+    _stream?.addListener(listener);
+  }
+
+  void _updateImage(ImageInfo info, bool _) {
+    setState(() {
+      _size = Size(
+        info.image.width.toDouble(),
+        info.image.height.toDouble(),
+      );
+    });
+  }
+
+  @override
+  void dispose() {
+    _stream?.removeListener(ImageStreamListener(_updateImage));
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var color = widget.isDefaultUser ? Colors.lightBlue.shade700 : Colors.white;
+
+    var style = TextStyle(
+      color: widget.isDefaultUser ? Colors.white : Colors.black,
+    );
+
+    Widget image;
+    if (_size.aspectRatio == 0) {
+      image = Container(color: color, height: _size.height, width: _size.width);
+    } else if (_size.aspectRatio < 0.1 || _size.aspectRatio > 10) {
+      image = Container(
+        color: color,
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Container(
+              height: 64,
+              margin: const EdgeInsets.all(16),
+              width: 64,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(15),
+                child: Image(
+                  fit: BoxFit.cover,
+                  image: _image!,
+                ),
+              ),
+            ),
+            Flexible(
+              child: Container(
+                margin: const EdgeInsets.all(12),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      widget.message.name,
+                      style: style,
+                      textWidthBasis: TextWidthBasis.longestLine,
+                    ),
+                    Container(
+                      margin: const EdgeInsets.only(
+                        top: 4,
+                      ),
+                      child: Text(
+                        filesize(widget.message.size.truncate()),
+                        style: style,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+    } else {
+      image = Container(
+        constraints: BoxConstraints(
+          maxHeight: widget.messageWidth.toDouble(),
+          minWidth: 170,
+        ),
+        child: AspectRatio(
+          aspectRatio: _size.aspectRatio > 0 ? _size.aspectRatio : 1,
+          child: Image(
+            fit: BoxFit.contain,
+            image: _image!,
+          ),
+        ),
+      );
+    }
+
+    String? description = widget.message.metadata?['description'];
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        image,
+        if (description != null) ...[
+          const SizedBox(height: 12),
+          Text(description, style: style),
+          const SizedBox(height: 8),
+        ],
+      ],
+    );
+  }
+}
