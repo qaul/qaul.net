f3e4ceed52ef9bb4e42427219ef1d68354aa81dd
brenodt <54450520+brenodt@users.noreply.github.com>
2022-08-29 13:22:45 -0300

    feat: [Flutter] compress images only if they're greater than 150kb, compression occurs on its own isolate

--- qaul_ui/packages/utils/lib/src/image_manipulation.dart
+++ qaul_ui/packages/utils/lib/src/image_manipulation.dart
@@ -1,25 +1,57 @@
 import 'dart:io';
+import 'dart:isolate';
 import 'dart:math' as math;
 
 import 'package:image/image.dart';
 import 'package:mime/mime.dart';
 import 'package:path_provider/path_provider.dart';
 
-
 bool isImage(String filePath) {
   String? mimeStr = lookupMimeType(filePath);
   return (mimeStr != null && RegExp('image/.*').hasMatch(mimeStr));
 }
 
-Future<File?> compressImage(File file, {int quality = 70}) async {
+/// Resizes image to Full HD (maintaining aspect ratio) if width is greater than
+/// 1920, and compresses it to a JPEG 85% quality.
+Future<File?> processImage(File file) async {
   assert(isImage(file.path));
+  ReceivePort receivePort = ReceivePort();
+  await Isolate.spawn(_resizeAndCompressImage, _ProcessParam(file, receivePort.sendPort));
+
+  // Get the processed image from the isolate.
+  List<int>? imageBytes = await receivePort.first;
+
+  if (imageBytes == null) return null;
+
   final tempDir = await getTemporaryDirectory();
   final path = tempDir.path;
   int rand = math.Random().nextInt(10000);
 
-  var image = decodeImage(file.readAsBytesSync());
-  if (image == null) return null;
+  return File('$path/img_$rand.jpg')..writeAsBytesSync(imageBytes);
+}
 
-  return File('$path/img_$rand.jpg')
-    ..writeAsBytesSync(encodeJpg(image, quality: quality.clamp(0, 100)));
+class _ProcessParam {
+  _ProcessParam(this.file, this.sendPort);
+
+  final File file;
+  final SendPort sendPort;
+}
+
+void _resizeAndCompressImage(_ProcessParam param) {
+  // Read an image from file.
+  // decodeImage will identify the format of the image and use the appropriate
+  // decoder.
+  var image = decodeImage(param.file.readAsBytesSync());
+  if (image == null) {
+    param.sendPort.send(null);
+    return;
+  }
+
+  if (image.width > 1920) {
+    // Resize the image to Full HD 1920x? (maintaining the aspect ratio).
+    image = copyResize(image, width: 1920);
+  }
+
+  // Send back encoded jpg image
+  param.sendPort.send(encodeJpg(image, quality: 85));
 }
