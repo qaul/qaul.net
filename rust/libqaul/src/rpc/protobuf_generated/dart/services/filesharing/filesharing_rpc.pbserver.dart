3e22d7a907c312c8fc6b32be64328baf97641158
brenodt <54450520+brenodt@users.noreply.github.com>
2022-08-30 14:19:49 -0300

    chore: remove duplicated generated dart files from rust folder, update build script to only generate the files contained within the flutter folder

--- rust/libqaul/src/rpc/protobuf_generated/dart/services/filesharing/filesharing_rpc.pbserver.dart
+++
@@ -1,9 +0,0 @@
-///
-//  Generated code. Do not modify.
-//  source: services/filesharing/filesharing_rpc.proto
-//
-// @dart = 2.12
-// ignore_for_file: annotate_overrides,camel_case_types,unnecessary_const,non_constant_identifier_names,library_prefixes,unused_import,unused_shown_name,return_of_invalid_type,unnecessary_this,prefer_final_fields,deprecated_member_use_from_same_package
-
-export 'filesharing_rpc.pb.dart';
-
