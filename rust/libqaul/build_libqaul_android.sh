137a09f354d5ab873629d00503de926f2430eefd
Mathias Jud <jud@wachter-jud.net>
2022-10-19 14:21:59 +0200

    Updated build script to build libqaul on macos & windows

--- rust/libqaul/build_libqaul_android.sh
+++ rust/libqaul/build_libqaul_android.sh
@@ -49,33 +49,12 @@ rustup target add \
   i686-linux-android \
   x86_64-linux-android
 
-# 2. Define linker variables
-PREBUILT_BINARIES="$(find "$ANDROID_NDK_HOME/toolchains/llvm/prebuilt" -maxdepth 1 -mindepth 1 -type d )/bin"
-echo "Using prebuilt binaries found in '$PREBUILT_BINARIES'"
+# 2. make sure cargo-ndk is installed
+cargo install cargo-ndk
 
-AARCH64_LINKER="$PREBUILT_BINARIES/aarch64-linux-android26-clang"
-ARMV7_LINKER="$PREBUILT_BINARIES/armv7a-linux-androideabi26-clang"
-I686_LINKER="$PREBUILT_BINARIES/i686-linux-android26-clang"
-X86_64_LINKER="$PREBUILT_BINARIES/x86_64-linux-android26-clang"
-
-# 3. Set Path to all NDK binaries for some of the additional packages such as 'ring'
-export PATH="$PREBUILT_BINARIES:$PATH"
-
-# 4. Build
-CARGO_TARGET_AARCH64_LINUX_ANDROID_LINKER=$AARCH64_LINKER cargo build --target=aarch64-linux-android ${buildTypeCargo}
-CARGO_TARGET_ARMV7_LINUX_ANDROIDEABI_LINKER=$ARMV7_LINKER cargo build --target=armv7-linux-androideabi ${buildTypeCargo}
-CARGO_TARGET_I686_LINUX_ANDROID_LINKER=$I686_LINKER cargo build --target=i686-linux-android ${buildTypeCargo}
-AR=llvm-ar CARGO_TARGET_X86_64_LINUX_ANDROID_LINKER=$X86_64_LINKER cargo build --target=x86_64-linux-android ${buildTypeCargo}
-
-# Copy to flutter shared library location for android
+# 3. clean jni location
 jniLibs=../../android/libqaul/src/main/jniLibs
 rm -rf ${jniLibs}
-mkdir ${jniLibs}
-mkdir -p ${jniLibs}/arm64-v8a
-mkdir -p ${jniLibs}/armeabi-v7a
-mkdir -p ${jniLibs}/x86
-mkdir -p ${jniLibs}/x86_64
-cp ../target/aarch64-linux-android/${buildType}/${libName} ${jniLibs}/arm64-v8a/${libName}
-cp ../target/armv7-linux-androideabi/${buildType}/${libName} ${jniLibs}/armeabi-v7a/${libName}
-cp ../target/i686-linux-android/${buildType}/${libName} ${jniLibs}/x86/${libName}
-cp ../target/x86_64-linux-android/${buildType}/${libName} ${jniLibs}/x86_64/${libName}
+
+# 4. build libqaul for all targets
+cargo ndk -t armeabi-v7a -t arm64-v8a -t x86 -t x86_64  -o $jniLibs build ${buildTypeCargo}
