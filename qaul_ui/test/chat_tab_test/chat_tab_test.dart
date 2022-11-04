3198b35abc0fccd16b2db924b6b891bab199ecc5
brenodt <54450520+brenodt@users.noreply.github.com>
2022-11-04 15:43:44 -0300

    feat: add open chat test

--- qaul_ui/test/chat_tab_test/chat_tab_test.dart
+++ qaul_ui/test/chat_tab_test/chat_tab_test.dart
@@ -5,6 +5,7 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:local_notifications/src/local_notifications.dart';
 import 'package:qaul_rpc/qaul_rpc.dart';
 import 'package:qaul_ui/providers/providers.dart';
+import 'package:qaul_ui/screens/home/tabs/chat/widgets/chat.dart';
 import 'package:qaul_ui/screens/home/tabs/tab.dart';
 import 'package:qaul_ui/widgets/widgets.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -75,6 +76,46 @@ void main() {
     await expectLater(
       find.byKey(chatKey),
       matchesGoldenFile('goldens/chatGolden_withGroupRoom_$sizeName.png'),
+    );
+  });
+
+  testResponsiveWidgets('opening a group chat', (tester) async {
+    final wut = ProviderScope(
+      overrides: [
+        defaultUserProvider.overrideWithValue(
+          StateController(defaultUser),
+        ),
+        chatNotificationControllerProvider.overrideWithValue(
+          NullChatNotificationController(),
+        ),
+        chatRoomsProvider.overrideWithValue(
+          ChatRoomListNotifier(rooms: [groupChat]),
+        ),
+        currentOpenChatRoom.overrideWithProvider(StateProvider((_) => null)),
+        qaulWorkerProvider.overrideWithValue(StubLibqaulWorker())
+      ],
+      child: materialAppWithLocalizations(BaseTab.chat(key: chatKey)),
+    );
+
+    await tester.pumpWidget(wut);
+
+    var chatRoomTileFinder = find.byType(QaulListTile);
+    expect(
+      chatRoomTileFinder,
+      findsOneWidget,
+      reason: 'one chat room available',
+    );
+
+    expect(find.byType(ChatScreen), findsNothing, reason: 'no open chats');
+    await tester.tap(chatRoomTileFinder);
+    await tester.pumpAndSettle();
+    expect(find.byType(ChatScreen), findsOneWidget, reason: 'one open chat');
+  }, goldenCallback: (sizeName, tester) async {
+    await expectLater(
+      find.byType(ChatScreen),
+      matchesGoldenFile(
+        'goldens/chatGolden_withGroupRoom_openChat_$sizeName.png',
+      ),
     );
   });
 }
