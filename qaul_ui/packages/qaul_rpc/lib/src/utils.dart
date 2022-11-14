4144fb26f9053379e4090802d27bd4a9c349c11e
brenodt <54450520+brenodt@users.noreply.github.com>
2022-11-14 17:39:09 -0300

    refactor: extract repeating code from File classes in RPC

--- qaul_ui/packages/qaul_rpc/lib/src/utils.dart
+++ qaul_ui/packages/qaul_rpc/lib/src/utils.dart
@@ -1,6 +1,12 @@
 import 'dart:io';
 
-Future<String?> findFolderWithFilesOfExtension(Directory root, String extension) async {
+import 'package:flutter/cupertino.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import '../qaul_rpc.dart';
+
+Future<String?> findFolderWithFilesOfExtension(
+    Directory root, String extension) async {
   for (final FileSystemEntity entity in root.listSync()) {
     if (entity is File && entity.path.endsWith(extension)) {
       return root.path;
@@ -10,4 +16,18 @@ Future<String?> findFolderWithFilesOfExtension(Directory root, String extension)
     }
   }
   return null;
+}
+
+abstract class FilePathResolverMixin {
+  @protected
+  String getFilePath(
+    Reader read, {
+    required String id,
+    required String extension,
+  }) {
+    var storagePath = read(libqaulLogsStoragePath)!.replaceAll('/logs', '');
+    var uuid = read(defaultUserProvider)!.idBase58;
+
+    return '$storagePath/$uuid/files/$id.$extension';
+  }
 }
