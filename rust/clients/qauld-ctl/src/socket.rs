892bc38fee25abe463ab108ab651308a4de050a4
Harshil Jani <harshiljani2002@gmail.com>
2023-03-30 17:27:18 +0530

    secure the socket by setting file permissions
    Signed-off-by: Harshil Jani <harshiljani2002@gmail.com>

--- rust/clients/qauld-ctl/src/socket.rs
+++ rust/clients/qauld-ctl/src/socket.rs
@@ -1,9 +1,19 @@
 use std::os::unix::net::UnixListener;
 use std::os::unix::net::UnixStream;
 use std::io::{Read, Write};
+use std::fs::{self, Permissions};
+use std::os::unix::fs::PermissionsExt;
 
 pub fn run_socket() {
-    let listener = UnixListener::bind("/tmp/qauld.sock").expect("Unable to bind to Unix socket");
+
+    let socket_dir = "/tmp/";
+    let socket_file = "/tmp/qauld.sock";
+    // 0700 (read, write, execute for owner only)
+    fs::set_permissions(socket_dir, Permissions::from_mode(0o700));
+    // 0600 (read, write for owner only)
+    fs::set_permissions(socket_file, Permissions::from_mode(0o600));
+    
+    let listener = UnixListener::bind(socket_file).expect("Unable to bind to Unix socket");
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream) => {
