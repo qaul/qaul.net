2e533c988bba97f8cc8f1d4b0af96e2a8ae9ca2e
6d7a <westphal@consider-it.de>
2023-04-14 14:27:44 +0200

    refactor(linux-ble): remove main.rs and pass callback to init

--- rust/ble_module/src/lib.rs
+++ rust/ble_module/src/lib.rs
@@ -4,39 +4,48 @@
 //! # qaul BLE Module for Linux
 //!
 //! qaul BLE module for Linux
-
+#[macro_use]
+extern crate log;
 use crate::ble::ble_service::IdleBleService;
-use async_std::task::spawn;
+use async_std::{stream::StreamExt, task::spawn};
 use futures::executor::block_on;
-use rpc::msg_loop::listen_for_sys_msgs;
+use rpc::{msg_loop::listen_for_sys_msgs, utils::BleResultSender};
 use std::thread;
 
 mod ble;
 pub mod rpc;
 
 /// initialize and start the ble_module in an own thread
-pub fn init() {
+pub fn init(sys_rpc_callback: Box<dyn FnMut(Vec<u8>) + Send>) {
     // Spawn new thread
     thread::spawn(move || {
         block_on(async move {
             // start BLE module main loop
-            main_loop().await;
+            main_loop(sys_rpc_callback).await;
         })
     });
 }
 
 /// Start the setup and main loop of this library
-async fn main_loop() {
+async fn main_loop(mut sys_rpc_callback: Box<dyn FnMut(Vec<u8>) + Send>) {
     let rpc_receiver = rpc::init();
     let ble_service = IdleBleService::new().await.unwrap_or_else(|err| {
-        log::error!("{:#?}", err);
+        error!("{:#?}", err);
         std::process::exit(1);
     });
 
-    // listen_for_sys_msgs(rpc_receiver, ble_service)
-    //     .await
-    //     .unwrap_or_else(|err| {
-    //         log::error!("{:#?}", err);
-    //         std::process::exit(1);
-    //     });
+    let (tx, mut rx) = async_std::channel::unbounded::<Vec<u8>>();
+
+    spawn(async move {
+        while let Some(result) = rx.next().await {
+            sys_rpc_callback(result)
+        }
+    });
+
+    listen_for_sys_msgs(rpc_receiver, ble_service, BleResultSender::new(tx))
+        .await
+        .unwrap_or_else(|err| {
+            error!("{:#?}", err);
+            std::process::exit(1);
+        });
 }
