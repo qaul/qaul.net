fbd2921b63477d7f9e8e38024a6faf9a6d289d90
6d7a <westphal@consider-it.de>
2023-04-14 16:35:01 +0200

    refactor(ble_module): run in tokio runtime

--- rust/ble_module/src/lib.rs
+++ rust/ble_module/src/lib.rs
@@ -8,27 +8,33 @@
 extern crate log;
 use crate::ble::ble_service::IdleBleService;
 use async_std::{stream::StreamExt, task::spawn};
-use futures::executor::block_on;
-use rpc::{msg_loop::listen_for_sys_msgs, utils::BleResultSender};
+use rpc::{msg_loop::listen_for_sys_msgs, utils::BleResultSender, BleRpc};
 use std::thread;
+use tokio::runtime;
 
 mod ble;
 pub mod rpc;
 
 /// initialize and start the ble_module in an own thread
 pub fn init(sys_rpc_callback: Box<dyn FnMut(Vec<u8>) + Send>) {
+    let rpc_receiver = rpc::init();
+
     // Spawn new thread
     thread::spawn(move || {
-        block_on(async move {
+        let rt = runtime::Builder::new_current_thread()
+            .enable_all()
+            .build()
+            .expect("Failed to create BLE module tokio runtime!");
+
+        rt.block_on(async move {
             // start BLE module main loop
-            main_loop(sys_rpc_callback).await;
+            main_loop(sys_rpc_callback, rpc_receiver).await;
         })
     });
 }
 
 /// Start the setup and main loop of this library
-async fn main_loop(mut sys_rpc_callback: Box<dyn FnMut(Vec<u8>) + Send>) {
-    let rpc_receiver = rpc::init();
+async fn main_loop(mut sys_rpc_callback: Box<dyn FnMut(Vec<u8>) + Send>, ble_rpc_receiver: BleRpc) {
     let ble_service = IdleBleService::new().await.unwrap_or_else(|err| {
         error!("{:#?}", err);
         std::process::exit(1);
@@ -42,7 +48,7 @@ async fn main_loop(mut sys_rpc_callback: Box<dyn FnMut(Vec<u8>) + Send>) {
         }
     });
 
-    listen_for_sys_msgs(rpc_receiver, ble_service, BleResultSender::new(tx))
+    listen_for_sys_msgs(ble_rpc_receiver, ble_service, BleResultSender::new(tx))
         .await
         .unwrap_or_else(|err| {
             error!("{:#?}", err);
