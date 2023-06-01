c74a4ffe0959ab481d5b52d9e20c23bd312c82a1
Harshil Jani <harshiljani2002@gmail.com>
2023-06-01 22:16:47 +0530

    Connected Matrix World with Qaul World
    By this commit, We have achieved a very initial milestone of connecting
    the Matrix with Qaul. Currently, In Qaul we have a separate thread
    running which listens for any activity into the Matrix room and
    forwards the messagess into the Qaul feel list.
    
    Signed-off-by: Harshil Jani <harshiljani2002@gmail.com>

--- rust/clients/bridge/src/relay_bot.rs
+++ rust/clients/bridge/src/relay_bot.rs
@@ -1,3 +1,10 @@
+// Copyright (c) 2021 Open Community Project Association https://ocpa.ch
+// This software is published under the AGPLv3 license.
+
+//! # Relay Bot functions
+//!
+//! Logging in and listening for the message on matrix room and sending messages from qaul.
+use super::rpc::Rpc;
 use matrix_sdk::{
     room::Room,
     ruma::events::{
@@ -6,49 +13,84 @@ use matrix_sdk::{
     },
     Client, ClientConfig, SyncSettings,
 };
+use prost::Message;
 use url::Url;
+/// include generated protobuf RPC rust definition file
+mod proto {
+    include!("../../../libqaul/src/rpc/protobuf_generated/rust/qaul.rpc.feed.rs");
+    include!("../../../libqaul/src/rpc/protobuf_generated/rust/qaul.rpc.users.rs");
+    include!("../../../libqaul/src/rpc/protobuf_generated/rust/qaul.rpc.rs");
+}
 
 async fn on_room_message(event: SyncMessageEvent<MessageEventContent>, room: Room) {
     if let Room::Joined(room) = room {
-        let msg_body = if let SyncMessageEvent {
+        let (msg_body, msg_sender) = if let SyncMessageEvent {
             content:
                 MessageEventContent {
                     msgtype: MessageType::Text(TextMessageEventContent { body: msg_body, .. }),
                     ..
                 },
+            sender: msg_sender,
             ..
         } = event
         {
-            msg_body
+            (msg_body, msg_sender)
         } else {
             return;
         };
+        if msg_sender != "@qaul-bot:matrix.org" {
+            let msg_text = format!("{} : {}", msg_sender, msg_body);
+            let proto_message = proto::Feed {
+                message: Some(proto::feed::Message::Send(proto::SendMessage {
+                    content: msg_text,
+                })),
+            };
+            // encode message
+            let mut buf = Vec::with_capacity(proto_message.encoded_len());
+            proto_message
+                .encode(&mut buf)
+                .expect("Vec<u8> provides capacity as needed");
 
-        if msg_body.contains("!qaul") {
-            let content = AnyMessageEventContent::RoomMessage(MessageEventContent::text_plain(
-                "I am a message sent from qaul network\n",
-            ));
-            println!("sending");
-            // send our message to the room we found the "!qaul command
-            room.send(content, None).await.unwrap();
-            println!("message sent");
+            // send message
+            Rpc::send_message(buf, super::rpc::proto::Modules::Feed.into(), "".to_string());
+
+            if msg_body.contains("!qaul") {
+                let content = AnyMessageEventContent::RoomMessage(MessageEventContent::text_plain(
+                    "I am a message sent from qaul network\n",
+                ));
+
+                // send our message to the matrix room we found the "!qaul command
+                room.send(content, None).await.unwrap();
+            }
+
+            if msg_body.contains("!users-list") {
+                // Here I need to trigger the RPC call from users.rs file.
+                todo!();
+            }
+            // If we are successfull in triggering for user-list, we can add all CLI commands in qaul to matrix commands.
+        } else {
+            println!("Sent the message in the matrix room by !qaul-bot");
         }
     }
 }
 
-async fn login (
+async fn login(
     homeserver_url: &str,
     username: &str,
     password: &str,
 ) -> Result<(), matrix_sdk::Error> {
     // the location for `JsonStore` to save files to
-    let mut home = dirs::home_dir().expect("no home directory found");
-    home.push("qaul_bot");
+    let mut home = dirs::config_dir().expect("no home directory found");
+    home.push("qaul/matrix");
+    println!("{:?}",home);
     let client_config = ClientConfig::new().store_path(home);
     let homeserver_url = Url::parse(&homeserver_url).expect("Couldn't parse the homeserver URL");
+
     // create a new Client with the given homeserver url and config
     let client = Client::new_with_config(homeserver_url, client_config).unwrap();
-    client.login(&username, &password, None, Some("command bot")).await?;
+    client
+        .login(&username, &password, None, Some("command bot"))
+        .await?;
     println!("logged in as {}", username);
 
     // An initial sync to set up state and so our bot doesn't respond to old
@@ -69,10 +111,10 @@ async fn login (
 }
 
 #[tokio::main]
-pub async fn connect()->Result<(), matrix_sdk::Error>{
+pub async fn connect() -> Result<(), matrix_sdk::Error> {
     println!("Connecting to Matrix Bot");
     // TODO : Instead of passing direct arguments, Either take them from CLI or pass it secretly.
     // this is something which we don't need to be exposed.
-    login("https://matrix.org","qaul-bot","qaul123promise").await?;
+    login("https://matrix.org", "qaul-bot", "qaul123promise").await?;
     Ok(())
 }
