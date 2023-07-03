0e391a49ea1c5cf39d224b1e4a241dd1600092bf
dastansam <dastan.samatov.33@gmail.com>
2023-07-03 23:06:24 +0300

    Push changes

--- rust/libqaul/src/rpc/protobuf_generated/rust/qaul.net.rtc.rs
+++ rust/libqaul/src/rpc/protobuf_generated/rust/qaul.net.rtc.rs
@@ -1,83 +1,93 @@
 /// RTC network message container
+#[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct RtcContainer {
-    #[prost(oneof="rtc_container::Message", tags="1, 2")]
+    #[prost(oneof = "rtc_container::Message", tags = "1, 2")]
     pub message: ::core::option::Option<rtc_container::Message>,
 }
 /// Nested message and enum types in `RtcContainer`.
 pub mod rtc_container {
+    #[allow(clippy::derive_partial_eq_without_eq)]
     #[derive(Clone, PartialEq, ::prost::Oneof)]
     pub enum Message {
         /// rtc session request
-        #[prost(message, tag="1")]
+        #[prost(message, tag = "1")]
         RtcSessionRequest(super::RtcSessionRequest),
         /// rtc session management
-        #[prost(message, tag="2")]
+        #[prost(message, tag = "2")]
         RtcSessionManagement(super::RtcSessionManagement),
     }
 }
 /// rtc session request
+#[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct RtcSessionRequest {
     /// type
-    #[prost(uint32, tag="1")]
+    #[prost(uint32, tag = "1")]
     pub session_type: u32,
 }
 /// rtc session management
+#[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct RtcSessionManagement {
     /// option (1: accept, 2: deny, 3: end)
-    #[prost(uint32, tag="1")]
+    #[prost(uint32, tag = "1")]
     pub option: u32,
 }
 /// Rtc message
+#[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct RtcMessage {
     /// sequence
-    #[prost(uint32, tag="1")]
+    #[prost(uint32, tag = "1")]
     pub sequence: u32,
     /// content
-    #[prost(bytes="vec", tag="2")]
+    #[prost(bytes = "vec", tag = "2")]
     pub content: ::prost::alloc::vec::Vec<u8>,
 }
 /// Rtc contents
+#[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct RtcContent {
     /// content
-    #[prost(oneof="rtc_content::Content", tags="1, 2, 3")]
+    #[prost(oneof = "rtc_content::Content", tags = "1, 2, 3")]
     pub content: ::core::option::Option<rtc_content::Content>,
 }
 /// Nested message and enum types in `RtcContent`.
 pub mod rtc_content {
     /// content
+    #[allow(clippy::derive_partial_eq_without_eq)]
     #[derive(Clone, PartialEq, ::prost::Oneof)]
     pub enum Content {
         /// video content
-        #[prost(message, tag="1")]
+        #[prost(message, tag = "1")]
         VideoContent(super::RtcVideoContent),
         /// audio content
-        #[prost(message, tag="2")]
+        #[prost(message, tag = "2")]
         AudioContent(super::RtcAudioContent),
         /// chat content
-        #[prost(message, tag="3")]
+        #[prost(message, tag = "3")]
         ChatContent(super::RtcChatContent),
     }
 }
+#[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct RtcChatContent {
     /// content
-    #[prost(string, tag="1")]
+    #[prost(string, tag = "1")]
     pub content: ::prost::alloc::string::String,
 }
+#[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct RtcVideoContent {
     /// content
-    #[prost(bytes="vec", tag="1")]
+    #[prost(bytes = "vec", tag = "1")]
     pub content: ::prost::alloc::vec::Vec<u8>,
 }
+#[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct RtcAudioContent {
     /// content
-    #[prost(bytes="vec", tag="1")]
+    #[prost(bytes = "vec", tag = "1")]
     pub content: ::prost::alloc::vec::Vec<u8>,
 }
--- rust/libqaul/src/services/dtn/qaul.rpc.dtn.rs
+++ rust/libqaul/src/services/dtn/qaul.rpc.dtn.rs
@@ -1,126 +1,136 @@
 /// DTN service RPC message container
+#[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct Dtn {
     /// message type
-    #[prost(oneof="dtn::Message", tags="1, 2, 3, 4, 5, 6, 7, 8, 9, 10")]
+    #[prost(oneof = "dtn::Message", tags = "1, 2, 3, 4, 5, 6, 7, 8, 9, 10")]
     pub message: ::core::option::Option<dtn::Message>,
 }
 /// Nested message and enum types in `DTN`.
 pub mod dtn {
     /// message type
+    #[allow(clippy::derive_partial_eq_without_eq)]
     #[derive(Clone, PartialEq, ::prost::Oneof)]
     pub enum Message {
         /// dtn state request
-        #[prost(message, tag="1")]
+        #[prost(message, tag = "1")]
         DtnStateRequest(super::DtnStateRequest),
         /// dtn state response
-        #[prost(message, tag="2")]
+        #[prost(message, tag = "2")]
         DtnStateResponse(super::DtnStateResponse),
         /// dtn config request
-        #[prost(message, tag="3")]
+        #[prost(message, tag = "3")]
         DtnConfigRequest(super::DtnConfigRequest),
         /// dtn config response
-        #[prost(message, tag="4")]
+        #[prost(message, tag = "4")]
         DtnConfigResponse(super::DtnConfigResponse),
         /// dtn add user request
-        #[prost(message, tag="5")]
+        #[prost(message, tag = "5")]
         DtnAddUserRequest(super::DtnAddUserRequest),
         /// dtn add user response
-        #[prost(message, tag="6")]
+        #[prost(message, tag = "6")]
         DtnAddUserResponse(super::DtnAddUserResponse),
         /// dtn remove user request
-        #[prost(message, tag="7")]
+        #[prost(message, tag = "7")]
         DtnRemoveUserRequest(super::DtnRemoveUserRequest),
         /// dtn remove user response
-        #[prost(message, tag="8")]
+        #[prost(message, tag = "8")]
         DtnRemoveUserResponse(super::DtnRemoveUserResponse),
         /// dtn set total size request
-        #[prost(message, tag="9")]
+        #[prost(message, tag = "9")]
         DtnSetTotalSizeRequest(super::DtnSetTotalSizeRequest),
         /// dtn set total size response
-        #[prost(message, tag="10")]
+        #[prost(message, tag = "10")]
         DtnSetTotalSizeResponse(super::DtnSetTotalSizeResponse),
     }
 }
 /// Dtn State Request
+#[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
-pub struct DtnStateRequest {
-}
+pub struct DtnStateRequest {}
 /// Dtn State Response
+#[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct DtnStateResponse {
     /// used size
-    #[prost(uint64, tag="1")]
+    #[prost(uint64, tag = "1")]
     pub used_size: u64,
     /// dtn message count
-    #[prost(uint32, tag="2")]
+    #[prost(uint32, tag = "2")]
     pub dtn_message_count: u32,
     /// unconfirmed count
-    #[prost(uint32, tag="3")]
+    #[prost(uint32, tag = "3")]
     pub unconfirmed_count: u32,
 }
 /// Dtn Config Request
+#[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
-pub struct DtnConfigRequest {
-}
+pub struct DtnConfigRequest {}
 /// Dtn Config Response
+#[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct DtnConfigResponse {
     /// total_size
-    #[prost(uint32, tag="1")]
+    #[prost(uint32, tag = "1")]
     pub total_size: u32,
     /// users
-    #[prost(bytes="vec", repeated, tag="2")]
+    #[prost(bytes = "vec", repeated, tag = "2")]
     pub users: ::prost::alloc::vec::Vec<::prost::alloc::vec::Vec<u8>>,
 }
 /// Dtn Add User Request
+#[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct DtnAddUserRequest {
     /// user id
-    #[prost(bytes="vec", tag="1")]
+    #[prost(bytes = "vec", tag = "1")]
     pub user_id: ::prost::alloc::vec::Vec<u8>,
 }
 /// Dtn Add User Response
+#[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct DtnAddUserResponse {
     /// total_size
-    #[prost(bool, tag="1")]
+    #[prost(bool, tag = "1")]
     pub status: bool,
     /// users
-    #[prost(string, tag="2")]
+    #[prost(string, tag = "2")]
     pub message: ::prost::alloc::string::String,
 }
 /// Dtn Remove User Request
+#[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct DtnRemoveUserRequest {
     /// user id
-    #[prost(bytes="vec", tag="1")]
+    #[prost(bytes = "vec", tag = "1")]
     pub user_id: ::prost::alloc::vec::Vec<u8>,
 }
 /// Dtn Remove User Response
+#[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct DtnRemoveUserResponse {
     /// total_size
-    #[prost(bool, tag="1")]
+    #[prost(bool, tag = "1")]
     pub status: bool,
     /// users
-    #[prost(string, tag="2")]
+    #[prost(string, tag = "2")]
     pub message: ::prost::alloc::string::String,
 }
 /// Dtn SetTotalSize Request
+#[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct DtnSetTotalSizeRequest {
     /// total_size
-    #[prost(uint32, tag="1")]
+    #[prost(uint32, tag = "1")]
     pub total_size: u32,
 }
 /// Dtn Remove User Response
+#[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct DtnSetTotalSizeResponse {
     /// total_size
-    #[prost(bool, tag="1")]
+    #[prost(bool, tag = "1")]
     pub status: bool,
     /// users
-    #[prost(string, tag="2")]
+    #[prost(string, tag = "2")]
     pub message: ::prost::alloc::string::String,
 }
