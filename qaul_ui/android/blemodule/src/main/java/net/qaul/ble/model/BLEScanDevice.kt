578fae764d0072b035488c8932885b55fb687afb
Dhaval <dhaval.bluepixel@gmail.com>
2023-07-06 18:03:15 +0530

    1. Fixing reconnect issue.

--- qaul_ui/android/blemodule/src/main/java/net/qaul/ble/model/BLEScanDevice.kt
+++ qaul_ui/android/blemodule/src/main/java/net/qaul/ble/model/BLEScanDevice.kt
@@ -6,12 +6,13 @@ package net.qaul.ble.model
 import android.bluetooth.BluetoothDevice
 import android.bluetooth.le.ScanResult
 
-abstract class BLEScanDevice  {
+abstract class BLEScanDevice {
     companion object {
         fun getDevice(): BLEScanDevice {
             return BLEDevice()
         }
     }
+
     abstract var deviceRSSI: Int
     abstract var scanResult: ScanResult?
     abstract var bluetoothDevice: BluetoothDevice?
