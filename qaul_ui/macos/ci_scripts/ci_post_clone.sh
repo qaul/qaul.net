7253c19a75b9b79ea6a7e7acce80cdca1801549e
Breno Teodoro <54450520+brenodt@users.noreply.github.com>
2024-04-24 15:25:25 -0300

    [CI] Xcode Cloud -MacOS setup (#625)
    * ci: (macos - xcode cloud) fix paths in post clone script
    
    * ci: (macos - xcode cloud) fix initial cd command so that flutter cmd is run on root of ui
    
    * ci: (macos - xcode cloud) try adding flutter build step
    
    * ci: (macos - xcode cloud) try adding flutter build step nº2
    
    * ci: (macos - xcode cloud) try adding flutter build step nº3
    
    * ci: (macos - xcode cloud) try adding flutter build step nº4
    
    * ci: (macos - xcode cloud) set automatic code signing, remove notarization post-script from archive
    
    * ci: (macos - xcode cloud) disable library validation
    
    * ci: (macos - xcode cloud) force signature on dylib
    
    * ci: (macos - xcode cloud) force signature on dylib, attempt the second
    
    * ci: (macos - xcode cloud) force signature on dylib, attempt the third
    
    * ci: (macos - xcode cloud) attempt to embed rust with cargo-xcode
    
    * ci: (macos - xcode cloud) remove pre-xcodebuild script
    
    * ci: (macos xcode) embed project in qaul_ui/macos
    
    * ci: (macos xcode) install protobuf
    
    * ci: (macos xcode) attempt to fix issue with cfBundleShortVersionString by removing extras from semver
    
    * ci: (macos xcode) attempt to fix issue with cfBundleShortVersionString by removing extras from semver - directly in xcode
    
    * ci: (macos xcode) automate approval of testflight build by declaring non-use of encryption
    
    For reference, see docs:
    https://developer.apple.com/documentation/security/complying_with_encryption_export_regulations#3145067
    
    * chore: set minimum deployment target to be 10.13
    
    * Revert "chore: set minimum deployment target to be 10.13"
    
    This reverts commit 3846e4771974fb44749418cd05e4ac40d117067d.
    
    * chore: revert unnecessary version change, update comment on post clone script

--- qaul_ui/macos/ci_scripts/ci_post_clone.sh
+++ qaul_ui/macos/ci_scripts/ci_post_clone.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
 #-----------------------------------------------------------------NoticeStart-
@@ -10,16 +10,17 @@ set -e
 #
 # DESCRIPTION
 #   This post-clone script is meant to be used to setup the XCode Cloud
-#   environment. It does mainly two things:
-#     - download and install flutter
-#     - download the latest libqaul *.dylib
+#   environment. It does:
+#     - install flutter
+#     - install rust
+#     - install protoc (rust dependency)
 #
 #   It's based on the Flutter documentation for setting up XCode Cloud:
 #   https://docs.flutter.dev/deployment/cd#custom-build-script
 #
 
 # The default execution directory of this script is the ci_scripts directory.
-cd $CI_PRIMARY_REPOSITORY_PATH # change working directory to the root of cloned repo.
+cd $CI_PRIMARY_REPOSITORY_PATH/qaul_ui # change working directory to the root of cloned repo.
 
 # Install Flutter using git.
 git clone https://github.com/flutter/flutter.git --depth 1 -b stable $HOME/flutter
@@ -36,12 +37,15 @@ HOMEBREW_NO_AUTO_UPDATE=1 # disable homebrew's automatic updates.
 brew install cocoapods
 
 # Install CocoaPods dependencies.
-cd ios && pod install # run `pod install` in the `ios` directory.
+cd macos && pod install # run `pod install` in the `macos` directory.
 
-# Install Github CLI
-brew install gh
+# Install Rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
-# Download Libqaul *.dylib File from latest Github Release
-gh release download --pattern "*.dylib" --repo "https://github.com/qaul/qaul.net/" --dir macos
+# Install protobuf
+brew install protobuf
+
+# Pre-build for macos
+flutter build macos --config-only
 
 exit 0
