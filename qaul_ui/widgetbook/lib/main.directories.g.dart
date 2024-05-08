db52daf60679d2f9db3d4442a62cc72d6f2a6206
brenodt <54450520+brenodt@users.noreply.github.com>
2024-05-08 17:22:06 -0300

    feat: add force update dialog widget; setup widgetbook and add use case for new widget

---
+++ qaul_ui/widgetbook/lib/main.directories.g.dart
@@ -0,0 +1,23 @@
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: unused_import, prefer_relative_imports, directives_ordering
+
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// AppGenerator
+// **************************************************************************
+
+// ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:widgetbook/widgetbook.dart' as _i1;
+import 'package:widgetbook_workspace/force_update_dialog.dart' as _i2;
+
+final directories = <_i1.WidgetbookNode>[
+  _i1.WidgetbookLeafComponent(
+    name: 'ForceUpdateDialog',
+    useCase: _i1.WidgetbookUseCase(
+      name: 'Default',
+      builder: _i2.buildCoolButtonUseCase,
+    ),
+  )
+];
