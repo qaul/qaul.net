9071c951f4a86e6bea9cdb7f3882e801146e5112
Mathias Jud <jud@wachter-jud.net>
2024-06-02 14:51:45 +0200

    Updated license screen

---
+++ qaul_ui/lib/screens/license_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
+
+import '../widgets/widgets.dart';
+
+class AboutScreen extends StatelessWidget {
+  const AboutScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final l10n = AppLocalizations.of(context)!;
+    var bundle = DefaultAssetBundle.of(context);
+
+    return ResponsiveScaffold(
+      icon: Icons.info_outline_rounded,
+      title: l10n.agplLicense,
+      body: FutureBuilder<String>(
+          future: bundle.loadString('assets/license/agpl-3.0.md'),
+          builder: (context, ss) {
+            if (!ss.hasData || ss.connectionState != ConnectionState.done) {
+              return const LoadingIndicator();
+            }
+            return Markdown(
+              data: ss.data ?? 'An error occurred',
+
+            );
+          }),
+    );
+  }
+}
