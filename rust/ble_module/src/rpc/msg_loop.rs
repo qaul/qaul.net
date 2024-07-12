055b0178bfd5b2f013f6f44a159f5f0d50c8333b
sukhman <sukhmansinghsaluja@gmail.com>
2024-07-12 05:47:44 +0530

    Debug: Spawn Rust Async thread

--- rust/ble_module/src/rpc/msg_loop.rs
+++ rust/ble_module/src/rpc/msg_loop.rs
@@ -1,6 +1,6 @@
 use async_std::task::spawn;
 use bytes::Bytes;
-use std::error::Error;
+use std::{clone, error::Error};
 
 use crate::{
     ble::ble_service::{get_device_info, QaulBleService},
@@ -17,47 +17,64 @@ pub async fn listen_for_sys_msgs(
 ) -> Result<(), Box<dyn Error>> {
     let mut local_sender_handle = internal_sender.clone();
     loop {
+        // async_std::task::spawn(async move {
         let evt = rpc_receiver.recv().await;
+        log::info!("Received event: ",);
         match evt {
             None => {
                 log::info!("Qaul 'sys' message channel closed. Shutting down gracefully.");
                 break;
             }
             Some(msg) => {
-                log::debug!("Received 'sys' message: {:#?}", msg);
                 if msg.message.is_none() {
                     continue;
                 }
                 match msg.message.unwrap() {
                     StartRequest(req) => match ble_service {
                         QaulBleService::Idle(svc) => {
+                            let mut internal_sender_1 = local_sender_handle.clone();
+                            // let mut internal_sender_2 = local_sender_handle.clone();
+                            // let
                             let qaul_id = Bytes::from(req.qaul_id);
-                            let handle = async_std::task::spawn(async move {
-                                let ble_service = svc
-                                    .advertise_scan_listen(qaul_id, None, internal_sender.clone())
-                                    .await;
-                                match ble_service {
-                                    QaulBleService::Idle(_) => {
-                                        log::error!("Error occured in configuring BLE module");
-                                    }
-                                    QaulBleService::Started(svc) => {
-                                        svc.spawn_handles().await;
-                                    }
+                            // let handle =
+                            //  async_std::task::spawn(async move {
+                            // let mut
+                            ble_service = svc
+                                .advertise_scan_listen(qaul_id, None, internal_sender_1.clone())
+                                .await;
+                            log::info!("BLE Service started successfully");
+                            match ble_service {
+                                QaulBleService::Idle(_) => {
+                                    log::error!("Error occured in configuring BLE module");
                                 }
-                                local_sender_handle.send_start_successful();
-                            });
-                            handle.await;
-                            break;
+                                QaulBleService::Started(ref mut svc) => {
+                                    // ble_service = QaulBleService::Started(svc);
+                                    //  async_std::task::spawn(async move {
+                                    // ble_service = 
+                                        svc.spawn_handles().await;
+                                        // svc.join_handles.await;
+                                    // });
+                                }
+                            }
+                            internal_sender_1.send_start_successful();
+                            //     ble_service
+                            // });
+                            // ble_service = handle.await;
+                            log::info!("BLE Service started successfully");
+                            // continue;
+                            // break;
                         }
                         QaulBleService::Started(_) => {
                             log::warn!(
                                 "Received Start Request, but bluetooth service is already running!"
                             );
                             local_sender_handle.send_result_already_running()
+                            // continue;
                         }
                     },
                     StopRequest(_) => match ble_service {
                         QaulBleService::Started(svc) => {
+                            log::info!("Received Stop Request");
                             ble_service = svc.stop(&mut local_sender_handle).await;
                         }
                         QaulBleService::Idle(_) => {
@@ -69,7 +86,7 @@ pub async fn listen_for_sys_msgs(
                     },
                     DirectSend(req) => match ble_service {
                         QaulBleService::Started(ref mut svc) => {
-                            log::debug!("Received Direct Send Request: {:#?}", req);
+                            log::info!("Received Direct Send Request: {:#?}", req);
                             let receiver_id = req.receiver_id.clone();
                             match svc.direct_send(req).await {
                                 Ok(_) => local_sender_handle.send_direct_send_success(receiver_id),
@@ -78,7 +95,7 @@ pub async fn listen_for_sys_msgs(
                             }
                         }
                         QaulBleService::Idle(_) => {
-                            log::warn!("Received Direct Send Request, but bluetooth service is not running!");
+                            log::info!("Received Direct Send Request, but bluetooth service is not running!");
                             local_sender_handle.send_result_not_running()
                         }
                     },
@@ -89,7 +106,9 @@ pub async fn listen_for_sys_msgs(
                                 Ok(info) => {
                                     sender_handle_clone.send_ble_sys_msg(InfoResponse(info))
                                 }
-                                Err(err) => log::error!("Error getting device info: {:#?}", &err),
+                                Err(err) => {
+                                    log::error!("Error getting device info: {:#?}", &err)
+                                }
                             }
                         });
                     }
@@ -97,6 +116,7 @@ pub async fn listen_for_sys_msgs(
                 }
             }
         }
+        // });
     }
     Ok(())
 }
