b6ff7ae1a45a00f649b062f164dba71cc1e6de6f
sukhman <sukhmansinghsaluja@gmail.com>
2024-08-30 19:12:47 +0530

    refactor: clean android code and added docs string in kotlin ble_module.

--- rust/ble_module/src/ble/ble_service.rs
+++ rust/ble_module/src/ble/ble_service.rs
@@ -1,3 +1,6 @@
+// Copyright (c) 2023 Open Community Project Association https://ocpa.ch
+// This software is published under the AGPLv3 license.
+
 use super::super::BleRpc;
 use super::utils::find_device_by_mac;
 use crate::{
