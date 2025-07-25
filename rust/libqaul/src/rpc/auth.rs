b169f295214c63ef4438125a41f2c069a70c17fe
claddy <0xcladdy@gmail.com>
2025-07-25 13:23:22 +0530

    feat(auth): implement auth challenge generation and nonce management

--- rust/libqaul/src/rpc/auth.rs
+++ rust/libqaul/src/rpc/auth.rs
@@ -1,3 +1,11 @@
+use std::collections::BTreeMap;
+use std::sync::RwLock;
+use config::FileFormat::Ini;
+use libp2p::PeerId;
+use state::InitCell;
+use crate::node::user_accounts::{UserAccount, UserAccounts};
+use crate::utilities::timestamp::Timestamp;
+
 pub mod proto {
     include!("qau.rpc.auth.rs");
 }
@@ -10,19 +18,52 @@ pub struct AuthChallenge {
     pub expires_at: u64,
 }
 
-// NONCE COUNTER
-// ACTIVE_CHALLENGES = TREE COULD BE USED
-// AUTHENTICATED_USERS = TREE
+static NONCE_COUNTER : InitCell<RwLock<u64>> = InitCell::new();
+static ACTIVE_CHALLENGES: InitCell<RwLock<BTreeMap<Vec<u8>, AuthChallenge>>> = InitCell::new();
+static AUTHENTICATED_USERS : InitCell<RwLock<BTreeMap<Vec<u8>, u64>>> = InitCell::new();
 
 pub struct Authentication{}
 
 impl Authentication {
 
-    pub fn init() {}
+    pub fn init() {
+        NONCE_COUNTER.set(RwLock::new(1));
+        ACTIVE_CHALLENGES.set(RwLock::new(BTreeMap::new()));
+        AUTHENTICATED_USERS.set(RwLock::new(BTreeMap::new()));
+    }
 
-    fn next_nonce() {}
+    fn next_nonce() -> u64 {
+        let mut counter = NONCE_COUNTER.get().write().unwrap();
+        let nonce = *counter;
+        *counter += 1;
+        nonce
+    }
 
-    pub fn create_challenge() {}
+    pub fn create_challenge(qaul_id: Vec<u8>) -> Result<u64, String> {
+        let peer_id = PeerId::from_bytes(&qaul_id)
+            .map_err(|_| "Invalid qaulId".to_string())?;
+
+        if UserAccounts::get_by_id(peer_id).is_none() {
+            return Err("User not found".to_string());
+        }
+
+        let nonce = Self::next_nonce();
+
+        let now = Timestamp::get_timestamp();
+        let challenge = AuthChallenge {
+            nonce,
+            qaul_id: qaul_id.clone(),
+            created_at: now,
+            expires_at: now + 300, // I need to confirm this
+        };
+
+        let mut challenges = ACTIVE_CHALLENGES.get().write().unwrap();
+        challenges.insert(qaul_id, challenge);
+
+        Self::cleanup_expired_challenge(&mut challenges, now);
+
+        Ok(nonce)
+    }
 
     pub fn verify_challenge() {}
 
@@ -33,10 +74,10 @@ impl Authentication {
     pub fn logout() {}
 
     fn cleanup_expired_challenge(
-        /*
-        Should I reuse them after a while?
-         */
-    ) {}
+        challenges: &mut BTreeMap<Vec<u8>, AuthChallenge>, now: u64
+    ) {
+        challenges.retain(|_, challenge| now < challenge.expires_at);
+    }
 
     pub fn rpc() {}
 
