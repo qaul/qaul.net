bdd0d8031dbabd667ad3f7435d99287c13ff0ed2
Gustavo Silva <contato@gustavodcs.com>
2026-01-22 12:22:11 -0300

    refactor: update widgetbook imports and adjust component structure

--- qaul_ui/widgetbook/lib/main.directories.g.dart
+++ qaul_ui/widgetbook/lib/main.directories.g.dart
@@ -1,3 +1,4 @@
+// dart format width=80
 // coverage:ignore-file
 // ignore_for_file: type=lint
 // ignore_for_file: unused_import, prefer_relative_imports, directives_ordering
@@ -9,15 +10,19 @@
 // **************************************************************************
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:widgetbook/widgetbook.dart' as _i1;
-import 'package:widgetbook_workspace/force_update_dialog.dart' as _i2;
+import 'package:widgetbook/widgetbook.dart' as _widgetbook;
+import 'package:widgetbook_workspace/force_update_dialog.dart'
+    as _widgetbook_workspace_force_update_dialog;
 
-final directories = <_i1.WidgetbookNode>[
-  _i1.WidgetbookLeafComponent(
+final directories = <_widgetbook.WidgetbookNode>[
+  _widgetbook.WidgetbookComponent(
     name: 'ForceUpdateDialog',
-    useCase: _i1.WidgetbookUseCase(
-      name: 'Default',
-      builder: _i2.buildCoolButtonUseCase,
-    ),
+    useCases: [
+      _widgetbook.WidgetbookUseCase(
+        name: 'Default',
+        builder:
+            _widgetbook_workspace_force_update_dialog.buildCoolButtonUseCase,
+      )
+    ],
   )
 ];
