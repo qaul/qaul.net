830c9fb03458f3278bd429c075f84b8acd8b247e
Gustavo Silva <contato@gustavodcs.com>
2026-01-23 16:29:07 -0300

    refactor: simplify file size calculations by removing unnecessary conversions and rounding options

--- qaul_ui/packages/utils/lib/src/file_size.dart
+++ qaul_ui/packages/utils/lib/src/file_size.dart
@@ -1,64 +1,18 @@
-/// A method returns a human readable string representing a file _size
-String fileSize(dynamic size, [int round = 2]) {
-  /** 
-   * [size] can be passed as number or as string
-   *
-   * the optional parameter [round] specifies the number 
-   * of digits after comma/point (default is 2)
-   */
-  var divider = 1024;
-  int parsedSize;
-  try {
-    parsedSize = int.parse(size.toString());
-  } catch (e) {
-    throw ArgumentError('Can not parse the size parameter: $e');
-  }
+String fileSize(dynamic size) {
+  const divider = 1024;
+  final parsedSize = int.parse(size.toString());
 
   if (parsedSize < divider) {
     return '$parsedSize B';
   }
 
-  if (parsedSize < divider * divider && parsedSize % divider == 0) {
-    return '${(parsedSize / divider).toStringAsFixed(0)} KB';
-  }
-
   if (parsedSize < divider * divider) {
-    return '${(parsedSize / divider).toStringAsFixed(round)} KB';
-  }
-
-  if (parsedSize < divider * divider * divider && parsedSize % divider == 0) {
-    return '${(parsedSize / (divider * divider)).toStringAsFixed(0)} MB';
+    return '${(parsedSize / divider).toStringAsFixed(2)} KB';
   }
 
   if (parsedSize < divider * divider * divider) {
-    return '${(parsedSize / divider / divider).toStringAsFixed(round)} MB';
+    return '${(parsedSize / (divider * divider)).toStringAsFixed(2)} MB';
   }
 
-  if (parsedSize < divider * divider * divider * divider && parsedSize % divider == 0) {
-    return '${(parsedSize / (divider * divider * divider)).toStringAsFixed(0)} GB';
-  }
-
-  if (parsedSize < divider * divider * divider * divider) {
-    return '${(parsedSize / divider / divider / divider).toStringAsFixed(round)} GB';
-  }
-
-  if (parsedSize < divider * divider * divider * divider * divider &&
-      parsedSize % divider == 0) {
-    num r = parsedSize / divider / divider / divider / divider;
-    return '${r.toStringAsFixed(0)} TB';
-  }
-
-  if (parsedSize < divider * divider * divider * divider * divider) {
-    num r = parsedSize / divider / divider / divider / divider;
-    return '${r.toStringAsFixed(round)} TB';
-  }
-
-  if (parsedSize < divider * divider * divider * divider * divider * divider &&
-      parsedSize % divider == 0) {
-    num r = parsedSize / divider / divider / divider / divider / divider;
-    return '${r.toStringAsFixed(0)} PB';
-  } else {
-    num r = parsedSize / divider / divider / divider / divider / divider;
-    return '${r.toStringAsFixed(round)} PB';
-  }
+  return '${(parsedSize / (divider * divider * divider)).toStringAsFixed(2)} GB';
 }
--- qaul_ui/packages/utils/lib/src/file_size_descriptor.dart
+++ qaul_ui/packages/utils/lib/src/file_size_descriptor.dart
@@ -1,7 +1,3 @@
 extension FileSizeDescriptor on int {
   int get kb => this * 1000;
-
-  int get mb => kb * 1000;
-
-  int get gb => mb * 1000;
 }
