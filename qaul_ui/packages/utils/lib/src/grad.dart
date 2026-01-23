983ce8d9cec78516f412cbd784dacaff6f171e57
Gustavo Silva <contato@gustavodcs.com>
2026-01-23 16:08:45 -0300

    refactor: remove Grad2 and Grad3 classes, clean up open_simplex_2.dart file

--- qaul_ui/packages/utils/lib/src/grad.dart
+++ qaul_ui/packages/utils/lib/src/grad.dart
@@ -1,30 +1,3 @@
-/// 2-dimensional gradient.
-class Grad2 {
-  /// Creates a 2-dimensional gradient from its components.
-  const Grad2(this.dx, this.dy);
-
-  /// The x component of the gradient.
-  final double dx;
-
-  /// The y component of the gradient.
-  final double dy;
-}
-
-/// 3-dimensional gradient.
-class Grad3 {
-  /// Creates a 3-dimensional gradient from its components.
-  const Grad3(this.dx, this.dy, this.dz);
-
-  /// The x component of the gradient.
-  final double dx;
-
-  /// The y component of the gradient.
-  final double dy;
-
-  /// The z component of the gradient.
-  final double dz;
-}
-
 /// 4-dimensional gradient.
 class Grad4 {
   /// Creates a 4-dimensional gradient from its components.
--- qaul_ui/packages/utils/lib/src/open_simplex_2.dart
+++ qaul_ui/packages/utils/lib/src/open_simplex_2.dart
@@ -1,5 +1,3 @@
-import 'open_simplex_2f.dart';
-
 /// Abstract base class for OpenSimplex2 noise.
 abstract class OpenSimplex2 {
   /// 4D noise, classic lattice orientation.
