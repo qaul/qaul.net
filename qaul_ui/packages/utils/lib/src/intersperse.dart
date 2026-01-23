94395fd09271cccd939c24c0eaf042d1991a1146
Gustavo Silva <contato@gustavodcs.com>
2026-01-23 16:14:41 -0300

    refactor: remove intersperse_extensions.dart and streamline intersperse implementation to what is aplicable in our repo

--- qaul_ui/packages/utils/lib/src/intersperse.dart
+++ qaul_ui/packages/utils/lib/src/intersperse.dart
@@ -1,12 +1,4 @@
-/// Puts [element] between every element in [iterable].
-///
-/// Example:
-///
-///     final list1 = intersperse(2, <int>[]); // [];
-///     final list2 = intersperse(2, [0]); // [0];
-///     final list3 = intersperse(2, [0, 0]); // [0, 2, 0];
-///
-Iterable<T> intersperse<T>(T element, Iterable<T> iterable) sync* {
+Iterable<T> _intersperseImpl<T>(T element, Iterable<T> iterable) sync* {
   final iterator = iterable.iterator;
   if (iterator.moveNext()) {
     yield iterator.current;
@@ -17,21 +9,27 @@ Iterable<T> intersperse<T>(T element, Iterable<T> iterable) sync* {
   }
 }
 
-/// Puts [element] between every element in [iterable] and at the bounds of [iterable].
+/// Puts [element] between every element in [iterable].
 ///
 /// Example:
 ///
-///     final list1 = intersperseOuter(2, <int>[]); // [];
-///     final list2 = intersperseOuter(2, [0]); // [2, 0, 2];
-///     final list3 = intersperseOuter(2, [0, 0]); // [2, 0, 2, 0, 2];
+///     final list1 = intersperse(2, <int>[]); // [];
+///     final list2 = intersperse(2, [0]); // [0];
+///     final list3 = intersperse(2, [0, 0]); // [0, 2, 0];
 ///
-Iterable<T> intersperseOuter<T>(T element, Iterable<T> iterable) sync* {
-  final iterator = iterable.iterator;
-  if (iterable.isNotEmpty) {
-    yield element;
-  }
-  while (iterator.moveNext()) {
-    yield iterator.current;
-    yield element;
+Iterable<T> intersperse<T>(T element, Iterable<T> iterable) =>
+    _intersperseImpl(element, iterable);
+
+extension IntersperseExtensions<T> on Iterable<T> {
+  /// Puts [element] between every element in [list].
+  ///
+  /// Example:
+  ///
+  ///     final list1 = <int>[].intersperse(2); // [];
+  ///     final list2 = [0].intersperse(2); // [0];
+  ///     final list3 = [0, 0].intersperse(2); // [0, 2, 0];
+  ///
+  Iterable<T> intersperse(T element) {
+    return _intersperseImpl(element, this);
   }
 }
