2a7e2596edd1ebe4b4936561eacb80936bdf3e89
Gustavo Silva <contato@gustavodcs.com>
2026-01-25 23:39:08 -0300

    feat: add test case to solidify changes

---
+++ qaul_ui/packages/utils/test/file_size_test.dart
@@ -0,0 +1,35 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:utils/utils.dart';
+
+void main() {
+  group('fileSize', () {
+    final testcases = <MapEntry<dynamic, String>>[
+      MapEntry(0, '0 B'),
+      MapEntry(1, '1 B'),
+      MapEntry(512, '512 B'),
+      MapEntry(1023, '1023 B'),
+      MapEntry(1024, '1.00 KB'),
+      MapEntry(1536, '1.50 KB'),
+      MapEntry(2048, '2.00 KB'),
+      MapEntry(10240, '10.00 KB'),
+      MapEntry(1048575, '1024.00 KB'),
+      MapEntry(1048576, '1.00 MB'),
+      MapEntry(1572864, '1.50 MB'),
+      MapEntry(10485760, '10.00 MB'),
+      MapEntry(1073741823, '1024.00 MB'),
+      MapEntry(1073741824, '1.00 GB'),
+      MapEntry(1610612736, '1.50 GB'),
+      MapEntry(10737418240, '10.00 GB'),
+      MapEntry('0', '0 B'),
+      MapEntry('1024', '1.00 KB'),
+      MapEntry('1048576', '1.00 MB'),
+      MapEntry('1073741824', '1.00 GB'),
+    ];
+
+    for (final tc in testcases) {
+      test('${tc.key} becomes ${tc.value}', () {
+        expect(fileSize(tc.key), tc.value);
+      });
+    }
+  });
+}
