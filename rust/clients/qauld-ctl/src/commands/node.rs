49e98dad7ffc0df76afedb9e41f596919119e948
Baasit <baasisek01@gmail.com>
2026-03-09 16:11:45 +0100

    feat(server): #541 added json flag for outpus

--- rust/clients/qauld-ctl/src/commands/node.rs
+++ rust/clients/qauld-ctl/src/commands/node.rs
@@ -1,4 +1,5 @@
 use prost::Message;
+use serde_json::json;
 
 use crate::{cli::NodeSubcmd, commands::RpcCommand, proto::Modules};
 
@@ -20,16 +21,21 @@ impl RpcCommand for NodeSubcmd {
         Ok((buf, Modules::Node))
     }
 
-    fn decode_response(&self, data: &[u8]) -> Result<(), Box<dyn std::error::Error>> {
+    fn decode_response(&self, data: &[u8], json: bool) -> Result<(), Box<dyn std::error::Error>> {
         match Node::decode(data) {
             Ok(node) => {
                 match node.message {
                     Some(node::Message::Info(proto_nodeinformation)) => {
                         // print information
-                        println!("Node ID is: {}", proto_nodeinformation.id_base58);
-                        println!("Node Addresses are:");
-                        for address in proto_nodeinformation.addresses {
-                            println!("    {}", address);
+                        if json {
+                            let obj = json!({ "node_id": proto_nodeinformation.id_base58, "addresses": proto_nodeinformation.addresses });
+                            println!("{}", serde_json::to_string_pretty(&obj)?);
+                        } else {
+                            println!("Node ID is: {}", proto_nodeinformation.id_base58);
+                            println!("Node Addresses are:");
+                            for address in proto_nodeinformation.addresses {
+                                println!("    {}", address);
+                            }
                         }
                     }
                     _ => {}
