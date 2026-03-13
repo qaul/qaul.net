844e53403b1b295daab3cc5015aa3d5202943f67
brenodt <54450520+brenodt@users.noreply.github.com>
2026-03-13 12:09:52 -0300

    chore: update flutter analyze ci pipeline with new widgetbook path

--- qaul_ui/packages/qaul_components/widgetbook_workspace/lib/main.directories.g.dart
+++ qaul_ui/packages/qaul_components/widgetbook_workspace/lib/main.directories.g.dart
@@ -10,6 +10,8 @@
 // **************************************************************************
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:qaul_components_widgetbook/use_cases/qaul_fab.dart'
+    as _qaul_components_widgetbook_use_cases_qaul_fab;
 import 'package:qaul_components_widgetbook/use_cases/qaul_navbar.dart'
     as _qaul_components_widgetbook_use_cases_qaul_navbar;
 import 'package:widgetbook/widgetbook.dart' as _widgetbook;
@@ -18,6 +20,21 @@ final directories = <_widgetbook.WidgetbookNode>[
   _widgetbook.WidgetbookFolder(
     name: 'widgets',
     children: [
+      _widgetbook.WidgetbookComponent(
+        name: 'QaulFAB',
+        useCases: [
+          _widgetbook.WidgetbookUseCase(
+            name: 'Default',
+            builder: _qaul_components_widgetbook_use_cases_qaul_fab
+                .buildQaulFabDefaultUseCase,
+          ),
+          _widgetbook.WidgetbookUseCase(
+            name: 'Small (chat)',
+            builder: _qaul_components_widgetbook_use_cases_qaul_fab
+                .buildQaulFabSmallUseCase,
+          ),
+        ],
+      ),
       _widgetbook.WidgetbookComponent(
         name: 'QaulNavBar',
         useCases: [
